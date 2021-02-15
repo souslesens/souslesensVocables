688bfd438c148fb55a323bdbdda2ccd2029c838c
Claude.fauconnet <claude.fauconnet@neuf.fr>
2021-02-15 12:59:02 +0100

    suite

--- public/vocables/js/sparqlProxies/sparql_generic.js
+++ public/vocables/js/sparqlProxies/sparql_generic.js
@@ -1,7 +1,7 @@
 //biblio
 //https://www.iro.umontreal.ca/~lapalme/ift6281/sparql-1_1-cheat-sheet.pdf
 
-/**
+/** The MIT License
  Copyright 2020 Claude Fauconnet / SousLesens Claude.fauconnet@gmail.com
 
  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:

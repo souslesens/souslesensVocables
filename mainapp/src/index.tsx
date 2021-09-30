6e508b011066a6a1434f2dbd366739ee4c050377
yoelisA <acourt.yoelis@gmail.com>
2021-09-30 11:30:49 +0200

    fix(deps)

--- mainapp/src/index.tsx
+++ mainapp/src/index.tsx
@@ -1,5 +1,7 @@
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Button } from '@mui/material';
 
-ReactDOM.render(<div>Hello world!</div>, document.getElementById("mount-app-here"));
+
+ReactDOM.render(<Button>Hello bob</Button>, document.getElementById("mount-app-here"));

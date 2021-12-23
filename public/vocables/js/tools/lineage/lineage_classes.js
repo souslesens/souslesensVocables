5e2be963a82f221083efb3535ebc443c6d82dcd9
xgaia <xgaia@gmx.com>
2021-12-23 11:46:05 +0100

    solve conflict

--- public/vocables/js/tools/lineage/lineage_classes.js
+++ public/vocables/js/tools/lineage/lineage_classes.js
@@ -147,19 +147,12 @@ var Lineage_classes = (function () {
                 var str = "<a href='" + wikiUrl + "' target='_blank'>" + "Wiki page..." + "</a>"
                 $("#lineage_sourceDescriptionDiv").html(str)
                 self.registerSourceImports(sourceLabel)
-<<<<<<< HEAD
-                if (!self.mainSource) {
-                    self.initUI();
-                }
-=======
 
                 if (!self.mainSource) {
                   self.initUI();
                 }
 
->>>>>>> master
                 Lineage_relations.init()
-
 
 
             })

b9da054f4c722e92fac052a91502093a71659270
yoelis <yacourt@logilab.fr>
2022-01-14 18:02:13 +0100

    fix(api): fix nested object

--- api/v1/paths/profiles.js
+++ api/v1/paths/profiles.js
@@ -1,10 +1,11 @@
 const path = require("path");
 const fs = require("fs")
+const ulid = require('ulid');
 const profilesJSON = path.resolve('config/profiles.json');
 exports.profilesJSON = profilesJSON;
 const _ = require("lodash")
 const { rest } = require("lodash");
-const { readRessource, writeRessource, ressourceFetched, ressourceUpdated, responseSchema } = require("./utils");
+const { readRessource, writeRessource, ressourceFetched, ressourceUpdated, responseSchema, ressourceCreated } = require("./utils");
 
 module.exports = function () {
     let operations = {
@@ -12,25 +13,23 @@ module.exports = function () {
         POST,
         PUT
     };
-    function GET(req, res, next) {
-        fs.readFile(profilesJSON, 'utf8', (err, data) => {
-            if (err) {
-                res.status(500).json({ message: "I couldn't read profiles.json" })
-            } else {
-                const profiles = JSON.parse(data);
-                ressourceFetched(res, profiles)
-            }
-        });
+    async function GET(req, res, next) {
+        const profiles = await readRessource(profilesJSON, res);
+        // I need to have the db indexed by a unique id. 
+        //const sanitizedDB = await writeRessource(profilesJSON, profiles, res)
+
+        ressourceFetched(res, profiles)
     }
+
 
     async function PUT(req, res, next) {
         const updatedProfile = req.body
         try {
             const objectToUpdateKey = Object.keys(req.body)[0]
             const oldProfiles = await readRessource(profilesJSON, res)//.catch(e => res.status((500).json({ message: 'I couldn\'t read the ressource' })));
-            const updatedProfiles = { ...oldProfiles, [objectToUpdateKey]: updatedProfile }
-            const savedProfiles = await writeRessource(profilesJSON, updatedProfiles, res)//.catch(e => res.status((500).json({ message: "I couldn't write the ressource" })));
+            const updatedProfiles = { ...oldProfiles, ...updatedProfile }
             if (oldProfiles.hasOwnProperty(objectToUpdateKey)) {
+                const savedProfiles = await writeRessource(profilesJSON, updatedProfiles, res)//.catch(e => res.status((500).json({ message: "I couldn't write the ressource" })));
                 ressourceUpdated(res, savedProfiles)
             } else { res.status(400).json({ message: "Ressource does not exist. If you want to create another ressource, use POST instead." }) }
 
@@ -44,9 +43,9 @@ module.exports = function () {
             const oldProfiles = await readRessource(profilesJSON, res)
             const profileDoesntExist = !oldProfiles.hasOwnProperty(Object.keys(profileToAdd)[0])
             const newProfiles = { ...oldProfiles, ...profileToAdd }
-            const saved = await writeRessource(profilesJSON, newProfiles, res)
             if (profileDoesntExist) {
-                res.status(200).json(saved)
+                const saved = await writeRessource(profilesJSON, newProfiles, res)
+                ressourceCreated(res, saved);
             } else { res.status(400).json({ message: "Ressource already exists. If you want to update an existing ressource, use PUT instead." }) }
         } catch (e) { res.status(500) }
 
@@ -85,4 +84,10 @@ module.exports = function () {
 }
 
 
+
+function sanitizeDB(profiles) {
+    const sanitized = Object.entries(profiles).map(([key, val]) => { let id = ulid(); if (!val.id) { ({ [id]: { ...val, id: id } }); } else { [key, val] } });
+    console.log("DATA SANITIZED", sanitized)
+    return sanitized
+}
 

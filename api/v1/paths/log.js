1bde3e6c7aeccc866671cce87811c0e54eb94944
xgaia <xgaia@gmx.com>
2022-01-26 15:56:19 +0100

    feat: add GET logs

--- api/v1/paths/log.js
+++ api/v1/paths/log.js
@@ -1,9 +1,11 @@
 const path = require("path");
+const config = require(path.resolve("config/mainConfig.json"));
 const logger = require(path.resolve("bin/logger..js"));
+const fs = require('fs');
 
 module.exports = function() {
   let operations = {
-    POST
+    POST, GET
   };
 
   function POST(req, res, next) {
@@ -12,6 +14,49 @@ module.exports = function() {
     req.body.infos += "," + ip;
     logger.info(req.body.infos);
     return res.status(200).json({ done: true })
+  }
+
+  function GET(req, res, next) {
+    const logsArray = fs.readFileSync(path.resolve(config.logDir + "/vocables.log")).toString().split("\n").filter(line => line != "").map(line => {
+
+      const jsonLine = JSON.parse(line)
+      const message = jsonLine.message.split(",")
+      return {
+        user: message[0],
+        tool: message[1],
+        timestamp: jsonLine.timestamp
+      }
+    })
+    return res.status(200).json(logsArray)
+  }
+
+  GET.apiDoc = {
+    security: [{restrictAdmin: []}],
+    summary: 'Get users logs',
+    description: "Get users logs",
+    operationId: 'Get users logs',
+    responses: {
+      200: {
+        description: 'Logs',
+        schema: {
+          type: 'array',
+            items:{
+                type: "object",
+                properties: {
+                  user: {
+                    type: "string"
+                  },
+                  tool: {
+                    type: "string"
+                  },
+                  timestamp: {
+                    type: "string"
+                  }
+                }
+            }
+        }
+      },
+    }
   }
 
   POST.apiDoc = {

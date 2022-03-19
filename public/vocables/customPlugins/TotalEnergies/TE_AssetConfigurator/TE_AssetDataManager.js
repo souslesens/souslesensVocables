2ef7e341396566acae32922df26b953e696ed6e6
claudefauconnet <claude.fauconnet@neuf.fr>
2022-03-19 07:22:30 +0100

    continue TE_AssetConfigurator.js

--- public/vocables/customPlugins/TotalEnergies/TE_AssetConfigurator/TE_AssetDataManager.js
+++ public/vocables/customPlugins/TotalEnergies/TE_AssetConfigurator/TE_AssetDataManager.js
@@ -3,27 +3,31 @@ var TE_AssetDataManager = (function () {
     var self = {}
     var assetTreeDistinctNodes = {}
     var graphAssetNodes;
+    var coloredNodesMap;
     self.loadAsset = function (asset) {
         if (asset == "")
             return;
-        self.currenTable = asset;
 
-        if (self.currenTable == "girassol") {
+
+        if (asset == "girassol") {
             self.currentTable_14224Field = "RDLRelation";
             self.workOrdersTable = "Wordorder_girassol"
             self.failuresTable = ""
             self.currentDbName = 'data14224'
             self.currentFLcolumn = 'functionalLocationDescription'
+            self.currenTable = girassol;
 
-        } else if (self.currenTable == "absheron") {
+        } else if (asset == "absheron") {
             self.currentTable_14224Field = "RDLRelation"
             self.currentDbName = 'data14224'
+            self.currenTable = absheron;
 
-        } else if (self.currenTable == "Moho_N") {
+        } else if (asset == "Moho_N") {
             self.currentTable_14224Field = "RDScode"
             self.currentDbName = 'evolen'
             self.currentOpenNodeQuery = ' select *  from Moho_N where  location2 ='
             self.currentFLcolumn = 'location3'
+            self.currenTable = null;
         } else {
             self.currentTable_14224Field = null
         }
@@ -47,9 +51,11 @@ var TE_AssetDataManager = (function () {
 
 
         ]
+
+        coloredNodesMap = TE_AssetConfigurator.asset.getLinkedAssetNodesMap()
         async.series([
                 function (callback) {
-                    self.getEquipments("equipments", "Equipments", function (err, jstreeData) {
+                    TE_SqlTojstreeConnectors.getJsTreeData_moho_equipments("Equipments", coloredNodesMap, function (err, jstreeData) {
                         if (err)
                             return callback(err)
                         allJstreeData = allJstreeData.concat(jstreeData)
@@ -57,16 +63,23 @@ var TE_AssetDataManager = (function () {
                     })
                 },
 
-
                 function (callback) {
-                  return  callback()
-                    self.getLines("lines", "Lines", function (err, jstreeData) {
+                    TE_SqlTojstreeConnectors.getJsTreeData_moho_lines("Lines", coloredNodesMap, function (err, jstreeData) {
+                        if (err)
+                            return callback(err)
+                        allJstreeData = allJstreeData.concat(jstreeData)
+                        callback()
+                    })
+                },
+                function (callback) {
+                    TE_SqlTojstreeConnectors.getJsTreeData_moho_instruments("Instruments", coloredNodesMap, function (err, jstreeData) {
                         if (err)
                             return callback(err)
                         allJstreeData = allJstreeData.concat(jstreeData)
                         callback()
                     })
                 }
+
             ], function (err) {
                 if (err)
                     return MainController.UI.message(err)
@@ -87,147 +100,6 @@ var TE_AssetDataManager = (function () {
     }
 
 
-    self.getEquipments = function (table, parentNode, callback) {
-        graphAssetNodes = TE_AssetConfigurator.asset.getLinkedAssetNodesMap()
-
-        var limit = 100000
-
-        var sqlQuery = " select distinct concat('',id) as id,location1,location2 from " + table +
-            " where (location4 is null or location4='')"
-        ""// " where (location3 is null or location3='') and (location2 is not null and location2 !='')";
-
-
-        self.querySQLserver(sqlQuery, function (err, data) {
-            if (err)
-                return MainController.UI.message(err)
-            var jstreeData = []
-            assetTreeDistinctNodes = {}
-
-            data.forEach(function (item) {
-
-                if (!item.location1)
-                    return
-                if (!assetTreeDistinctNodes[item.location1]) {
-                    var text = item.location1
-                    if (graphAssetNodes && graphAssetNodes["A_" + item.id])
-                        text = "<span class='RDSassetTreeNode'>" + text + " </span>"
-                    assetTreeDistinctNodes[item.location1] = 1
-                    jstreeData.push({
-                        id: item.location1,
-                        text: item.location1,
-                        parent: parentNode,
-                        data: {
-                            FunctionalLocationCode: text,
-                            id: item.location1,
-                            label: item.location1,
-                            type: "location",
-                        }
-
-
-                    })
-                }
-                if (!assetTreeDistinctNodes[item.location2]) {
-                    assetTreeDistinctNodes[item.location2] = 1
-                    if (item.id == "684")
-                        var x = 3
-                    var text = item.location2
-                    if (graphAssetNodes && graphAssetNodes["A_" + item.id])
-                        text = "<span class='RDSassetTreeNode'>" + text + " </span>"
-                    jstreeData.push({
-                        //  id: item.location1 + "/" + item.location2,
-                        id: item.id,
-                        text: text,
-                        parent: item.location1,
-                        data: {
-                            FunctionalLocationCode: item.location1 + "/" + item.location2,
-                            // id: item.location1 + "/" + item.location2,
-                            id: item.id,
-                            label: item.location2,
-                            type: "location",
-                        }
-
-
-                    })
-                }
-
-
-            })
-
-            return callback(null, jstreeData)
-        })
-
-    }
-    self.getLines = function (table, parentNode, callback) {
-        return
-        graphAssetNodes = TE_AssetConfigurator.asset.getLinkedAssetNodesMap()
-
-        var limit = 100000
-
-        var sqlQuery = " select distinct LineId,Fluid from" + table +
-            " where (location4 is null or location4='')"
-
-
-
-        self.querySQLserver(sqlQuery, function (err, data) {
-            if (err)
-                return MainController.UI.message(err)
-            var jstreeData = []
-            assetTreeDistinctNodes = {}
-
-            data.forEach(function (item) {
-
-                if (!item.LineId)
-                    return
-                if (!assetTreeDistinctNodes[item.location1]) {
-                    var text = item.location1
-                    if (graphAssetNodes && graphAssetNodes["A_" + item.id])
-                        text = "<span class='RDSassetTreeNode'>" + text + " </span>"
-                    assetTreeDistinctNodes[item.location1] = 1
-                    jstreeData.push({
-                        id: item.location1,
-                        text: item.location1,
-                        parent: parentNode,
-                        data: {
-                            FunctionalLocationCode: text,
-                            id: item.location1,
-                            label: item.location1,
-                            type: "location",
-                        }
-
-
-                    })
-                }
-                if (!assetTreeDistinctNodes[item.location2]) {
-                    assetTreeDistinctNodes[item.location2] = 1
-                    if (item.id == "684")
-                        var x = 3
-                    var text = item.location2
-                    if (graphAssetNodes && graphAssetNodes["A_" + item.id])
-                        text = "<span class='RDSassetTreeNode'>" + text + " </span>"
-                    jstreeData.push({
-                        //  id: item.location1 + "/" + item.location2,
-                        id: item.id,
-                        text: text,
-                        parent: item.location1,
-                        data: {
-                            FunctionalLocationCode: item.location1 + "/" + item.location2,
-                            // id: item.location1 + "/" + item.location2,
-                            id: item.id,
-                            label: item.location2,
-                            type: "location",
-                        }
-
-
-                    })
-                }
-
-
-            })
-
-            return callback(null, jstreeData)
-        })
-
-    }
     self.getAssetJstreeContextMenu = function () {
 
         var items = {}
@@ -235,7 +107,8 @@ var TE_AssetDataManager = (function () {
         items.nodeInfos = {
             label: "Node infos",
             action: function (e) {// pb avec source
-                TE_AssetDataManager.showAssetNodeInfos(self.currentTreeNode)
+
+                TE_SqlTojstreeConnectors.showAssetNodeInfos(self.currentDbName, self.currentTreeNode)
             }
         }
 
@@ -254,7 +127,7 @@ var TE_AssetDataManager = (function () {
         items.addToPID = {
             label: "addToPID",
             action: function (e) {// pb avec source
-                TE_AssetDataManager.addToPID(self.currentTreeNode.data.id)
+                TE_AssetDataManager.addToPID(self.currentTreeNode)
             }
         }
         return items
@@ -277,139 +150,16 @@ var TE_AssetDataManager = (function () {
     }
 
     self.openAssetTreeNode = function (node, level, callback) {
-
-
-        var limit = 100000
-        var parentData = node.data;
-        var sqlQuery
-        if (self.currentOpenNodeQuery) {
-            sqlQuery = self.currentOpenNodeQuery + "'" + node.data.label + "'"
-        } else {
-
-            sqlQuery = " select id," +
-                "FunctionalLocationCode,functionalLocationDescription," +
-                self.currentTable_14224Field + " as mapping_14224 " +
-                " from " + TE_AssetDataManager.currenTable + " where  parentFunctionalLocation ='" + parentData.FunctionalLocationCode + "' order by className";
-            //   var sqlQuery = " select distinct tag," + self.currentTable_14224Field + " as mapping_14224 from " + self.currenTable + " where  id='"+node.data.id+"' order by tag";
-        }
-
-        self.querySQLserver(sqlQuery, function (err, result) {
+        TE_SqlTojstreeConnectors.getChildrenNodesJsTreeData(self.currentDbName, node, coloredNodesMap, function (err, jstreeData) {
             if (err)
-                return MainController.UI.message(err)
-            if (callback)
-                return callback(null, result)
-            var jstreeData = []
-            result.forEach(function (item) {
-                if (item.id == node.id)
-                    return;
-
-                var data = item;
-                data.level = level;
-                //  var childId = nodeId + "/" + common.formatUriToJqueryId(item.functionalLocationDescription)
-                if (!assetTreeDistinctNodes[item.id]) {
-                    assetTreeDistinctNodes[item.id] = 1
-                    if (item.id == "684")
-                        var x = 3
-                    var text = item[self.currentFLcolumn] + (item.FullTag || "")
-                    if (graphAssetNodes && graphAssetNodes["A_" + item.id])
-                        text = "<span class='RDSassetTreeNode'>" + text + " </span>"
-                    jstreeData.push({
-                        id: "" + item.id,
-                        text: text,
-                        parent: node.id,
-                        type: "owl:Class",
-                        data: data
-                    })
-                }
-            })
-            common.jstree.addNodesToJstree("TE_AssetConfigurator_assetPanelTreeDiv", node.id, jstreeData)
+                return alert(err)
+            if (jstreeData.length > 0)
+                common.jstree.addNodesToJstree("TE_AssetConfigurator_assetPanelTreeDiv", node.id, jstreeData)
         })
 
-
-    }
-    self.querySQLserver = function (sqlQuery, callback) {
-        var limit = 100000
-        var dataSource = {
-            "type": "sql.sqlserver",
-            "connection": "_default",
-            "dbName": self.currentDbName,
-            "table_schema": "dbo"
-        }
-
-        console.log(sqlQuery)
-
-        $.ajax({
-            type: "POST",
-            url: Config.serverUrl,
-            data: {
-                KGquery: 1,
-                getData: 1,
-                dataSource: JSON.stringify(dataSource),
-                sqlQuery: sqlQuery
-            },
-            dataType: "json",
-
-            success: function (data, textStatus, jqXHR) {
-                callback(null, data)
-            },
-            error(err) {
-                return callback(err)
-            }
-        })
     }
 
 
-    self.showAssetNodeInfos = function (node, callee) {
-        if (!node) {
-            if (callee == "Graph")
-                node = self.currentGraphNode
-            else
-                node = self.currentTreeNode;
-        }
-        if (!node)
-            return;
-
-
-        var sqlQuery = " select distinct * from " + self.currenTable + " where  id=" + node.data.id
-
-
-        self.querySQLserver(sqlQuery, function (err, data) {
-            if (err)
-                return MainController.UI.message(err)
-            var jstreeData = []
-            var nodeId = node.id
-            if (data.length == 0)
-                return
-            var headers = Object.keys(data[0])
-
-            var nodeId = data[0].tag
-            var str = "<div style='max-height:800px;overflow: auto'>" +
-                "<table class='infosTable'>"
-            str += "<tr><td class='detailsCellName'>UUID</td><td><a target='_blank' href='" + nodeId + "'>" + nodeId + "</a></td></tr>"
-            str += "<tr><td>&nbsp;</td><td>&nbsp;</td></tr>"
-
-            data.forEach(function (item) {
-
-
-                headers.forEach(function (key) {
-
-
-                    str += "<tr class='infos_table'>"
-
-                    str += "<td class='detailsCellName'>" + key + "</td>"
-
-                    str += "<td class='detailsCellValue'>" + item[key] + "</td>"
-                    str += "</tr>"
-                })
-                str += "</table>"
-
-            })
-            $("#mainDialogDiv").html(str);
-            $("#mainDialogDiv").dialog("open")
-        })
-
-
-    }
     self.loadPIDgraph = function () {
         var options = {}
         options.onclickFn = function (node, point, options) {
@@ -505,16 +255,34 @@ var TE_AssetDataManager = (function () {
     self.addToPID = function (node) {
         //   var positions = visjsGraph.network.getPositions(["PP"])
         var visjsData = {nodes: [], edges: []}
+        var shape;
+        var color;
+        var size=10
+        if (node.parents.indexOf( "Equipments")>-1) {
+            shape = "box";
+            color = "#09a4f3";
+        }
+        else if (node.parents.indexOf( "Lines")>-1) {
+            shape = "hexagon";
+            color = "#09f320";
+        }
+       else if (node.parents.indexOf(  "Instruments")>-1) {
+            shape = "dot";
+            color = "#f38209";
+        }
+
         visjsData.nodes.push({
+
 
             label: TE_AssetDataManager.getAssetNodeLabel(TE_AssetDataManager.currentTreeNode.data),
             id: "A_" + TE_AssetDataManager.currentTreeNode.id,
-            shape: "box",
-            color: "#09a4f3",
+            shape:shape,
+            color: color,
+            size:size,
             data: {id: TE_AssetDataManager.currentTreeNode.id},
             // fixed: {x: true, y: true},
-            x: 0,
-            y: 0,
+            x: 100,
+            y: 100,
 
         })
 
---
+++ public/vocables/customPlugins/TotalEnergies/TE_AssetConfigurator/TE_SqlTojstreeConnectors.js
@@ -0,0 +1,264 @@
+var TE_SqlTojstreeConnectors = (function () {
+
+    var self = {}
+
+
+    self.getJsTreeData_moho_equipments = function (parentNode, coloredNodes, callback) {
+        var sqlQuery = " select distinct  id,location1,location2 from equipments"
+        var dbName = "evolen"
+        self.querySQLserver(dbName, sqlQuery, function (err, data) {
+            if (err)
+                return MainController.UI.message(err)
+            var jstreeData = []
+            var assetTreeDistinctNodes = {}
+
+            data.forEach(function (item) {
+
+                if (!item.location1)
+                    return
+                if (!assetTreeDistinctNodes[item.location1]) {
+                    var text = item.location1
+                    if (coloredNodes && coloredNodes["A_" + item.id])
+                        text = "<span class='RDSassetTreeNode'>" + text + " </span>"
+                    assetTreeDistinctNodes[item.location1] = 1
+                    jstreeData.push({
+                        id: item.location1,
+                        text: item.location1,
+                        parent: parentNode,
+                        data: {
+                            FunctionalLocationCode: text,
+                            id: item.location1,
+                            label: item.location1,
+                            type: "equipments",
+
+                        }
+
+
+                    })
+                }
+                if (!assetTreeDistinctNodes[item.location2]) {
+                    assetTreeDistinctNodes[item.location2] = 1
+                    var text = item.location2
+                    if (coloredNodes && coloredNodes["A_" + item.id])
+                        text = "<span class='RDSassetTreeNode'>" + text + " </span>"
+                    jstreeData.push({
+                        //  id: item.location1 + "/" + item.location2,
+                        id: item.id,
+                        text: text,
+                        parent: item.location1,
+                        data: {
+                            FunctionalLocationCode: item.location1 + "/" + item.location2,
+                            // id: item.location1 + "/" + item.location2,
+                            id: item.id,
+                            label: item.location2,
+                            type: "equipments",
+                        }
+
+
+                    })
+                }
+
+
+            })
+            return callback(null, jstreeData)
+        })
+    }
+    self.getJsTreeData_moho_lines = function (parentNode, coloredNodes, callback) {
+        var sqlQuery = " select distinct LineId,Fluid from lines ";
+        var dbName = "evolen"
+        self.querySQLserver(dbName,sqlQuery, function (err, data) {
+            if (err)
+                return MainController.UI.message(err)
+            var jstreeData = []
+            var assetTreeDistinctNodes = {}
+
+            data.forEach(function (item) {
+
+                if (!item.LineId)
+                    return
+                if (!assetTreeDistinctNodes[item.LineId]) {
+                    assetTreeDistinctNodes[item.LineId] = 1
+                    var text = item.LineId
+                    if (coloredNodes && coloredNodes["A_" + item.id])
+                        text = "<span class='RDSassetTreeNode'>" + text + " </span>"
+
+                    jstreeData.push({
+                        id: item.LineId,
+                        text: item.LineId+" "+item.Fluid,
+                        parent: parentNode,
+                        data: {
+                            FunctionalLocationCode: text,
+                            id: item.LineId,
+                            label: item.LineId+" "+item.Fluid,
+                            type: "location",
+                        }
+
+
+                    })
+                }
+
+
+            })
+
+            return callback(null, jstreeData)
+        })
+
+    }
+    self.getJsTreeData_moho_instruments = function (parentNode, coloredNodes, callback) {
+        var sqlQuery = " select distinct id,TagNumber,INSTRUMENTTYPEDESCRIPTION from instruments ";
+        var dbName = "evolen"
+        self.querySQLserver(dbName,sqlQuery, function (err, data) {
+            if (err)
+                return MainController.UI.message(err)
+            var jstreeData = []
+            var assetTreeDistinctNodes = {}
+
+            data.forEach(function (item) {
+
+                if (!item.id)
+                    return
+                if (!assetTreeDistinctNodes[item.id]) {
+                    assetTreeDistinctNodes[item.LineId] = 1
+                    var text =item.TagNumber+" "+item.INSTRUMENTTYPEDESCRIPTION;
+                    if (coloredNodes && coloredNodes["A_" + item.id])
+                        text = "<span class='RDSassetTreeNode'>" + text + " </span>"
+
+                    jstreeData.push({
+                        id: item.id,
+                        text:text ,
+                        parent: parentNode,
+                        data: {
+                            FunctionalLocationCode: text,
+                            id: item.id,
+                            label: text,
+                            type: "location",
+                        }
+                    })
+                }
+            })
+            return callback(null, jstreeData)
+        })
+    }
+
+
+    self.showAssetNodeInfos = function (dbName, node, callee) {
+
+
+        var sqlQuery = " select distinct * from " + node.data.type + " where  id=" + node.data.id
+
+
+        self.querySQLserver(dbName, sqlQuery, function (err, data) {
+            if (err)
+                return MainController.UI.message(err)
+            var jstreeData = []
+            var nodeId = node.id
+            if (data.length == 0)
+                return
+            var headers = Object.keys(data[0])
+
+            var nodeId = data[0].tag
+            var str = "<div style='max-height:800px;overflow: auto'>" +
+                "<table class='infosTable'>"
+            str += "<tr><td class='detailsCellName'>UUID</td><td><a target='_blank' href='" + nodeId + "'>" + nodeId + "</a></td></tr>"
+            str += "<tr><td>&nbsp;</td><td>&nbsp;</td></tr>"
+
+            data.forEach(function (item) {
+
+
+                headers.forEach(function (key) {
+
+
+                    str += "<tr class='infos_table'>"
+
+                    str += "<td class='detailsCellName'>" + key + "</td>"
+
+                    str += "<td class='detailsCellValue'>" + item[key] + "</td>"
+                    str += "</tr>"
+                })
+                str += "</table>"
+
+            })
+            $("#mainDialogDiv").html(str);
+            $("#mainDialogDiv").dialog("open")
+        })
+
+
+    }
+
+    self.getChildrenNodesJsTreeData = function (dbName,node,coloredNodes,callback) {
+
+        if (node.data.type = "equipments") {
+            var limit = 100000
+            var parentData = node.data;
+            var  sqlQuery = "  select *  from equipments where  location2 ='" + node.data.label + "'"
+            self.querySQLserver(dbName,sqlQuery, function (err, result) {
+                if (err)
+                        return callback(null, result)
+
+                var jstreeData = []
+                var assetTreeDistinctNodes={}
+                result.forEach(function (item) {
+                    if (item.id == node.id)
+                        return;
+
+                    var data = item;
+                    //  var childId = nodeId + "/" + common.formatUriToJqueryId(item.functionalLocationDescription)
+                    if (!assetTreeDistinctNodes[item.id]) {
+                        assetTreeDistinctNodes[item.id] = 1
+
+                        var text =(item.FullTag || "")+" "+ item["location3"]
+                        if (coloredNodes && coloredNodes["A_" + item.id])
+                            text = "<span class='RDSassetTreeNode'>" + text + " </span>"
+                        jstreeData.push({
+                            id: "" + item.id,
+                            text: text,
+                            parent: node.id,
+                            type: "owl:Class",
+                            data: data
+                        })
+                    }
+                })
+             return callback(null,jstreeData)
+            })
+        }
+        return []
+
+    }
+
+
+    self.querySQLserver = function (dbName, sqlQuery, callback) {
+        var limit = 100000
+        var dataSource = {
+            "type": "sql.sqlserver",
+            "connection": "_default",
+            "dbName": dbName,
+            "table_schema": "dbo"
+        }
+
+        console.log(sqlQuery)
+
+        $.ajax({
+            type: "POST",
+            url: Config.serverUrl,
+            data: {
+                KGquery: 1,
+                getData: 1,
+                dataSource: JSON.stringify(dataSource),
+                sqlQuery: sqlQuery
+            },
+            dataType: "json",
+
+            success: function (data, textStatus, jqXHR) {
+                callback(null, data)
+            },
+            error(err) {
+                return callback(err)
+            }
+        })
+    }
+
+
+    return self;
+
+
+})()

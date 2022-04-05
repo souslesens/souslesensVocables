2be9ed1773150199387f345d4d599190a4d7ee02
yoelis <yacourt@logilab.fr>
2022-04-05 17:42:18 +0200

    chore(test): removes eslint errors

--- public/vocables/js/tools/KG/mappings/test.js
+++ public/vocables/js/tools/KG/mappings/test.js
@@ -2,7 +2,9 @@ var test = (function () {
     var self = {};
 
     self.myVar = { x: 1, y: 2 };
-    self.myFunction1 = function (param1, param2) {};
+    self.myFunction1 = function (param1, param2) {
+        // Pass
+    };
 
     self.myFunction2 = function () {
         self.myFunction1(3, 2);

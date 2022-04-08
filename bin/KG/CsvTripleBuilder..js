b4474c399d579c13d38e699a0d7f5bc1b41f33b3
Frank Bessou <frank.bessou@logilab.fr>
2022-04-08 15:17:37 +0200

    fix(CsvTripleBuilder): fix eslint

--- bin/KG/CsvTripleBuilder..js
+++ bin/KG/CsvTripleBuilder..js
@@ -63,7 +63,6 @@ var CsvTripleBuilder = {
             "rdfs:subClassOf": "uri",
             _restriction: "uri",
         };
-        var totalTriplesCount = 0;
         async.eachSeries(
             mappings,
             function (mapping, callbackEachMapping) {
@@ -168,7 +167,7 @@ var CsvTripleBuilder = {
                                         }
 
                                         if (item.lookup_s) {
-                                            var lookupValue = getLookupValue(item.lookup_s, subjectStr);
+                                            const lookupValue = getLookupValue(item.lookup_s, subjectStr);
                                             if (!lookupValue) {
                                                 console.log("missing lookup_s: " + line[item.s]);
                                             } else if (lookupValue == "badLookupName");
@@ -193,7 +192,7 @@ var CsvTripleBuilder = {
                                         } else objectStr = line[item.o];
 
                                         if (item.lookup_o) {
-                                            var lookupValue = getLookupValue(item.lookup_o, objectStr);
+                                            const lookupValue = getLookupValue(item.lookup_o, objectStr);
 
                                             if (!lookupValue) console.log("missing lookup_o: " + line[item.o]);
                                             else if (lookupValue == "badLookupName");
@@ -208,25 +207,19 @@ var CsvTripleBuilder = {
                                     //format subject
                                     {
                                         subjectStr = subjectStr.trim();
-                                        if (typeof item.s === "function") subjectStr = subjectStr;
 
                                         if (subjectStr.indexOf && subjectStr.indexOf("http") == 0) subjectStr = "<" + subjectStr + ">";
-                                        else if (subjectStr.indexOf && subjectStr.indexOf(":") > -1) subjectStr = subjectStr;
-                                        else subjectStr = "<" + graphUri + util.formatStringForTriple(subjectStr, true) + ">";
+                                        else if (subjectStr.indexOf && subjectStr.indexOf(":") > -1) {
+                                            /*pass*/
+                                        } else subjectStr = "<" + graphUri + util.formatStringForTriple(subjectStr, true) + ">";
                                     }
 
                                     //format object
                                     {
                                         objectStr = objectStr.trim();
-                                        if (!objectStr || !objectStr.indexOf) {
-                                            var x = line;
-                                            var y = item;
-                                        }
-                                        if (typeof item.o === "function") objectStr = objectStr;
-
                                         if (objectStr.indexOf && objectStr.indexOf("http") == 0) objectStr = "<" + objectStr + ">";
                                         else if (objectStr.indexOf && objectStr.indexOf(":") > -1 && objectStr.indexOf(" ") < 0) {
-                                            objectStr = objectStr;
+                                            /*pass*/
                                         } else if (propertiesTypeMap[item.p] == "string" || item.isString) objectStr = "'" + util.formatStringForTriple(objectStr, false) + "'";
                                         else objectStr = "<" + graphUri + util.formatStringForTriple(objectStr, true) + ">";
                                     }
@@ -235,12 +228,12 @@ var CsvTripleBuilder = {
                                         item.p = "<" + graphUri + util.formatStringForTriple(item.p, true) + ">";
                                     }
                                     if (item.isRestriction) {
-                                        var propStr = item.p;
+                                        let propStr = item.p;
                                         if (typeof item.p === "function") {
                                             propStr = item.p(line, item);
                                         }
                                         var blankNode = "<_:b" + util.getRandomHexaId(10) + ">";
-                                        var prop = propStr;
+                                        let prop = propStr;
                                         if (prop.indexOf("$") == 0) prop = line[prop.substring(1)];
                                         if (prop.indexOf("http") == 0) prop = "<" + prop + ">";
 
@@ -260,8 +253,6 @@ var CsvTripleBuilder = {
                                                 p: "<http://www.w3.org/2002/07/owl#someValuesFrom>",
                                                 o: objectStr,
                                             });
-                                        } else {
-                                            var x = 5;
                                         }
                                         triples.push({
                                             s: subjectStr,
@@ -270,10 +261,10 @@ var CsvTripleBuilder = {
                                         });
 
                                         if (item.inverseRestrictionProperty) {
-                                            var propStr = item.inverseRestrictionProperty;
+                                            let propStr = item.inverseRestrictionProperty;
 
-                                            var blankNode = "<_:b" + util.getRandomHexaId(10) + ">";
-                                            var prop = propStr;
+                                            let blankNode = "<_:b" + util.getRandomHexaId(10) + ">";
+                                            let prop = propStr;
                                             if (prop.indexOf("$") == 0) prop = line[prop.substring(1)];
                                             if (prop.indexOf("http") == 0) prop = "<" + prop + ">";
 
@@ -293,8 +284,6 @@ var CsvTripleBuilder = {
                                                     p: "<http://www.w3.org/2002/07/owl#someValuesFrom>",
                                                     o: subjectStr,
                                                 });
-                                            } else {
-                                                var x = 5;
                                             }
                                             triples.push({
                                                 s: objectStr,
@@ -328,11 +317,8 @@ var CsvTripleBuilder = {
                                         }
                                     }
                                 });
-
-                                var x = triples;
                             });
 
-                            var x = triples;
                             callbackSeries();
                         },
 
@@ -343,8 +329,6 @@ var CsvTripleBuilder = {
                                 return callback(null, sampleTriples);
                             }
 
-                            totalTriplesCount += triples.length;
-
                             var slices = util.sliceArray(triples, 200);
 
                             var sliceIndex = 0;
@@ -353,7 +337,6 @@ var CsvTripleBuilder = {
                                 function (slice, callbackEach) {
                                     CsvTripleBuilder.writeTriples(slice, graphUri, sparqlServerUrl, function (err, result) {
                                         if (err) {
-                                            var x = sparqlServerUrl;
                                             errors += err + " slice " + sliceIndex + "\n";
                                             return callbackEach(err);
                                         }
@@ -414,7 +397,6 @@ var CsvTripleBuilder = {
 
         httpProxy.post(sparqlServerUrl, null, params, function (err, _result) {
             if (err) {
-                var x = queryGraph;
                 return callback(err);
             }
             totalTriples += triples.length;
@@ -491,10 +473,10 @@ var CsvTripleBuilder = {
 
                     function getFunction(argsArray, fnStr, callback) {
                         try {
-                            fnStr = fnStr.replace(/[\/r\/n\/t]gm/, "");
+                            fnStr = fnStr.replace(/[/r/n/t]gm/, "");
                             var array = /\{(?<body>.*)\}/.exec(fnStr);
                             if (!array) {
-                                return callbackSeries("cannot parse object function " + JSON.stringify(item) + " missing enclosing body into 'function{..}'");
+                                return callbackSeries("cannot parse object function " + JSON.stringify(fnStr) + " missing enclosing body into 'function{..}'");
                             }
                             var fnBody = array.groups["body"];
                             var fn = new Function(argsArray, fnBody);
@@ -524,7 +506,7 @@ var CsvTripleBuilder = {
                         var fnStr = mappings.transform[key];
                         if (fnStr.indexOf("function") > -1) {
                             getFunction(["value", "role", "prop", "row"], fnStr, function (err, fn) {
-                                if (err) return callbackSeries(err + " in mapping" + JSON.stringify(item));
+                                if (err) return callbackSeries(err + " in mapping" + JSON.stringify(fnStr));
                                 mappings.transform[key] = fn;
                             });
                         }
@@ -564,11 +546,13 @@ var CsvTripleBuilder = {
 };
 
 module.exports = CsvTripleBuilder;
-
+// eslint-disable-next-line no-constant-condition
 if (false) {
     var options = {
         deleteOldGraph: true,
         sampleSize: 500,
     };
-    CsvTripleBuilder.createTriplesFromCsv("D:\\webstorm\\souslesensVocables\\data\\CSV\\CFIHOS_V1.5_RDL", "CFIHOS tag class v1.5.csv.json", options, function (_err, _result) {});
+    CsvTripleBuilder.createTriplesFromCsv("D:\\webstorm\\souslesensVocables\\data\\CSV\\CFIHOS_V1.5_RDL", "CFIHOS tag class v1.5.csv.json", options, function (_err, _result) {
+        /*pass*/
+    });
 }

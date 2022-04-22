2e5b6308b1e4053913becfe511598d69fbfac187
Frank Bessou <frank.bessou@logilab.fr>
2022-04-22 09:52:01 +0200

    fix: add source in users returned by api

--- model/users.js
+++ model/users.js
@@ -2,6 +2,18 @@ const path = require("path");
 const fs = require("fs");
 const usersJSON = path.resolve("config/users/users.json");
 
+/**
+ * @typedef {Object} UserAccount
+ * @prop {string} id
+ * @prop {string} login
+ * @prop {string[]} groups
+ * @prop {string} source
+ * @prop {"user"} _type
+ */
+
+/**
+ * @returns {Promise<Record<string, UserAccount>>} a collection of UserAccount
+ */
 const getUsers = async () => {
     const data = await fs.promises.readFile(usersJSON);
     const users = {};
@@ -10,6 +22,7 @@ const getUsers = async () => {
             id: value.id,
             login: value.login,
             groups: value.groups,
+            source: value.source,
             _type: value._type,
         };
     });

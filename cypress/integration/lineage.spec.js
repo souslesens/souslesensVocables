c620c93679d9e8be6b8b00ed18ab86f7cbf78f26
xgaia <xgaia@gmx.com>
2022-05-03 14:42:51 +0200

    style: prettier

--- cypress/integration/lineage.spec.js
+++ cypress/integration/lineage.spec.js
@@ -3,15 +3,15 @@ describe("Lineage", function () {
         cy.viewport(960, 630);
 
         // Login as admin
-        cy.visit('http://localhost:3011/login');
-        cy.get('#username').click();
-        cy.get('#username').type('admin');
-        cy.get('#password').type('admin');
-        cy.get('.btn').click();
-        cy.url().should('contains', 'http://localhost:3011/vocables/');
+        cy.visit("http://localhost:3011/login");
+        cy.get("#username").click();
+        cy.get("#username").type("admin");
+        cy.get("#password").type("admin");
+        cy.get(".btn").click();
+        cy.url().should("contains", "http://localhost:3011/vocables/");
 
         // test BFO source
-        cy.get('#lineage_anchor').click();
-        cy.get('#BFO_anchor').click();
+        cy.get("#lineage_anchor").click();
+        cy.get("#BFO_anchor").click();
     });
 });

711b8bf99c168c964deaab0b27faa3f2957ee564
xgaia <xgaia@gmx.com>
2022-05-04 16:08:11 +0200

    ci: test SPARQL endpoint

--- cypress/integration/sparql_endpoint.spec.js
+++ cypress/integration/sparql_endpoint.spec.js
@@ -1,15 +1,47 @@
-describe("Sparql Endpoint test suit", function () {
+describe("SPARQL endpoint test suit", function () {
     afterEach(() => {
         cy.logout();
     });
 
-    it("When I logged as an admin and when I click on genealogy, choose a source, and compare with a term ", function () {
-        cy.login("admin");
+    context("Given I'm an admin", function () {
+        beforeEach(() => {
+            cy.login("admin");
+        });
 
-        cy.get("#SPARQL").click();
+        it("when I click SPARQL endpoint, I can perform a sparql query", () => {
+            cy.visit('http://localhost:3011/vocables/');
+            cy.get('#SPARQL_anchor').click();
+            cy.get('#BFO_anchor').click();
+            cy.get('#queryIcon').click();
+            cy.get('table[id="DataTables_Table_0"]')
 
-        // cy.on("uncaught:exception", () => {
-        //     return false;
-        // });
+        });
+    });
+
+    context("Given I'm an owl_user", function () {
+        beforeEach(() => {
+            cy.login("owl_user");
+        });
+
+        it("when I click SPARQL endpoint, I can perform a sparql query", () => {
+            cy.visit('http://localhost:3011/vocables/');
+            cy.get('#SPARQL_anchor').click();
+            cy.get('#BFO_anchor').click();
+            cy.get('#queryIcon').click();
+            cy.get('table[id="DataTables_Table_0"]')
+        });
+    });
+
+    context("Given I'm a skos_user", function () {
+        beforeEach(() => {
+            cy.login("skos_user");
+        });
+
+        it("when I click genealogy, it can't display the BFO source", () => {
+            cy.visit('http://localhost:3011/vocables/');
+            cy.get('#SPARQL_anchor').click();
+            cy.get("#BFO_anchor").should("not.exist");
+            cy.get('table[id="DataTables_Table_0"]').should("not.exist");
+        });
     });
 });

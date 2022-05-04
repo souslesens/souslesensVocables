cc54559dfc4fe393f1034b39454ace1a458fb206
yoelis <yacourt@logilab.fr>
2022-05-04 16:39:12 +0200

    fix(model.users.test): add passowrd field to assert equality

--- tests/model.users.test.js
+++ tests/model.users.test.js
@@ -18,11 +18,13 @@ describe("UserModel", () => {
                 login: "admin",
                 groups: ["admin"],
                 source: "json",
+                password: "$2b$10$ed6ZDHUbN7NfOGrufiDniudK9EMLCFg9qFgJ0N/T.nRTBPlA5Mm9C",
                 _type: "user",
             },
             owl_user: {
                 id: "owl_user",
                 login: "owl_user",
+                password: "$2b$10$ed6ZDHUbN7NfOGrufiDniudK9EMLCFg9qFgJ0N/T.nRTBPlA5Mm9C",
                 groups: ["owl_only"],
                 source: "json",
                 _type: "user",
@@ -30,6 +32,7 @@ describe("UserModel", () => {
             skos_user: {
                 id: "skos_user",
                 login: "skos_user",
+                password: "$2b$10$ed6ZDHUbN7NfOGrufiDniudK9EMLCFg9qFgJ0N/T.nRTBPlA5Mm9C",
                 groups: ["skos_only"],
                 source: "json",
                 _type: "user",

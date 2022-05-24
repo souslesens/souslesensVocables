a9864cfa982f8d7156e308f4246a3927f4bc9ef2
yoelis <yacourt@logilab.fr>
2022-05-24 11:31:49 +0200

    fix(cypress): fix flaky tests

--- cypress/integration/lineage.spec.js
+++ cypress/integration/lineage.spec.js
@@ -10,16 +10,19 @@ describe("Lineage", function () {
         });
         it("When I log in as admin and when I click lineage and click BFO, a graph pops up", function () {
             cy.get("#lineage_anchor").click();
+            cy.get("#ui-id-2").as("tools-panel");
+            cy.get("@tools-panel").should("have.css", "display", "none");
             cy.get("#BFO_anchor").click();
         });
     });
-
     context("Given I'm an owl_user", function () {
         beforeEach(() => {
             cy.login("owl_user");
         });
         it("When I click lineage and click BFO, a graph pops up", function () {
             cy.get("#lineage_anchor").click();
+            cy.get("#ui-id-2").as("tools-panel");
+            cy.get("@tools-panel").should("have.css", "display", "none");
             cy.get("#BFO_anchor").click();
         });
     });

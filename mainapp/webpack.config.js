33de609b738887d52361dbef805f41eb8b6d2640
xgaia <xgaia@gmx.com>
2022-08-22 09:28:58 +0200

    chore: improve webpack config

--- mainapp/webpack.config.js
+++ mainapp/webpack.config.js
@@ -1,56 +1,11 @@
 const path = require("path");
 const mode = process.env.NODE_ENV ? process.env.NODE_ENV : "production";
-const TerserPlugin = require("terser-webpack-plugin");
 
 // config editor
 module.exports = {
     mode: mode,
     entry: {
         mainapp: "./src/index.tsx",
-    },
-    output: {
-        path: path.resolve(__dirname, "static/"),
-    },
-    devtool: "source-map",
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json"],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "ts-loader",
-            },
-            {
-                test: /\.(ttf|eot|svg|woff(2)?|png|jpe?g|gif)(\?[a-z0-9=&.]+)?$/,
-                loader: "file-loader",
-            },
-            { test: /\.css$/, loader: "css-loader" },
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
-        ],
-    },
-    // don't generate License file
-    optimization: {
-        minimizer: [
-            new TerserPlugin({
-                extractComments: false,
-            }),
-        ],
-    },
-    devServer: {
-        inline: false,
-        contentBase: "build",
-        compress: true,
-        historyApiFallback: {
-            index: "index.html",
-        },
-    },
-};
-
-// kg-upload-app
-module.exports = {
-    mode: mode,
-    entry: {
         kg_upload_app: "./src/kg-upload-app.tsx",
     },
     output: {
@@ -72,14 +27,6 @@ module.exports = {
             },
             { test: /\.css$/, loader: "css-loader" },
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
-        ],
-    },
-    // don't generate License file
-    optimization: {
-        minimizer: [
-            new TerserPlugin({
-                extractComments: false,
-            }),
         ],
     },
     devServer: {

ee64a8ac9455633e2d46ce148bdc3facbf7aa3da
xgaia <xgaia@gmx.com>
2022-10-12 15:43:40 +0200

    feat: add script to get default graph

---
+++ scripts/get_default_local_graphs.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const sourcesTemplatePath = "config_templates/sources.json.default";
+fs.readFile(sourcesTemplatePath, (_err, data) => {
+    const sources = JSON.parse(data)
+    const sourcesList = Object.entries(sources)
+    const graphList = sourcesList.filter(([_name, source]) => {
+      if (source.sparql_server.url === "_default") {
+        return [_name, source]
+      }
+    }).forEach(([name, source]) => {
+      console.log(source.graphUri)
+    })
+});
+
+
+
+

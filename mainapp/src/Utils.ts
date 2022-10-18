cc6d07cfda9860124d27d8332b577978c76247d1
xgaia <xgaia@gmx.com>
2022-10-18 17:01:31 +0200

    feat: wider modal

--- mainapp/src/Utils.ts
+++ mainapp/src/Utils.ts
@@ -17,7 +17,7 @@ const style = {
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: 800,
+    width: 900,
     height: "auto",
     bgcolor: "background.paper",
     border: "2px solid #000",

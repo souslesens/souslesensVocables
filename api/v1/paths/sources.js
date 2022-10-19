e754642e7cc22c18f7acdd01180cc65ffff672f3
xgaia <xgaia@gmx.com>
2022-10-19 16:58:11 +0200

    fix: put sources

--- api/v1/paths/sources.js
+++ api/v1/paths/sources.js
@@ -3,59 +3,15 @@ const fs = require("fs");
 const { configPath, config } = require("../../../model/config");
 const sourcesJSON = path.resolve(configPath + "/sources.json");
 const profilesJSON = path.resolve(configPath + "/profiles.json");
-exports.profilesJSON = sourcesJSON;
 const util = require("util");
-const { readResource, writeResource, resourceCreated, resourceUpdated, responseSchema, resourceFetched } = require("./utils");
+const { readResource, writeResource, resourceCreated, responseSchema, resourceFetched } = require("./utils");
 const userManager = require(path.resolve("bin/user."));
 const read = util.promisify(fs.readFile);
+const { getAllowedSources, filterSources } = require("./utils.js");
 module.exports = function () {
     let operations = {
         GET,
         POST,
-        PUT,
-    };
-
-    const getAllowedSources = (user, profiles, sources) => {
-        const aProfiles = Object.entries(profiles);
-        const aSources = Object.entries(sources);
-
-        // get the formalOntologySourceLabel source
-        const formalOntologySourceLabel = config.formalOntologySourceLabel;
-        console.log(formalOntologySourceLabel);
-
-        // for all profile
-        const allAccessControl = aProfiles.flatMap(([_k, profile]) => {
-            const defaultSourceAccessControl = profile.defaultSourceAccessControl;
-            const sourcesAccessControl = profile.sourcesAccessControl;
-            const allowedSourceSchemas = profile.allowedSourceSchemas;
-            // browse all sources, filter allowedSourceSchemas and get accessControl
-            return aSources
-                .filter(([sourceName, source]) => {
-                    if (allowedSourceSchemas.includes(source.schemaType)) {
-                        return [sourceName, source];
-                    }
-                })
-                .map(([sourceName, _v]) => {
-                    if (sourceName in sourcesAccessControl) {
-                        return [sourceName, sourcesAccessControl[sourceName]];
-                    } else {
-                        return [sourceName, defaultSourceAccessControl];
-                    }
-                });
-        });
-
-        // get all read or readwrite source
-        const allowedSources = allAccessControl
-            .filter(([sourceName, accessControl]) => {
-                if (["read", "readwrite"].includes(accessControl)) {
-                    return sourceName;
-                }
-            })
-            .map(([sourceName, _v]) => sourceName)
-            .concat([formalOntologySourceLabel, "read"]);
-
-        // uniq
-        return Array.from(new Set(allowedSources));
     };
 
     ///// GET api/v1/sources
@@ -70,7 +26,7 @@ module.exports = function () {
             } else {
                 const profiles = await read(profilesJSON);
                 const parsedProfiles = JSON.parse(profiles);
-                const allowedSources = getAllowedSources(userInfo.user, parsedProfiles, parsedSources);
+                const allowedSources = getAllowedSources(userInfo.user, parsedProfiles, parsedSources, config.formalOntologySourceLabel);
                 const filteredSources = filterSources(allowedSources, parsedSources);
                 resourceFetched(res, filteredSources);
             }
@@ -83,32 +39,6 @@ module.exports = function () {
         security: [{ loginScheme: [] }],
         operationId: "getSources",
         responses: responseSchema("Sources", "GET"),
-    };
-
-    ///// PUT api/v1/sources
-    async function PUT(req, res, next) {
-        try {
-            const updatedSource = req.body;
-            const profiles = await read(profilesJSON).then((p) => JSON.parse(p));
-            const userInfo = await userManager.getUser(req.user);
-            const allowedSources = getAllowedSources(userInfo.user, profiles);
-            const oldSources = await readResource(sourcesJSON, res);
-            const updatedSources = { ...oldSources, ...updatedSource };
-            if (Object.keys(oldSources).includes(Object.keys(req.body)[0])) {
-                resourceUpdated(res, filterSources(allowedSources, updatedSources));
-            } else {
-                res.status(400).json({ message: "Resource does not exist. If you want to create another resource, use POST instead." });
-            }
-        } catch (err) {
-            next(err);
-        }
-    }
-    PUT.apiDoc = {
-        summary: "Update Sources",
-        security: [{ restrictAdmin: [] }],
-        operationId: "updateSources",
-        parameters: [],
-        responses: responseSchema("Sources", "PUT"),
     };
 
     ///// POST api/v1/sources
@@ -140,7 +70,3 @@ module.exports = function () {
 
     return operations;
 };
-
-function filterSources(allowedSources, sources) {
-    return Object.fromEntries(Object.entries(sources).filter(([sourceId, _source]) => allowedSources.includes(sourceId)));
-}
--- api/v1/paths/sources/{id}.js
+++ api/v1/paths/sources/{id}.js
@@ -1,8 +1,9 @@
 const path = require("path");
 const fs = require("fs");
-const { configPath, config } = require("../../../../model/config");
-const profilesJSON = path.resolve(configPath + "/sources.json");
+const { configPath } = require("../../../../model/config");
+const sourcesJSON = path.resolve(configPath + "/sources.json");
 const util = require("util");
+const { readResource, resourceUpdated } = require("../utils");
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
@@ -10,41 +11,42 @@ module.exports = function () {
     let operations = {
         GET,
         DELETE,
+        PUT,
     };
 
     function GET(req, res, _next) {
-        fs.readFile(profilesJSON, "utf8", (err, data) => {
+        fs.readFile(sourcesJSON, "utf8", (err, data) => {
             if (err) {
                 res.status(500).json({ message: "I couldn't read profiles.json" });
             } else {
-                const profiles = JSON.parse(data);
-                const profile = profiles[req.params.id];
+                const sources = JSON.parse(data);
+                const source = sources[req.params.id];
                 req.params.id
-                    ? profile
-                        ? res.status(200).json(profiles[req.params.id])
-                        : res.status(400).json({ message: `Profile with id ${req.params.id} not found` })
-                    : res.status(200).json(profiles);
+                    ? source
+                        ? res.status(200).json(sources[req.params.id])
+                        : res.status(400).json({ message: `Source with id ${req.params.id} not found` })
+                    : res.status(200).json(sources);
             }
         });
     }
     async function DELETE(req, res, _next) {
-        const profiles = await readFile(profilesJSON).catch((err) => res.status(500).json(err));
-        const oldProfiles = JSON.parse(profiles);
-        const { [req.params.id]: idToDelete, ...remainingProfiles } = oldProfiles;
-        const successfullyDeleted = JSON.stringify(remainingProfiles) !== JSON.stringify(oldProfiles);
+        const sources = await readFile(sourcesJSON).catch((err) => res.status(500).json(err));
+        const oldSources = JSON.parse(sources);
+        const { [req.params.id]: idToDelete, ...remainingSources } = oldSources;
+        const successfullyDeleted = JSON.stringify(remainingSources) !== JSON.stringify(oldSources);
 
         if (req.params.id && successfullyDeleted) {
-            await writeFile(profilesJSON, JSON.stringify(remainingProfiles)).catch((err) =>
+            await writeFile(sourcesJSON, JSON.stringify(remainingSources)).catch((err) =>
                 res.status(500).json({
-                    message: "I couldn't write profiles.json",
+                    message: "I couldn't write sources.json",
                     error: err,
                 })
             );
 
-            const updatedProfiles = await readFile(profilesJSON).catch((_err) => res.status(500).json({ message: "Couldn't read profiles json" }));
+            const updatedSources = await readFile(sourcesJSON).catch((_err) => res.status(500).json({ message: "Couldn't read sources json" }));
             res.status(200).json({
                 message: `${req.params.id} successfully deleted`,
-                resources: JSON.parse(updatedProfiles),
+                resources: JSON.parse(updatedSources),
             });
         } else if (!req.params.id) {
             res.status(500).json({ message: "I need a resource ID to perform this request" });
@@ -52,6 +54,30 @@ module.exports = function () {
             res.status(500).json({ message: `I couldn't delete resource ${req.params.id}. Maybe it has been deleted already?` });
         }
     }
+
+    async function PUT(req, res, next) {
+        try {
+            const updatedSource = req.body;
+            const oldSources = await readResource(sourcesJSON, res);
+            if (req.params.id in oldSources) {
+                if (req.params.id == updatedSource.name) {
+                    const updatedSources = { ...oldSources };
+                    updatedSources[req.params.id] = updatedSource;
+                    await writeFile(sourcesJSON, JSON.stringify(updatedSources, null, 2)).catch((_err) => {
+                        res.status(500).json({ message: "I couldn't write sources.json" });
+                    });
+                    resourceUpdated(res, updatedSource);
+                } else {
+                    res.status(400).json({ message: "Id and name are different." });
+                }
+            } else {
+                res.status(400).json({ message: "Resource does not exist. If you want to create another resource, use POST instead." });
+            }
+        } catch (err) {
+            next(err);
+        }
+    }
+
     GET.apiDoc = {
         summary: "This resource returns profiles list or a profile if an id is provided",
         operationId: "getProfiles",
@@ -70,6 +96,20 @@ module.exports = function () {
         summary: "Delete a specific source",
         security: [{ restrictAdmin: [] }],
         operationId: "DeleteOneSource",
+        parameters: [],
+        responses: {
+            200: {
+                description: "Sources",
+                schema: {
+                    $ref: "#/definitions/Source",
+                },
+            },
+        },
+    };
+    PUT.apiDoc = {
+        summary: "Update Sources",
+        security: [{ restrictAdmin: [] }],
+        operationId: "updateSources",
         parameters: [],
         responses: {
             200: {

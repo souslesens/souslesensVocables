0ff9640a2de7589114d527870515e40950c30c13
claudefauconnet <claude.fauconnet@neuf.fr>
2023-03-08 08:51:46 +0100

    corrected queryDialog suite

--- public/vocables/js/tools/lineage/lineage_query.js
+++ public/vocables/js/tools/lineage/lineage_query.js
@@ -32,58 +32,20 @@ var Lineage_query = (function() {
       $("#mainDialogDiv").load("snippets/lineage/relationsDialogFilter.html", function() {
 
         var propStr = self.currentProperty.vocabulary + "." + self.currentProperty.label + "<br>";
-        var domainStr = "any";
-        var rangeStr = "any";
-        var domainValue = "";
-        var rangeValue = "";
-        var domain = null;
-        var range = null;
+        self.domainStr = "any";
+        self.rangeStr = "any";
+        self.domainValue = "";
+        self.rangeValue = "";
+        self.domain = null;
+        self.range = null;
 
 
-        if (Config.basicVocabGraphs[self.currentProperty.vocabulary]) {
-          var constraints = Config.basicVocabGraphs[self.currentProperty.vocabulary].constraints;
-
-
-          if (constraints) {
-            if (constraints[self.currentProperty.id]) {
-              domain = constraints[self.currentProperty.id].domain;
-              range = constraints[self.currentProperty.id].range;
-              domainStr = domain || "any";
-              rangeStr = range || "any";
-            }
-
-          }
-
-
-        }
-        /*  var html = "<table>" +
-            "<tr>" +
-            "<td>Subject</td>" +
-            "<td>" + domainStr + "</td>" +
-            "<td>  <select id='Lineage_relation_domainTypeSelect' onchange='Lineage_relations.onFilterObjectTypeSelect(\"domain\",$(this).val())'></select>"+ domainValue + "</td>" +
-            "</tr>" +
-            "<tr>" +
-            "<td>Property</td>" +
-           "<td>" + propStr + "</td>" +
-            "<td>" + "" + "</td>" +
-
-            "</tr>" +
-            "<tr>" +
-            "<td>Object</td>" +
-            "<td>" + rangeStr + "</td>" +
-            "<td> <select id='Lineage_relation_rangeTypeSelect' onchange='Lineage_relations.onFilterObjectTypeSelect(\"range\",$(this).val())'></select>" + rangeValue + "</td>" +
-            "</tr>" +
-            "</table>";
-
-          $("#mainDialogDiv").html(html);*/
-
-
-        var ObjectsTypesMap = {
-          any: ["String", "Date", "Number", "Class", "Bag", "NamedIndividual", "ObjectProperty", "DataTypeProperty"],
+        self.ObjectsTypesMap = {
+          any: ["String", "Date", "Number", "owl:Class", "rdf:Bag", "owl:NamedIndividual", "owl:ObjectProperty", "owl:DataTypeProperty"],
           Literal: ["String", "Date", "Number"],
-          Resource: ["Class", "Bag", "NamedIndividual"],
-          Class: ["Class"],
-          Property: ["ObjectProperty", "DataTypeProperty"]
+          Resource: ["owl:Class", "rdf:Bag", "owl:NamedIndividual"],
+          Class: ["owl:Class"],
+          Property: ["owl:ObjectProperty", "owl:DataTypeProperty"]
         };
 
         self.operators = {
@@ -92,45 +54,85 @@ var Lineage_query = (function() {
 
         };
 
+        if (Config.basicVocabGraphs[self.currentProperty.vocabulary]) {
+          var constraints = Config.basicVocabGraphs[self.currentProperty.vocabulary].constraints;
+
+
+          if (constraints) {
+            if (constraints[self.currentProperty.id]) {
+              self.domain = constraints[self.currentProperty.id].domain;
+              self.range = constraints[self.currentProperty.id].range;
+              self.domainStr = self.domain || "any";
+              self.rangeStr = self.range || "any";
+            }
+
+          }
+
+
+        }
         $("#Lineage_relation_property").html(propStr);
 
-        var types = Object.keys(ObjectsTypesMap);
-        if (domain) {
-          for (var type in ObjectsTypesMap) {
-            if (domain.indexOf(type) > -1) {
-              common.fillSelectOptions("Lineage_relation_domainTypeSelect", ObjectsTypesMap[type], true);
+
+        if (false) {
+          var types = Object.keys(self.ObjectsTypesMap);
+          if (self.domain) {
+            for (var type in self.ObjectsTypesMap) {
+              if (domain.indexOf(type) > -1) {
+                common.fillSelectOptions("Lineage_relation_filterTypeSelect", self.ObjectsTypesMap[type], true);
+              }
             }
           }
-        }
-        if (range) {
-          for (var type in ObjectsTypesMap) {
-            if (range.indexOf(type) > -1) {
-              common.fillSelectOptions("Lineage_relation_rangeTypeSelect", ObjectsTypesMap[type], true);
+          if (self.range) {
+            for (var type in ObjectsTypesMap) {
+              if (range.indexOf(type) > -1) {
+                common.fillSelectOptions("Lineage_relation_rangeTypeSelect", self.ObjectsTypesMap[type], true);
+              }
             }
           }
-        }
-        else {
-          common.fillSelectOptions("Lineage_relation_rangeTypeSelect", ObjectsTypesMap["any"], true);
+          else {
+            common.fillSelectOptions("Lineage_relation_rangeTypeSelect", self.ObjectsTypesMap["any"], true);
+          }
         }
       });
 
     };
+
+
+  self.onSelectRoleType = function(role) {
+    self.currentFilterRole = role;
+    $("#lineage_relation_filterRole").html(role);
+
+    var types = Object.keys(self.ObjectsTypesMap);
+
+    for (var type in self.ObjectsTypesMap) {
+      if ((role = "subject" && self.domain.indexOf(type) > -1) || (role = "object" && self.range.indexOf(type) > -1)) {
+        common.fillSelectOptions("Lineage_relation_filterTypeSelect", self.ObjectsTypesMap[type], true);
+      }
+    }
+
+  };
+
+
   self.onSelectResourceType = function(role, type) {
     if (type == "String") {
       common.fillSelectOptions("lineageQuery_operator", self.operators["String"]);
       $("#lineageQuery_valueDiv").css("display", "block");
     }
     else {
-      var scopes = ["whiteBoardNodes", Lineage_sources.activeSource];
+      var scopes;
+      if (visjsGraph.isGraphNotEmpty) {
+        scopes.push("whiteBoardNodes");
+      }
+      scopes.push(Lineage_sources.activeSource);
       var imports = Config.sources[Lineage_sources.activeSource].imports;
       if (imports) {
         scopes = scopes.concat(imports);
       }
       if (role == "domain") {
-        common.fillSelectOptions("Lineage_relation_domainVocabularySelect", scopes, true);
+        common.fillSelectOptions("Lineage_relation_filterVocabularySelect", scopes, true);
       }
       else if (role == "range") {
-        common.fillSelectOptions("Lineage_relation_rangesVocabularySelect",scopes, true);
+        common.fillSelectOptions("Lineage_relation_rangesVocabularySelect", scopes, true);
       }
 
 
@@ -141,29 +143,49 @@ var Lineage_query = (function() {
 
 
   self.onSelectResource = function(role, type) {
-    if (role == "domain") {
-      var resourceType = $("#Lineage_relation_domainTypeSelect").val()
-      if (type == "whiteBoardNodes") {
+
+    var resourceType = $("#Lineage_relation_filterTypeSelect").val();
+    if (type == "whiteBoardNodes") {
+    }
+    else {
+      if (resourceType == "owl:Class") {
+        KGcreator.fillObjectOptionsFromPrompt("owl:Class", "Lineage_relation_filterResourcesSelect", type);
       }
-      else {
-        if (resourceType == "Class") {
-          KGcreator.fillObjectOptionsFromPrompt("owl:Class", "Lineage_relation_domainResourcesSelect",type)
-        }
+    }
+
+
+  };
+
+  self.attachFilterToProperty = function() {
+
+    var role = self.currentFilterRole;
+    var resourceType = $("#Lineage_relation_filterTypeSelect").val();
+    var resource = $("#Lineage_relation_filterResourcesSelect").val();
+
+    var filter = {};
+    if (resourceType == "whiteBoardNodes") {
+      var nodeIds = visjsGraph.data.nodes.getIds();
+      if (role == "subject") {
+        filter.subjectIds = nodeIds;
       }
+      else if (role == "object") {
+        filter.objectIds = nodeIds;
+      }
+    }
+    else { if (resourceType == "String") {
+      filter.filterStr="FILTER "
+    }else{
+      filter.filterStr="FILTER "
+      
+      
+      
+    }
+
 
     }
-    else if (role == "range") {
-      var resourceType = $("#Lineage_relation_rangeTypeSelect").val()
-      if (type == "whiteBoardNodes") {
-      }
-      else {
-        if (resourceType == "Class") {
-          KGcreator.fillObjectOptionsFromPrompt("owl:Class", "Lineage_relation_rangeResourcesSelect",type)
-        }
-      }
 
-    }
-  }
+
+  };
 
   return self;
 })();

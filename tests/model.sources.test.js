305fd76e2115309320b5583fb239c132eab1f107
xgaia <xgaia@gmx.com>
2023-04-12 12:54:26 +0200

    feat(model): getAllowedSources

--- tests/model.sources.test.js
+++ tests/model.sources.test.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const { SourceModel } = require("../model/sources");
+const { ProfileModel } = require("../model/profiles");
 const path = require("path");
 
 describe("SourceModel", () => {
@@ -8,8 +9,10 @@ describe("SourceModel", () => {
      */
 
     let sourceModel;
-    beforeAll(() => {
-        sourceModel = new SourceModel(path.join(__dirname, "data/config/sources.json"));
+    let sourcesFromFiles;
+    beforeAll(async () => {
+        sourceModel = new SourceModel(path.join(__dirname, "data/config/sources.json"), path.join(__dirname, "data/config/profiles.json"));
+        sourcesFromFiles = await fs.promises.readFile(path.join(__dirname, "data/config/sources.json")).then((data) => JSON.parse(data.toString()));
     });
 
     test("Can create instance", async () => {
@@ -21,11 +24,34 @@ describe("SourceModel", () => {
             login: "admin",
         };
         const sources = await sourceModel.getUserSources(admin);
-        const sourcesFromFiles = await fs.promises.readFile(path.join(__dirname, "data/config/sources.json")).then((data) => JSON.parse(data.toString()));
         const expectedResult = Object.fromEntries(
             Object.entries(sourcesFromFiles).map(([id, source]) => {
                 source.accessControl = "readwrite";
                 return [id, source];
+            })
+        );
+        expect(sources).toStrictEqual(expectedResult);
+    });
+    test("User in all_forbidden profile can't get any sources", async () => {
+        const jdoe = {
+            login: "jdoe",
+            groups: ["all_forbidden"],
+        };
+        const sources = await sourceModel.getUserSources(jdoe);
+        expect(sources).toStrictEqual({});
+    });
+    test("User in read_folder_1 profile can get 2 sources", async () => {
+        const jdoe = {
+            login: "jdoe",
+            groups: ["read_folder_1"],
+        };
+        const sources = await sourceModel.getUserSources(jdoe);
+        const expectedResult = Object.fromEntries(
+            Object.entries(sourcesFromFiles).filter(([id, source]) => {
+                if (["SOURCE_1", "SOURCE_2"].includes(id)) {
+                    source.accessControl = "read";
+                    return [id, source];
+                }
             })
         );
         expect(sources).toStrictEqual(expectedResult);

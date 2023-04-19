b14cd434ae45d62b80ba16480b38f2cefe5ab890
xgaia <xgaia@gmx.com>
2023-04-19 11:13:13 +0200

    feat(model): getUserProfiles

--- api/v1/paths/profiles.js
+++ api/v1/paths/profiles.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const ulid = require("ulid");
 const { configPath } = require("../../../model/config");
+const { profileModel } = require("../../../model/profiles");
 const profilesJSON = path.resolve(configPath + "/profiles.json");
 exports.profilesJSON = profilesJSON;
 const { sortObjectByKey, readResource, writeResource, resourceFetched, resourceUpdated, responseSchema, resourceCreated } = require("./utils");
@@ -12,56 +13,12 @@ module.exports = function () {
         POST,
     };
 
-    function parseAccessControl(accessControlJson) {
-        const accessControl = accessControlJson.toLowerCase();
-        if (!["forbidden", "read", "readwrite"].includes(accessControl)) {
-            throw new Error("Invalid SourcesAccessControl");
-        }
-        return accessControl;
-    }
-
     ///// GET api/v1/profiles
     async function GET(req, res, next) {
         try {
-            let profiles = await readResource(profilesJSON, res);
-            profiles = Object.fromEntries(
-                Object.entries(profiles).map(([profileId, profile]) => {
-                    profile.sourcesAccessControl = Object.fromEntries(
-                        Object.entries(profile.sourcesAccessControl).map(([sourceId, accessControl]) => {
-                            return [sourceId, parseAccessControl(accessControl)];
-                        })
-                    );
-                    return [profileId, profile];
-                })
-            );
-            const sortedProfiles = sortObjectByKey(profiles);
-            const currentUser = await userManager.getUser(req.user);
-            const groups = currentUser.user.groups;
-            if (groups.includes("admin")) {
-                // return an admin profile with all right
-                adminProfile = {
-                    name: "admin",
-                    _type: "profile",
-                    id: "admin",
-                    allowedSourceSchemas: ["OWL", "SKOS"],
-                    defaultSourceAccessControl: "readwrite",
-                    sourcesAccessControl: {},
-                    allowedTools: "ALL",
-                    forbiddenTools: [],
-                    blender: {
-                        contextMenuActionStartLevel: 0,
-                    },
-                };
-                const sortedProfilesWithAdmin = sortObjectByKey({ ...sortedProfiles, admin: adminProfile });
-                resourceFetched(res, sortedProfilesWithAdmin);
-            } else {
-                const sortedUserProfiles = Object.fromEntries(
-                    Object.entries(sortedProfiles).filter(([profileId, _profile]) => {
-                        return groups.includes(profileId);
-                    })
-                );
-                resourceFetched(res, sortedUserProfiles);
-            }
+            const userInfo = await userManager.getUser(req.user);
+            const profiles = await profileModel.getUserProfiles(userInfo.user);
+            resourceFetched(res, profiles);
         } catch (error) {
             next(error);
         }
--- model/profiles.js
+++ model/profiles.js
@@ -12,6 +12,53 @@ class ProfileModel {
     _write = async (profiles) => {
         await fs.promises.writeFile(this.configProfilesPath, JSON.stringify(profiles, null, 2));
     };
+
+    _sortProfiles = async (profiles) => {
+        return Object.fromEntries(
+            Object.entries(profiles).sort((a, b) => {
+                return a[0].localeCompare(b[0]);
+            })
+        );
+    };
+
+    getAllProfiles = async () => {
+        return await this._sortProfiles(await this._read());
+    };
+
+    _getAdminProfiles = async (profiles) => {
+        const adminProfile = {
+            name: "admin",
+            _type: "profile",
+            id: "admin",
+            allowedSourceSchemas: ["OWL", "SKOS"],
+            defaultSourceAccessControl: "readwrite",
+            sourcesAccessControl: {},
+            allowedTools: "ALL",
+            forbiddenTools: [],
+            blender: {
+                contextMenuActionStartLevel: 0,
+            },
+        };
+
+        return await this._sortProfiles({ ...profiles, admin: adminProfile });
+    };
+
+    _getAllowedProfiles = async (profiles, user) => {
+        const userProfiles = Object.fromEntries(
+            Object.entries(profiles).filter(([profileName, _profile]) => {
+                return user.groups.includes(profileName);
+            })
+        );
+        return await this._sortProfiles(userProfiles);
+    };
+
+    getUserProfiles = async (user) => {
+        const allProfiles = await this._read();
+        if (user.login === "admin" || user.groups.includes("admin")) {
+            return await this._getAdminProfiles(allProfiles);
+        }
+        return await this._getAllowedProfiles(allProfiles, user);
+    };
 }
 
 const profileModel = new ProfileModel(configProfilesPath);

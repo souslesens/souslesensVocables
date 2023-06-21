111777fec1475dd0e484843053f2388d47a6cb52
claudefauconnet <claude.fauconnet@neuf.fr>
2023-06-21 08:52:59 +0200

    npm run format

--- public/vocables/modules/tools/lineage/lineage_axioms_draw.js
+++ public/vocables/modules/tools/lineage/lineage_axioms_draw.js
@@ -103,18 +103,12 @@ var Lineage_axioms_draw = (function () {
             filterProps +
             "  optional {?s rdfs:label ?sLabel}" +
             "        optional {?o rdfs:label ?oLabel}" +
-
-
             "   optional {?o rdf:type ?oType" +
             filterObjTypeStr +
             "}" +
-
-          "   optional {?s rdf:type ?sType" +
-          filterSubTypeStr +
-          "}" +
-
-
-
+            "   optional {?s rdf:type ?sType" +
+            filterSubTypeStr +
+            "}" +
             "      optional {?p rdfs:label ?pLabel} " +
             "}" +
             "UNION {" +
@@ -140,8 +134,6 @@ var Lineage_axioms_draw = (function () {
             Config.queryLimit;
 
         var filterProps = ""; //Sparql_common.setFilter("p",props)
-
-
 
         var url = Config.sources[sourceLabel].sparql_server.url + "?format=json&query=";
         Sparql_proxy.querySPARQL_GET_proxy(url, query, "", { source: sourceLabel }, function (err, result) {
@@ -177,11 +169,10 @@ var Lineage_axioms_draw = (function () {
             depth: depth,
         };
 
-
         var sourceLabel = Lineage_sources.activeSource;
 
         self.getNodeAxioms(sourceLabel, nodeId, depth, options, function (err, result) {
-            if (result.length< 2 ) {
+            if (result.length < 2) {
                 //new resource
                 return self.drawNodeWithoutAxioms(sourceLabel, nodeId);
             }
@@ -215,7 +206,7 @@ var Lineage_axioms_draw = (function () {
                 }
                 if (!allNodesMap[item.s.value + item.p.value + item.o.value]) {
                     allNodesMap[item.s.value + item.p.value + item.o.value] = 1;
-                    if( item.p.value=="http://www.w3.org/1999/02/22-rdf-syntax-ns#type" && (item.o.value.indexOf("owl")>-1 || item.o.value.indexOf("rdf")>-1  )){
+                    if (item.p.value == "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" && (item.o.value.indexOf("owl") > -1 || item.o.value.indexOf("rdf") > -1)) {
                         return;
                     }
 
@@ -299,17 +290,13 @@ var Lineage_axioms_draw = (function () {
                         options.size = self.defaultNodeSize;
                     }
 
-
                     if (type.indexOf("List") > -1) {
-                     //   edgeStyle = "property";
+                        //   edgeStyle = "property";
                         options.shape = "text";
                         options.color = "#00afef";
                         options.label = "L"; //"∀";
                         options.size = self.defaultNodeSize;
-                    }
-
-
-                    else if (type.indexOf("Restriction") > -1) {
+                    } else if (type.indexOf("Restriction") > -1) {
                         edgeStyle = "restriction";
                         options.shape = "box";
                         options.label = "R"; //"∀";
@@ -351,7 +338,6 @@ var Lineage_axioms_draw = (function () {
                     edgeStyle = nodeParams.edgeStyle;
                     options.level = level;
 
-
                     var node = VisjsUtil.getVisjsNode(sourceLabel, item.s.value, item.sLabel ? item.sLabel.value : Sparql_common.getLabelFromURI(item.s.value), null, options);
                     existingNodes[item.s.value] = node;
                     node.data.infos = nodesMap[item.s.value];
@@ -370,12 +356,10 @@ var Lineage_axioms_draw = (function () {
                 item.children.forEach(function (child) {
                     var targetItem = nodesMap[child.obj];
 
-                    if(child.obj=="_:7a13435ef7")
-                        var x=3
+                    if (child.obj == "_:7a13435ef7") var x = 3;
                     if (!targetItem) {
-
                         if (!finalNodes[child.obj]) {
-                            finalNodes[child.obj] = {predicate:child.pred};
+                            finalNodes[child.obj] = { predicate: child.pred };
                         }
 
                         var node = VisjsUtil.getVisjsNode(sourceLabel, child.obj, "", child.pred, { level: level + 1, color: "#eee", shape: "hexagon", label: "" });
@@ -386,7 +370,6 @@ var Lineage_axioms_draw = (function () {
 
                         targetItem = { s: { value: child.obj } };
                     } else {
-
                         if (targetItem.s.value == "http://www.w3.org/1999/02/22-rdf-syntax-ns#nil") {
                             var symbol = Config.Lineage.logicalOperatorsMap["http://www.w3.org/1999/02/22-rdf-syntax-ns#nil"];
 
@@ -397,7 +380,6 @@ var Lineage_axioms_draw = (function () {
                             targetItem = { s: { value: id } };
                             // return;
                         }
-
 
                         if (!existingNodes[targetItem.s.value]) {
                             var nodeParams = geNodeParams(targetItem.sTypes ? targetItem.sTypes.value : null);
@@ -476,10 +458,8 @@ var Lineage_axioms_draw = (function () {
                             if (err) {
                                 return callbackSeries(err);
                             }
-                            var nodesToRemove=[]
+                            var nodesToRemove = [];
                             result.forEach(function (item) {
-
-
                                 finalNodes[item.subject.value] = {
                                     label: item.subjectLabel ? item.subjectLabel.value : "",
                                     type: item.subjectType ? item.subjectType.value : null,
@@ -498,7 +478,6 @@ var Lineage_axioms_draw = (function () {
                             callbackSeries();
                         });
                     },
-
 
                     //set color and legend
                     function (callbackSeries) {

335cff16068b9688bfd3a61be3caf7fef6f630e4
claudefauconnet <claude.fauconnet@neuf.fr>
2023-06-24 16:05:59 +0200

    begin rules.js

---
+++ api/v1/paths/jowl/rules.js
@@ -0,0 +1,175 @@
+const HttpProxy = require("../../../../bin/httpProxy.");
+const ConfigManager = require("../../../../bin/configManager.");
+const GraphStore = require("../../../../bin/graphStore.");
+const Util = require("../../../../bin/util.");
+const fs = require("fs");
+const { processResponse } = require("../utils");
+const request = require("request");
+const async = require("async");
+
+//https://jena.apache.org/documentation/inference/
+
+module.exports = function () {
+    let operations = {
+        GET,
+    };
+
+    function GET(req, res, next) {
+        let options = null;
+        if (req.query.options) {
+            options = JSON.parse(req.query.options);
+        }
+
+        var jowlConfig = ConfigManager.config.jowlServer;
+
+        if (req.query.type == "externalUrl") {
+            var url = jowlConfig.url + "reasoner/" + req.query.operation + "?filePath=" + req.query.url;
+            HttpProxy.post(jowlConfig.url, {}, function (err, result) {
+                if (err) {
+                    next(err);
+                } else {
+                    return processResponse(res, err, JSON.parse(result));
+                }
+            });
+        } else if (req.query.type == "internalGraphUri" && ConfigManager.config) {
+            var query = req.query.describeSparqlQuery;
+            var url = ConfigManager.config.default_sparql_url + "?query=";
+
+            var requestOptions = {
+                method: "POST",
+                url: url,
+                auth: {
+                    user: ConfigManager.config.sparql_server.user,
+                    pass: ConfigManager.config.sparql_server.password,
+                    sendImmediately: false,
+                },
+                headers: {
+                    Accept: "text/turtle",
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                form: {
+                    query: query,
+
+                    auth: {
+                        user: ConfigManager.config.sparql_server.user,
+                        pass: ConfigManager.config.sparql_server.password,
+                        sendImmediately: false,
+                    },
+                },
+                rejectUnauthorized: false,
+            };
+
+            var resultSize = 1001;
+            var offset = 0;
+            var size = 1000;
+            var str = "";
+
+            async.whilst(
+                function (callbackTest) {
+                    callbackTest(null, resultSize > 16);
+                },
+
+                function (callbackWhilst) {
+                    var query2 = query + " offset " + offset + " limit " + size + "";
+                    requestOptions.form.query = query2;
+
+                    request(requestOptions, function (error, response, body) {
+                        if (error) {
+                            console.log(error);
+                            return callbackWhilst(error);
+                        }
+                        resultSize = body.length;
+                        offset += size;
+                        str += body;
+                        return callbackWhilst();
+                    });
+                },
+                function (err) {
+                    if (err) return processResponse(res, err, null);
+
+                    var ontologyContentEncoded64 = Buffer.from(str).toString("base64");
+
+                    var payload = {
+                        ontologyContentEncoded64: ontologyContentEncoded64,
+                        predicates: JSON.parse(req.query.predicates),
+                    };
+
+                    var options = {
+                        method: "POST",
+                        json: payload,
+                        headers: {
+                            "content-type": "application/json",
+                        },
+                        url: jowlConfig.url + "reasoner/" + req.query.operation,
+                    };
+                    request(options, function (error, response, body) {
+                        return processResponse(res, error, body);
+                    });
+                }
+            );
+        }
+    }
+
+    GET.apiDoc = {
+        security: [{ loginScheme: [] }],
+        summary: "Query Jowl server",
+        description: "Query Jowl server",
+        operationId: "Query Jowl server",
+        parameters: [
+            {
+                name: "operation",
+                description: "operation",
+                type: "string",
+                in: "query",
+                required: true,
+            },
+            {
+                name: "type",
+                description: "externalUrl/ internalGraphUri",
+                in: "query",
+                type: "string",
+                required: true,
+            },
+            {
+                name: "url",
+                description: "source graphUri or url",
+                in: "query",
+                type: "string",
+                required: false,
+            },
+            {
+                name: "describeSparqlQuery",
+                description: "describeSparqlQuery",
+                in: "query",
+                type: "string",
+                required: false,
+            },
+            {
+                name: "predicates",
+                description: "predicates array ",
+                in: "query",
+                type: "string",
+                required: false,
+            },
+
+            {
+                name: "options",
+                description: "JSON ",
+                in: "query",
+                type: "string",
+                required: false,
+            },
+        ],
+
+        responses: {
+            200: {
+                description: "Results",
+                schema: {
+                    type: "object",
+                },
+            },
+        },
+    };
+
+    return operations;
+};
---
+++ public/vocables/modules/tools/lineage/lineage_rules.js
@@ -0,0 +1,56 @@
+import Lineage_sources from "./lineage_sources.js";
+
+var Lineage_rules = (function() {
+  var self = {};
+
+
+  self.showRulesDialog = function() {
+    $("#smallDialogDiv").dialog("open");
+    $("#smallDialogDiv").dialog("option", "title", "Reasoner");
+    self.currentSource = Lineage_sources.activeSource;
+    $("#smallDialogDiv").load("snippets/lineage/lineage_rulesDialog.html", function() {
+      if (!self.loaded) {
+        self.loaded = true;
+      }
+    });
+  };
+
+  self.runRule = function(operation) {
+
+    const params = new URLSearchParams({
+      operation: "inference",
+      type: "internalGraphUri",
+      predicates: JSON.stringify(predicates),
+      describeSparqlQuery: describeQuery,
+    });
+    $("#lineage_rules_infosDiv").html("<span style='color:green;font-style:italic'>Processing " + Lineage_sources.activeSource + "...</span>");
+
+    $.ajax({
+      type: "GET",
+      url: Config.apiUrl + "/jowl/rules?" + params.toString(),
+      dataType: "json",
+
+      success: function (data, _textStatus, _jqXHR) {
+        if (data.result && data.result == "Error") {
+          return alert(" JOWL error ");
+        }
+
+          callback(null, data);
+
+      },
+      error(err) {
+        return alert(err.responseText);
+        if (callback) {
+          return callback(err);
+        }
+      },
+    });
+  };
+
+
+  return self;
+
+
+})();
+export default Lineage_rules;
+window.Lineage_rules = Lineage_rules;

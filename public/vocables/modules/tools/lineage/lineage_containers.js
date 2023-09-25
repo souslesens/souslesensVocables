8446389ae857cc9c177ac7ebd4214442ed59c33f
OUNNOUGHI01~ <ounnoughikarim@gmail.com>
2023-09-25 12:38:06 +0200

    prettier launched

--- public/vocables/modules/tools/lineage/lineage_containers.js
+++ public/vocables/modules/tools/lineage/lineage_containers.js
@@ -115,7 +115,7 @@ Lineage_styles.showDialog(self.currentContainer.data);
 
         var filter = "";
         if (term) {
-            filter =  Sparql_common.setFilter("member", null, term) ;
+            filter = Sparql_common.setFilter("member", null, term);
         }
 
         var search_on_container = "";

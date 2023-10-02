131c89a5a4936d287b3a4418e7ce12da0fc30b3d
xgaia <xgaia@gmx.com>
2023-10-02 14:46:17 +0200

    chore: use standard-version

--- release-new.sh
+++ release-new.sh
@@ -1,16 +1,11 @@
-#! /bin/bash
+#! /bin/sh
 
+release=$(node -p "require('./package.json').version")
 
-if [[ ${1} != "patch" && ${1} != "minor" && ${1} != "major" ]];then
-  echo "Ugage: bash release-new.sh patch|minor|major"
-  exit 1
-fi
-
-npm run $1
-release=$(npm version --json | jq .souslesensvocables | sed 's/"//g')
-
-git commit package* -m "release ${release}"
+git add package* CHANGELOG.md
+git commit -m "chore: release ${release}"
 git tag ${release}
+
 echo "Release ${release} created. Push with:"
 echo ""
-echo "git push && git push --tags"
+echo "git push --tags"

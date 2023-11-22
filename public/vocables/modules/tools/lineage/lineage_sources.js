1c8bf583cbeebb05882043b832e37d38f06e504b
OUNNOUGHI01~ <ounnoughikarim@gmail.com>
2023-11-22 16:32:07 +0100

    prettier+ prettier doc

--- public/vocables/modules/tools/lineage/lineage_sources.js
+++ public/vocables/modules/tools/lineage/lineage_sources.js
@@ -709,7 +709,7 @@ sourceDivId +
             const sourcesAccessControl = Config.profiles[group].sourcesAccessControl;
             return sourcesAccessControl.hasOwnProperty(source) ? sourcesAccessControl[source] : defaultAccessControl;
         });
-        if (groups.indexOf("admin")> -1 && Config.sources[source].editable ) {
+        if (groups.indexOf("admin") > -1 && Config.sources[source].editable) {
             return true;
         }
 

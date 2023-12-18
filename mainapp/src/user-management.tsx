0d3d5810db501456957129a4a4177906e708058b
xgaia <xgaia@gmx.com>
2023-12-18 16:16:01 +0100

    feat(userManagement): add page to manage user token (see and update)

--- mainapp/src/user-management.tsx
+++ mainapp/src/user-management.tsx
@@ -2,8 +2,44 @@ import * as React from "react";
 import { createRoot } from "react-dom/client";
 import { useState, useEffect, useRef } from "react";
 
+import Stack from "react-bootstrap/Stack";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+
+import { fetchMe } from "./Utils";
+
 export default function UserManagenent() {
-    return <>Hello World</>;
+    const [currentUserToken, setCurrentUserToken] = useState<string>("");
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetchMe();
+            setCurrentUserToken(response.user.token);
+        })();
+    }, []);
+
+    const postToken = async () => {
+        const response = await fetch("/api/v1/users/token", { method: "post" });
+        const json = await response.json();
+        return json.token;
+    };
+
+    const handleUpdateToken = async (_event: React.MouseEvent<HTMLButtonElement>) => {
+        setCurrentUserToken(await postToken());
+    };
+
+    return (
+        <Stack className="p-3">
+            <h4>Manage API key</h4>
+            <InputGroup className="w-50">
+                <Form.Control value={currentUserToken} placeholder="" aria-label="API key" aria-describedby="basic-addon" />
+                <Button onClick={handleUpdateToken} variant="primary" id="button-addon">
+                    Renew
+                </Button>
+            </InputGroup>
+        </Stack>
+    );
 }
 
 const container = document.getElementById("mount-user-management-here");

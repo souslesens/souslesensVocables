f56a93402097b112ca8630b34a7707abc68560a6
xgaia <xgaia@gmx.com>
2023-12-18 16:15:18 +0100

    feat(migrations): update token generation in migration_1.45_users.js

--- scripts/migrations/migration_1.45_users.js
+++ scripts/migrations/migration_1.45_users.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const yargs = require("yargs");
-const crypto = require("crypto");
+const ULID = require("ulid");
+const { createHash } = require("crypto");
 
 const argv = yargs.alias("f", "file").describe("f", "Path to users file").demandOption(["f"]).alias("w", "write").describe("w", "Write to the file").boolean("w").help().argv;
 
@@ -9,9 +10,11 @@ const usersFilePath = argv.file;
 fs.readFile(usersFilePath, (_err, rawData) => {
     const data = JSON.parse(rawData);
     const lData = Object.entries(data);
-    const lNewData = lData.map(([_key, user]) => {
+    const lNewData = lData.map(([key, user]) => {
         if (!user.token) {
-            user.token = crypto.randomBytes(20).toString("hex");
+            const hashedLogin = createHash("sha256").update(key).digest("hex");
+            const ulid = ULID.ulid();
+            user.token = `sls-${ulid.toLowerCase()}${hashedLogin.substring(0, 5)}`;
         }
         return [user.login, user];
     });

233058ba9753f8fa63345876fb1d3967ca2a4f8f
xgaia <xgaia@gmx.com>
2023-12-20 12:27:48 +0100

    feat(userManagement): add copy btn to copy token

--- mainapp/src/user-management.tsx
+++ mainapp/src/user-management.tsx
@@ -7,10 +7,14 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import CheckIcon from "@mui/icons-material/Check";
+
 import { fetchMe } from "./Utils";
 
 export default function UserManagenent() {
     const [currentUserToken, setCurrentUserToken] = useState<string>("");
+    const [copied, setCopied] = useState<boolean>(false);
 
     useEffect(() => {
         (async () => {
@@ -29,11 +33,21 @@ export default function UserManagenent() {
         setCurrentUserToken(await postToken());
     };
 
+    const handleCopyToken = async () => {
+        setCopied(true);
+        navigator.clipboard.writeText(currentUserToken);
+        await new Promise((r) => setTimeout(r, 2000));
+        setCopied(false);
+    };
+
     return (
         <Stack className="p-3">
             <h4>Manage API key</h4>
             <InputGroup className="w-50">
                 <Form.Control value={currentUserToken} placeholder="" aria-label="API key" aria-describedby="basic-addon" />
+                <Button onClick={handleCopyToken} variant={copied ? "outline-success" : "outline-primary"} id="button-addon">
+                    {copied ? <CheckIcon /> : <ContentCopyIcon />}
+                </Button>
                 <Button onClick={handleUpdateToken} variant="primary" id="button-addon">
                     Renew
                 </Button>

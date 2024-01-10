a00f1b90ad1d66bd9e02975363ec6244e7256912
alubert <alubert@logilab.fr>
2024-01-10 16:59:51 +0100

    fix(migrations): add the allowSourceCreation and maxNumberCreatedSource keys to users

--- scripts/migrations/migration_1.51_users.js
+++ scripts/migrations/migration_1.51_users.js
@@ -5,6 +5,7 @@ const path = require("path");
 const argv = yargs.alias("c", "config").describe("c", "Path to config directory").demandOption(["c"]).alias("w", "write").describe("w", "Write to the file").boolean("w").help().argv;
 
 const sourcesFilePath = path.resolve(argv.config + "/sources.json");
+const usersFilePath = path.resolve(argv.config + "/users/users.json");
 
 fs.readFile(sourcesFilePath, (_err, rawData) => {
     const data = JSON.parse(rawData);
@@ -22,6 +23,27 @@ fs.readFile(sourcesFilePath, (_err, rawData) => {
 
     if (argv.w) {
         fs.writeFileSync(sourcesFilePath, JSON.stringify(newData, null, 2));
+    } else {
+        console.log(newData);
+    }
+});
+
+fs.readFile(usersFilePath, (_err, rawData) => {
+    const data = JSON.parse(rawData);
+    const lData = Object.entries(data);
+    const lNewData = lData.map(([id, user]) => {
+        if (!user.hasOwnProperty("allowSourceCreation")) {
+            user.allowSourceCreation = false;
+        }
+        if (!user.hasOwnProperty("maxNumberCreatedSource")) {
+            user.maxNumberCreatedSource = 5;
+        }
+        return [id, user];
+    });
+    const newData = Object.fromEntries(lNewData);
+
+    if (argv.w) {
+        fs.writeFileSync(usersFilePath, JSON.stringify(newData, null, 2));
     } else {
         console.log(newData);
     }

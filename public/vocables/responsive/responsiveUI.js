78153dd4c3a456bac1592206306b684817aceace
OUNNOUGHI01~ <ounnoughikarim@gmail.com>
2024-02-05 12:37:13 +0100

    Frame KGA onloaded SLSV

--- public/vocables/responsive/responsiveUI.js
+++ public/vocables/responsive/responsiveUI.js
@@ -18,7 +18,9 @@ var ResponsiveUI = (function () {
     self.toolsNeedSource = ["lineage", "KGquery", "KGcreator"];
     self.init = function () {
         self.oldRegisterSource = Lineage_sources.registerSource;
-        self.setSlsvCssClasses();
+        //self.setSlsvCssClasses();
+         //your remaining code
+        
         var tools = [];
 
         for (var key in Config.tools) {
@@ -315,34 +317,17 @@ var ResponsiveUI = (function () {
             .css("left", "30%");
     };
 
-    self.setSlsvCssClasses = function () {
-        async.series(
-            [
-                function (callbackSeries) {
-                    $.getScript("./responsive/less.min.js")
-                        .done(function (script, textStatus) {
-                            callbackSeries();
-                            //your remaining code
-                            less.pageLoadFinished.then(function () {
-                                //setTimeout(() => {}, "500");
-                                ResponsiveUI.changeTheme(Config.theme.defaultTheme);
-                                if (Config.theme.selector) {
-                                    $("#theme-selector-btn").show();
-                                }
-                            });
-                        })
-                        .fail(function (jqxhr, settings, exception) {
-                            console.log("eeee");
-                            callbackSeries(err);
-                        });
-                },
-            ],
-            function (err) {
-                if (err) {
-                    return alert(err);
-                }
+    self.setSlsvCssClasses = function (callback) {
+        
+        less.pageLoadFinished.then(function () {
+            //setTimeout(() => {}, "500");
+            ResponsiveUI.changeTheme(Config.theme.defaultTheme);
+            if (Config.theme.selector) {
+                $("#theme-selector-btn").show();
             }
-        );
+            callback();
+        });
+           
     };
 
     self.themeList = function () {

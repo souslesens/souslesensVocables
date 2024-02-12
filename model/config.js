18c6e8800306145350297dadcda51279d474251a
alubert <alubert@logilab.fr>
2024-02-12 09:34:02 +0100

    feat: add the databases.json file to the config module

--- model/config.js
+++ model/config.js
@@ -3,6 +3,7 @@ const configPath = process.env.CONFIG_PATH || "config";
 const configUsersPath = `${configPath}/users/users.json`;
 const configSourcesPath = `${configPath}/sources.json`;
 const configProfilesPath = `${configPath}/profiles.json`;
+const configDatabasesPath = `${configPath}/databases.json`;
 const config = require(path.resolve(`${configPath}/mainConfig.json`));
 
-module.exports = { configPath, configUsersPath, configSourcesPath, configProfilesPath, config };
+module.exports = { configPath, configUsersPath, configSourcesPath, configProfilesPath, configDatabasesPath, config };

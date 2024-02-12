8195975b7b503bd32e25f320b9b5a9f694aba40e
OUNNOUGHI01~ <ounnoughikarim@gmail.com>
2024-02-12 14:55:35 +0100

    config editor

--- mainapp/src/index.tsx
+++ mainapp/src/index.tsx
@@ -2,6 +2,18 @@ import * as React from "react";
 import Admin from "./Admin";
 import { createRoot } from "react-dom/client";
 
-const container = document.getElementById("mount-app-here");
-const root = createRoot(container!);
-root.render(<Admin />);
+
+declare global {
+    interface Window {
+        ConfigEditor: {
+            createApp: () => void;
+        } 
+    }
+}
+window.ConfigEditor.createApp = function createApp() {
+    const container = document.getElementById("mount-app-here");
+    const root = createRoot(container!);
+    root.render(<Admin />);
+    return root.unmount.bind(root);
+};
+
--- public/vocables/modules/tools/GraphManagement.js
+++ public/vocables/modules/tools/GraphManagement.js
@@ -1,15 +1,36 @@
 const GraphManagement = (function () {
     const self = {};
     self.onSourceSelect = function () {};
-
+    self.createApp=null;
+    self.umountKGUploadApp =null;
+    self.unload= function(){
+        self.umountKGUploadApp();
+    }
     self.onLoaded = function () {
         $("#accordion").accordion("option", { active: 2 });
+        import("/assets/graph_management.js");
         setTimeout(function () {
-            $("#graphDiv").html("");
-            $("#graphDiv").html(`
+            $("#mainDialogDiv").dialog("open")
+
+            $("#mainDialogDiv").dialog("option","title","Graph Management");
+            $("#mainDialogDiv").dialog({
+
+                close: function (event, ui) {
+                    self.umountKGUploadApp();
+                    
+                },
+            });
+            $("#mainDialogDiv").parent().css("left","100px");
+
+
+             
+            $("#mainDialogDiv").html("");
+            $("#mainDialogDiv").html(`
                     <div id="mount-graph-management-here"></div>
-                `);
-            import("/assets/graph_management.js");
+            `);
+            self.umountKGUploadApp = self.createApp();
+
+            
         }, 200);
     };
 

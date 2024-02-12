0b955c4392c86a8687d2288b1e59fde5b3aec267
alubert <alubert@logilab.fr>
2024-02-12 09:38:49 +0100

    feat: implements the databases tab in the configEditor

---
+++ mainapp/src/Component/DatabasesTable.tsx
@@ -0,0 +1,344 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import {
+    Alert,
+    Box,
+    Button,
+    Chip,
+    CircularProgress,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    FormControl,
+    Grid,
+    IconButton,
+    InputAdornment,
+    MenuItem,
+    Modal,
+    Paper,
+    Stack,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    TextField,
+} from "@mui/material";
+import Autocomplete from "@mui/material/Autocomplete";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import { Done, Visibility, VisibilityOff } from "@mui/icons-material";
+
+import * as React from "react";
+import { SRD } from "srd";
+import { ulid } from "ulid";
+import { zod } from "zod";
+import { useZorm } from "react-zorm";
+import { ZodCustomIssueWithMessage } from "react-zorm/dist/types";
+import * as z from "zod";
+
+import { useModel } from "../Admin";
+import { ButtonWithConfirmation } from "./ButtonWithConfirmation";
+import {
+    Database,
+    DatabaseSchema,
+    defaultDatabase,
+    deleteDatabase,
+    SourceAccessControl,
+} from "../Database";
+import { style } from "../Utils";
+
+const enum Type {
+    UserUpdatedField,
+}
+
+const enum Mode {
+    Creation,
+    Edition,
+}
+
+type DatabaseEditionState = {
+    form: Database;
+};
+
+type DatabaseFormProps = {
+    database?: Database;
+    create?: boolean;
+};
+
+type Msg_ =
+    | { type: Type.UserUpdatedField; payload: { fieldname: string; newValue: string } }
+
+
+const updateDatabase = (databaseEditionState: DatabaseEditionState, msg: Msg_): DatabaseEditionState => {
+    switch (msg.type) {
+        case Type.UserUpdatedField:
+            return {
+                form: {
+                    ...databaseEditionState.form,
+                    [msg.payload.id]: msg.payload.value,
+                }
+            };
+    }
+}
+
+const validateForm = (form: DatabaseFormProps) => {
+    const validation = DatabaseSchema.safeParse(form);
+
+    let errors = {};
+    if (!validation.success) {
+        validation.error.issues.map(item => item.path.map(path => errors[path] = item.message));
+    }
+
+    return errors;
+};
+
+const DatabaseFormDialog = ({ database = defaultDatabase(ulid()), create = false }: DatabaseFormProps) => {
+    const [databaseModel, update] = React.useReducer(updateDatabase, { form: database });
+    const [displayPassword, setDisplayPassword] = React.useState(false);
+    const [currentErrors, setErrors] = React.useState({});
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    const handleClickShowPassword = () => setDisplayPassword(!displayPassword);
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => event.preventDefault();
+
+    const handleValidation = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const errors = validateForm(databaseModel.form);
+        setErrors(errors);
+
+        if (Object.keys(errors).length === 0) {
+            event.preventDefault();
+            handleClose();
+        }
+    };
+
+    const handleFieldUpdate = (fieldName: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+        let fieldValue = event.target.value;
+
+        switch (fieldName) {
+            case "port":
+                fieldValue = parseInt(fieldValue);
+        }
+
+        const errors = validateForm({ ...databaseModel.form, [fieldName]: fieldValue });
+        // TODO: Find a way to only raise the error related to the field instead of the all form
+        setErrors(errors);
+
+        update({ type: Type.UserUpdatedField, payload: { id: fieldName, value: fieldValue } });
+    };
+
+    return (
+        <>
+            <Button variant="contained" color="primary" onClick={handleOpen}>
+                {create ? "Create Database" : "Edit"}
+            </Button>
+            <Dialog
+                fullWidth={true}
+                maxWidth="md"
+                onClose={handleClose}
+                open={open}
+                PaperProps={{ component: "form" }}
+            >
+                <DialogContent sx={{ marginTop: "1em" }}>
+                    <Stack spacing={4}>
+                        <TextField
+                            defaultValue={databaseModel.form.id}
+                            disabled={create ? false : true}
+                            error={currentErrors.name}
+                            fullWidth
+                            helperText={currentErrors.name}
+                            id="name"
+                            label="Name"
+                            onChange={handleFieldUpdate("name")}
+                            value={databaseModel.form.name}
+                        />
+                        <TextField
+                            defaultValue={databaseModel.form.driver}
+                            error={currentErrors.driver}
+                            fullWidth
+                            helperText={currentErrors.driver}
+                            id="driver"
+                            label="Driver"
+                            onChange={handleFieldUpdate("driver")}
+                            required
+                            select
+                        >
+                            <MenuItem value="postgres">PostgreSQL</MenuItem>
+                            <MenuItem value="sqlserver">SQLServer</MenuItem>
+                        </TextField>
+                        <Stack direction="row" spacing={1}>
+                            <TextField
+                                error={currentErrors.host}
+                                fullWidth
+                                helperText={currentErrors.host}
+                                id="host"
+                                label="Hostname"
+                                onChange={handleFieldUpdate("host")}
+                                required
+                                value={databaseModel.form.host}
+                            />
+                            <TextField
+                                error={currentErrors.port}
+                                helperText={currentErrors.port}
+                                id="port"
+                                label="Port"
+                                onChange={handleFieldUpdate("port")}
+                                required
+                                type="number"
+                                value={databaseModel.form.port}
+                            />
+                        </Stack>
+                        <TextField
+                            error={currentErrors.database}
+                            fullWidth
+                            helperText={currentErrors.database}
+                            id="database"
+                            label="Database"
+                            onChange={handleFieldUpdate("database")}
+                            required
+                            value={databaseModel.form.database}
+                        />
+                        <TextField
+                            error={currentErrors.user}
+                            fullWidth
+                            helperText={currentErrors.user}
+                            id="username"
+                            label="Username"
+                            onChange={handleFieldUpdate("user")}
+                            required
+                            value={databaseModel.form.user}
+                        />
+                        <TextField
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                        edge="end"
+                                    >
+                                        {displayPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                            error={currentErrors.password}
+                            fullWidth
+                            helperText={currentErrors.password}
+                            id="password"
+                            label="Password"
+                            onChange={handleFieldUpdate("password")}
+                            type={displayPassword ? "text" : "password"}
+                            value={databaseModel.form.password}
+                        />
+                    </Stack>
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        color="primary"
+                        component="label"
+                        onClick={handleValidation}
+                        startIcon={<Done />}
+                        type="submit"
+                        variant="contained"
+                    >Submit</Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    );
+};
+
+const DatabasesTable = () => {
+    const { model, updateModel } = useModel();
+    const [filteringChars, setFilteringChars] = React.useState("");
+    const [orderBy, setOrderBy] = React.useState<keyof Database>("id");
+    const [order, setOrder] = React.useState<Order>("asc");
+
+    type Order = "asc" | "desc";
+
+    function handleRequestSort(property: keyof Database) {
+        const isAsc = orderBy === property && order === "asc";
+        setOrder(isAsc ? "desc" : "asc");
+        setOrderBy(property);
+    }
+
+    const renderDatabases = SRD.match(
+        {
+            // eslint-disable-next-line react/no-unescaped-entities
+            notAsked: () => <p>Let’s fetch some data!</p>,
+            loading: () => (
+                <Box sx={{ display: "flex", justifyContent: "center", p: 4 }}>
+                    <CircularProgress />
+                </Box>
+            ),
+            failure: (msg: string) => (
+                <Alert variant="filled" severity="error" sx={{ m: 4 }}>
+                    {`${msg}. Please, reload this page.`}
+                </Alert>
+            ),
+            success: (gotDatabases: Database[]) => {
+                const sortedDatabases: Database[] = gotDatabases.slice().sort((a: Database, b: Database) => {
+                    const left: string = a[orderBy] as string;
+                    const right: string = b[orderBy] as string;
+                    return order === "asc" ? left.localeCompare(right) : right.localeCompare(left);
+                });
+                console.log("renderDatabases");
+
+                return (
+                    <Stack direction="column" spacing={{ xs: 2 }} sx={{ mx: 12, my: 4 }} useFlexGap>
+                        <Autocomplete
+                            disablePortal
+                            id="filter databases"
+                            options={sortedDatabases.map((database: Database) => { return database.id })}
+                            onInputChange={(event, newInputValue) => setFilteringChars(newInputValue)}
+                            renderInput={(params) => <TextField {...params} label="Search Databases by name" />}
+                        />
+                        <TableContainer sx={{ height: "400px" }} component={Paper}>
+                            <Table>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell style={{ fontWeight: "bold", width: "100%" }}>
+                                            <TableSortLabel active={orderBy === "id"} direction={order} onClick={() => handleRequestSort("id")}>Name</TableSortLabel>
+                                        </TableCell>
+                                        <TableCell align="center" style={{ fontWeight: "bold" }}>Driver</TableCell>
+                                        <TableCell align="center" style={{ fontWeight: "bold" }}>Actions</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody sx={{ width: "100%", overflow: "visible" }}>
+                                    {sortedDatabases
+                                        .filter((database: Database) => database.id.includes(filteringChars))
+                                        .map((database: Database) => {
+                                            return (
+                                                <TableRow key={database.id}>
+                                                    <TableCell>{database.id}</TableCell>
+                                                    <TableCell align="center">
+                                                        <Chip label={database.driver} size="small" />
+                                                    </TableCell>
+                                                    <TableCell align="center">
+                                                        <Stack direction="row" justifyContent="center" spacing={{ xs: 1 }} useFlexGap>
+                                                            <Button variant="contained" color="success">Test</Button>
+                                                            <DatabaseFormDialog database={database} />
+                                                            <ButtonWithConfirmation label="Delete" msg={() => deleteDatabase(database, updateModel)} />
+                                                        </Stack>
+                                                    </TableCell>
+                                                </TableRow>
+                                            );
+                                        })}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                        <Stack direction="row" justifyContent="center" spacing={{ sx: 1 }}>
+                            <DatabaseFormDialog create={true} />
+                        </Stack>
+                    </Stack>
+                );
+            },
+        },
+        model.databases
+    );
+
+    return renderDatabases;
+};
+
+export { DatabasesTable, Mode, Msg_, Type };

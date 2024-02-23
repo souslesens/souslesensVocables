aa4ef2087efa3c6f8914b605004d924709e9228d
xgaia <xgaia@gmx.com>
2024-02-23 12:17:54 +0100

    feat(migrations): migrate mappings files

--- scripts/migrations/migration_1.55_databases.js
+++ scripts/migrations/migration_1.55_databases.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const yargs = require("yargs");
 const path = require("path");
 const ULID = require("ulid");
-
+const { globSync } = require("glob");
 const argv = yargs.alias("c", "config").describe("c", "Path to config directory").demandOption(["c"]).alias("w", "write").describe("w", "Write to the file").boolean("w").help().argv;
 
 const mainconfigFilePath = path.resolve(argv.config, "mainConfig.json");
@@ -21,28 +21,57 @@ try {
 // remove SQLserver from mainconfig
 const { SQLserver: sqlServer, ...newMainConfig } = mainconfig;
 
-// return if file is already migrated
-if (!sqlServer) {
-    return 0;
-}
+// return if file is already migrateda
+let databaseId = ULID.ulid();
+const databaseName = "sqlserver-1";
+if (sqlServer) {
+    // migrate SQLserver form mainConfig to databases
+    const database = {
+        id: databaseId,
+        name: databaseName,
+        driver: "sqlserver",
+        host: sqlServer.server,
+        port: 1433,
+        database: sqlServer.database,
+        user: sqlServer.user,
+        password: sqlServer.password,
+    };
+    databases.push(database);
 
-// migrate SQLserver form mainConfig to databases
-const database = {
-    id: ULID.ulid(),
-    name: "sqlserver-1",
-    driver: "sqlserver",
-    host: sqlServer.server,
-    port: 1433,
-    database: sqlServer.database,
-    user: sqlServer.user,
-    password: sqlServer.password,
-};
-databases.push(database);
-
-if (argv.w) {
-    fs.writeFileSync(mainconfigFilePath, JSON.stringify(newMainConfig, null, 2));
-    fs.writeFileSync(databasesFilePath, JSON.stringify(databases, null, 2));
+    if (argv.w) {
+        fs.writeFileSync(mainconfigFilePath, JSON.stringify(newMainConfig, null, 2));
+        fs.writeFileSync(databasesFilePath, JSON.stringify(databases, null, 2));
+    } else {
+        console.log("---------------\nmainConfig.json\n---------------\n", mainconfig);
+        console.log("\n--------------\ndatabases.json\n--------------\n", databases);
+    }
 } else {
-    console.log("---------------\nmainConfig.json\n---------------\n", mainconfig);
-    console.log("\n--------------\ndatabases.json\n--------------\n", databases);
+    databaseId = databases.find((db) => db.name == databaseName).id;
 }
+
+// migrate data/mappings
+const mappingDirPath = path.resolve("data", "mappings");
+const mappingFiles = globSync(path.join(mappingDirPath, "*/main.json"));
+
+const fileToMigrate = mappingFiles.filter((file) => {
+    filePath = path.resolve(file);
+    const mapping = JSON.parse(fs.readFileSync(filePath, { encoding: "utf-8" }));
+    const databaseSources = Object.entries(mapping.databaseSources)[0];
+    if (databaseSources && "type" in databaseSources[1]) {
+        return filePath;
+    }
+});
+
+// backup mapping dir
+if (argv.w && fileToMigrate.length > 0) {
+    fs.cpSync(mappingDirPath, path.resolve("data", "mappings.bak"), { recursive: true });
+}
+fileToMigrate.forEach((filePath) => {
+    const mapping = JSON.parse(fs.readFileSync(filePath, { encoding: "utf-8" }));
+    mapping.databaseSources = { [databaseId]: { name: databaseName } };
+    if (argv.w) {
+        fs.writeFileSync(filePath, JSON.stringify(mapping, null, 2));
+    } else {
+        console.log(`--- ${filePath} ---\n`, mapping, "\n");
+    }
+});

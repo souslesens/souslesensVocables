00b02610b59859247dd87c65f321c5ea5b4e5d4e
xgaia <xgaia@gmx.com>
2024-04-05 15:10:16 +0200

    fix(UserManagement): import after timeout

--- public/vocables/modules/tools/UserManagement.js
+++ public/vocables/modules/tools/UserManagement.js
@@ -1,11 +1,11 @@
 const UserManagement = (function () {
     const self = {};
-    import("/assets/user_management.js");
     self.onSourceSelect = function () {};
 
     self.onLoaded = function () {
         $("#accordion").accordion("option", { active: 2 });
         setTimeout(function () {
+            import("/assets/user_management.js");
             $("#graphDiv").html("");
             $("#graphDiv").html(`
                     <div id="mount-user-management-here"></div>

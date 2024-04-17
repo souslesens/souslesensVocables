f5a0441fd7e42fabd31e4f50db95114d34e24667
alubert <alubert@logilab.fr>
2024-04-17 16:33:03 +0200

    feat(mainapp): migrate the kg-upload-app component to MaterialUI

--- mainapp/src/kg-upload-app.tsx
+++ mainapp/src/kg-upload-app.tsx
@@ -2,14 +2,18 @@ import * as React from "react";
 import { createRoot } from "react-dom/client";
 import { useState, useEffect } from "react";
 
-import Form from "react-bootstrap/Form";
-import Row from "react-bootstrap/Row";
-import Stack from "react-bootstrap/Stack";
-import Button from "react-bootstrap/Button";
-import Alert from "react-bootstrap/Alert";
+import {
+    Alert,
+    Button,
+    FormControl,
+    MenuItem,
+    Select,
+    Stack,
+} from "@mui/material";
+import { Done, Folder } from "@mui/icons-material";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFile, faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { VisuallyHiddenInput } from "./Utils";
+
 
 interface UploadFormData {
     displayForm: "database" | "file" | "";
@@ -32,15 +36,19 @@ declare global {
 export default function App(uploadFormData: UploadFormData) {
     const [databases, setDatabases] = useState<{ string: string }[]>([]);
     const [files, setFiles] = useState<File[]>([]);
-    const [uploadStatus, setUploadStatus] = useState("");
     const [selectedDatabase, setSelectedDatabase] = useState("_default");
+
+    // error management
+    const [error, setError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+
 
     useEffect(() => {
         void fetchDatabases();
     }, []);
 
     const uploadFileHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setUploadStatus("");
+        setError(false);
         if (event.currentTarget.files === null) {
             return;
         }
@@ -58,13 +66,13 @@ export default function App(uploadFormData: UploadFormData) {
 
         const response = await fetch("/api/v1/upload", { method: "POST", body: formData });
         if (response.status === 201) {
-            setUploadStatus("success");
+            setError(false);
             // reload
             window.KGcreator.uploadFormData.selectedFiles = files.map((file) => file.name);
             window.KGcreator.createCsvSourceMappings();
         } else {
-            setUploadStatus("error");
-            // todo: display error message
+            setError(true);
+            setErrorMessage("The upload did not works…");
         }
     };
 
@@ -75,66 +83,72 @@ export default function App(uploadFormData: UploadFormData) {
     };
 
     const handleDatabaseChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        const selectedIndex = event.currentTarget.selectedIndex
-        const option = event.currentTarget[selectedIndex];
-        setSelectedDatabase(option.value);
-        window.KGcreator.uploadFormData.selectedDatabase = {'id':option.value,'name':option.label};
+        const value = event.target.value;
+        setSelectedDatabase(value.id);
+        window.KGcreator.uploadFormData.selectedDatabase = value;
         window.KGcreator.createDataBaseSourceMappings();
     };
 
-    const selectDatabase = (
-        <Form.Select defaultValue="_default" value={selectedDatabase} onChange={handleDatabaseChange} aria-label="Select database">
-            <option value="_default" disabled={true}>
-                Select database
-            </option>
-            {databases.map((database) => {
-                return (
-                    <option key={database.name} value={database.id}>
-                        {database.name}
-                    </option>
-                );
-            })}
-        </Form.Select>
-    );
+    const widget = (displayForm: string) => {
+        if (displayForm === "database") {
+            return (
+                <Select
+                    label="Select database"
+                    onChange={handleDatabaseChange}
+                    value={selectedDatabase}
+                >
+                    <MenuItem disabled value={"_default"}>Select database</MenuItem>
+                    {databases.map((database) => <MenuItem value={{id: database.id, name: database.name}}>{database.name}</MenuItem>)}
+                </Select>
+            );
+        }
 
-    const browseForm = (
-        <Form.Group controlId="formFileMultiple" className="mb-3">
-            <Form.Control name="files" type="file" multiple accept=".csv,.tsv" onChange={uploadFileHandler} />
-            <Form.Text color="muted">
-                <p>Supported files: CSV</p>
-            </Form.Text>
-        </Form.Group>
-    );
+        if (displayForm === "file") {
+            return (
+                <Stack alignItems="flex-end" spacing={2} useFlexGap>
+                    <Button
+                        color="info"
+                        component="label"
+                        fullWidth
+                        role={undefined}
+                        startIcon={<Folder />}
+                        tabIndex={-1}
+                        variant="outlined"
+                    >
+                        {files.length === 1 ? files[0].name : "Select a file to upload…"}
+                        <VisuallyHiddenInput
+                            accept=".csv,.tsv"
+                            id="formUploadCSV"
+                            onChange={uploadFileHandler}
+                            type="file"
+                        />
+                    </Button>
+                    <Button
+                        disabled={files.length === 0}
+                        onClick={fileSubmitHandler}
+                        startIcon={<Done />}
+                        size="small"
+                        variant="contained"
+                    >
+                        Submit
+                    </Button>
+                </Stack>
+            );
+        }
 
-    const uploadButton = (
-        <div className="mb-3">
-            <Button variant="primary" onClick={fileSubmitHandler}>
-                <FontAwesomeIcon icon={uploadStatus === "success" ? faCheck : uploadStatus === "error" ? faTimes : faFile} /> Upload {files.length} {files.length === 1 ? "file" : "files"}
-            </Button>
-        </div>
-    );
-
-    const errorMessage = (message: string) => <Alert variant="danger">{message}</Alert>;
-
-    if (window.KGcreator.uploadFormData.displayForm == "database") {
-        return (
-            <Stack gap={2}>
-                {selectDatabase}
-            </Stack>
-        );
+        return <></>
     }
-    if (window.KGcreator.uploadFormData.displayForm == "file") {
-        return (
-            <>
-                <Row>
-                    {browseForm}
-                    {files.length > 0 ? uploadButton : null}
-                    {uploadStatus === "error" ? errorMessage("Error during upload") : null}
-                </Row>
-            </>
-        );
-    }
-    return <></>;
+
+    return (
+        <Stack spacing={2} sx={{ width: 400 }} useFlexGap>
+            {error ? (
+                <Alert variant="filled" severity="error">
+                    {errorMessage}
+                </Alert>
+            ) : null}
+            {widget(window.KGcreator.uploadFormData.displayForm)}
+        </Stack>
+    );
 }
 
 window.KGcreator.createApp = function createApp(uploadFormData: UploadFormData) {

17929ad1df200ee5a66b42f18b4d5c535cd1e762
xgaia <xgaia@gmx.com>
2024-05-02 09:40:41 +0200

    fix(api/health): remove sqlserver from health check (externaly managed now)

--- api/v1/paths/health.js
+++ api/v1/paths/health.js
@@ -9,11 +9,10 @@ module.exports = function () {
 
     ///// GET api/v1/health
     async function GET(req, res, next) {
-        const enabledServices = config.health_enabled_services ? config.health_enabled_services : ["virtuoso", "elasticsearch", "spacyserver", "sqlserver"];
+        const enabledServices = config.health_enabled_services ? config.health_enabled_services : ["virtuoso", "elasticsearch", "spacyserver"];
         const sparqlUrl = `${config.sparql_server.url}/?default-graph-uri=&query=SELECT+*%0D%0AWHERE+%7B%3Fs+%3Fp+%3Fo%7D%0D%0ALIMIT+10&format=application%2Fsparql-results%2Bjson`;
         const elasticSearchUrl = `${config.ElasticSearch.url}/_cat/health`;
         const spacyServerUrl = `${config.annotator.spacyServerUrl}/health_check`.replace("/pos", "");
-        const sqlServerConnStr = `Server=${config.SQLserver.server},1433;Database=${config.SQLserver.database};User Id=${config.SQLserver.user};Password=${config.SQLserver.password};Encrypt=false`;
         try {
             const fetchVirtuoso = fetch(sparqlUrl)
                 .then((r) => ({ name: "virtuoso", health: true }))
@@ -24,11 +23,7 @@ module.exports = function () {
             const fetchSpacyServer = fetch(spacyServerUrl)
                 .then((r) => ({ name: "spacyserver", health: true }))
                 .catch((e) => ({ name: "spacyserver", health: false }));
-            const connectSqlServer = sql
-                .connect(sqlServerConnStr)
-                .then((r) => ({ name: "sqlserver", health: true }))
-                .catch((e) => ({ name: "sqlserver", health: false }));
-            const results = await Promise.all([fetchVirtuoso, fetchElasticSearch, fetchSpacyServer, connectSqlServer].map((p) => p.catch((e) => e)));
+            const results = await Promise.all([fetchVirtuoso, fetchElasticSearch, fetchSpacyServer].map((p) => p.catch((e) => e)));
             const health = Object.fromEntries(
                 results
                     .map((res) => {

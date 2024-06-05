1ef1140895921018a3e9742b3808f0e7e9e0bb83
xgaia <xgaia@gmx.com>
2024-06-05 17:38:53 +0200

    chore(release-new): move migration_NEXT_*.js to migration_x.x.x_*.js with npm run release:tag

--- release-new.sh
+++ release-new.sh
@@ -2,6 +2,14 @@
 
 release=$(node -p "require('./package.json').version")
 
+# move migration_NEXT*.js with release name
+files=$(find scripts/migrations -name "migration_NEXT_*.js")
+for file in $files;do
+    new_file=$(echo $file | sed "s/NEXT/${release}/g")
+    echo "Moving $file to $new_file…"
+    git mv "${file}" "${new_file}"
+done
+
 git add package* CHANGELOG.md
 git commit -m "chore: release ${release}"
 git tag ${release}

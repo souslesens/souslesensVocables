7a66739ce8e49d280d625f2980b3af7b2f3ddb96
xgaia <xgaia@gmx.com>
2024-06-07 17:40:57 +0200

    chore(nelease-new): add a message to changelog when a new release need a migration

---
+++ scripts/post_release.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+const release = process.argv[2];
+const line = parseInt(process.argv[3]);
+
+if (release === undefined || line === undefined || line === NaN) {
+    console.error("Usage: changelog.js <release> <lineNumber>");
+    return;
+}
+
+const message = `
+> [!IMPORTANT]
+> Updating to ${release} require a data migration. Execute the following script after upgrade.
+
+\`\`\`bash
+npm run migrate
+\`\`\``;
+
+const changelogContent = fs.readFileSync("CHANGELOG.md").toString().split("\n");
+
+changelogContent.splice(line, 0, message);
+
+const newChangelogContent = changelogContent.join("\n");
+
+fs.writeFileSync("CHANGELOG.md", newChangelogContent);

6f5be84b407de34c835755348d2ef7e7d7b785e6
xgaia <xgaia@gmx.com>
2024-07-03 16:07:04 +0200

    feat: manage tools server side

--- public/vocables/config/tools.js
+++ public/vocables/config/tools.js
@@ -1,22 +1,24 @@
-const allTools = {
-    lineage: { label: "lineage", noSource: 0, controller: Lineage_r, toolDescriptionImg: null },
-    KGcreator: { label: "KGcreator", noSource: 1, controller: KGcreator_r, toolDescriptionImg: null },
-    KGquery: { label: "KGquery", noSource: 0, controller: KGquery_r, toolDescriptionImg: null, toTools: { Graph: "queryResultToVisjsGraph" } },
-    Standardizer: { label: "Standardizer", multiSources: 0, controller: Standardizer, toolDescriptionImg: null },
-    TSF_Dictionary: { label: "TSF_Dictionary", noSource: 1, controller: Lineage_dictionary, toolDescriptionImg: null },
-    SPARQL: { label: "SPARQL endpoint", multiSources: 0, controller: SPARQL_endpoint, toolDescriptionImg: null },
-    admin: { label: "Admin", multiSources: 0, controller: Admin, toolDescriptionImg: null },
-    ConfigEditor: { label: "ConfigEditor", noSource: 1, controller: ConfigEditor, toolDescriptionImg: null },
-    GraphManagement: { label: "GraphManagement", noSource: 1, controller: GraphManagement, toolDescriptionImg: null },
-    UserManagement: { label: "UserManagement", noSource: 1, controller: UserManagement, toolDescriptionImg: null },
-    OntoCreator: { label: "OntoCreator", noSource: 1, controller: Lineage_createSLSVsource, toolDescriptionImg: null },
-};
+// tools are defined server side. See model/tools.js
+const allTools = {};
 
 async function loadToolsAndPlugins(callback) {
     const request = await fetch("../../api/v1/tools");
     const allowedTools = await request.json();
     const availableToolName = allowedTools.resources.map((tool) => tool.name);
     const plugins = allowedTools.resources.filter((element) => element.type === "plugin");
+
+    allowedTools.resources
+        .filter((element) => element.type == "tool")
+        .forEach((tool) => {
+            allTools[tool.name] = {
+                label: tool.label,
+                noSource: !+tool.useSource,
+                multiSources: +tool.multiSources,
+                controller: window[tool.controller],
+                toolDescriptionImg: null,
+                toTools: tool.toTools,
+            };
+        });
 
     // We import plugins and register them in the window
     for (const element of plugins) {

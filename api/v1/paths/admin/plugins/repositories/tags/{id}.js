cadde8d3c700cbfec9d4b83d17d02dbf111e4382
alubert <alubert@logilab.fr>
2024-08-06 09:50:12 +0200

    feat(api): add the routes to manage repositories of plugins

---
+++ api/v1/paths/admin/plugins/repositories/tags/{id}.js
@@ -0,0 +1,31 @@
+const { toolModel } = require("../../../../../../../model/tools");
+const { responseSchema } = require("../../../../utils");
+
+module.exports = function() {
+    const operations = { GET };
+
+    // GET /api/v1/admin/plugins/repositories/tags/{id}
+    async function GET(req, res, _next) {
+        try {
+            const repository = req.params.id;
+
+            const response = await toolModel.getRepositoryTags(repository);
+            if (response.status === "success") {
+                res.status(200).json({ message: response.message, status: 200 });
+            } else {
+                res.status(404).json({ message: "This repository do not exists", status: 404 });
+            }
+        } catch (error) {
+            res.status(500).json({ message: error.message, status: error.cause });
+        }
+    }
+
+    GET.apiDoc = {
+        operationId: "plugins.repositories.tags.get",
+        responses: responseSchema("PluginConfig", "GET"),
+        security: [{ restrictAdmin: [] }],
+        summary: "Retrieve the tags of the Git repository",
+    };
+
+    return operations;
+}
--- model/tools.js
+++ model/tools.js
@@ -1,9 +1,17 @@
+const fg = require("fast-glob");
 const fs = require("fs");
+const os = require("os");
 const path = require("path");
 const { Lock } = require("async-await-mutex-lock");
+const { simpleGit } = require("simple-git");
 
 const { convertType } = require("./utils");
-const { configPlugins } = require("./config");
+const {
+    configPluginsConfig,
+    configPluginsRepository,
+    directoryPlugins,
+    directoryPluginsRepositories
+} = require("./config");
 
 /**
  * @typedef {import("./ToolTypes").Tool} Tool
@@ -41,6 +49,18 @@ class ToolModel {
     }
 
     /**
+     * Get the tokenize version of the Git repository URL
+     *
+     * @param {string} url – the URL of the Git repository
+     * @param {string} token – the token used to authenticate on this repository
+     *
+     * @returns {string}
+     */
+    _getTokenizeURL(url, token) {
+        return `https://token:${token}@${url.replace(/^https?:\/\//, "")}`
+    }
+
+    /**
      * @param {string} pluginsDirectory
      * @returns {Tool[]}
      */
@@ -48,7 +68,7 @@ class ToolModel {
         /**
          * @type {Record<string, unknown>}
          */
-        const pluginsConfig = JSON.parse(fs.readFileSync(configPlugins).toString());
+        const pluginsConfig = this.readConfig();
         try {
             const pluginsNames = fs.readdirSync(pluginsDirectory);
             return pluginsNames
@@ -100,16 +120,238 @@ class ToolModel {
     };
 
     /**
+     * Remove a repository from the configuration file
+     *
+     * @param {string} repositoryId – The identifier of the repository to delete
+     */
+    deleteRepository = async (repositoryId) => {
+        const repositories = await this.readRepositories();
+
+        const filteredRepositories = Object.fromEntries(
+            Object.entries(repositories).filter(
+                ([identifier, data]) => identifier !== repositoryId
+            )
+        );
+
+        await this.writeRepositories(filteredRepositories);
+
+        fs.rmSync(path.join(directoryPlugins, repositoryId), { force: true });
+        fs.rmSync(path.join(directoryPluginsRepositories, repositoryId), { force: true, recursive: true });
+    };
+
+    /**
+     * Retrieve the Git repository related to the specified identifier
+     *
+     * @param {string} repositoryId – The identifier of the repository to fetch
+     * @param {{}} repositoryInfo – The information of the repository
+     *
+     * @returns {boolean} – The status of the fetch process
+     */
+    fetchRepository = async (repositoryId, repositoryInfo) => {
+        try {
+            if (!fs.existsSync(directoryPluginsRepositories)) {
+                fs.mkdirSync(directoryPluginsRepositories);
+            }
+
+            let url = repositoryInfo.url;
+            if (repositoryInfo.hasOwnProperty("token")) {
+                url = this._getTokenizeURL(url, repositoryInfo.token);
+            }
+
+            const repositoryPath = path.join(directoryPluginsRepositories, repositoryId);
+
+            if (!fs.existsSync(repositoryPath)) {
+                await simpleGit()
+                    .clone(url, repositoryPath);
+            } else {
+                await simpleGit(repositoryPath)
+                    .remote(["set-url", "origin", url])
+                    .pull();
+            }
+
+            if (repositoryInfo.hasOwnProperty("version")) {
+                await simpleGit(repositoryPath).checkout(repositoryInfo.version);
+            }
+        } catch (error) {
+            console.error(error);
+            return false;
+        }
+
+        return true;
+    };
+
+    /**
+     * Retrieve the list of available plugins in the Git repository
+     *
+     * When the Git repository is a plugin, the only entry returns by this
+     * method is the identifier of the repository.
+     *
+     * @param {string} repositoryId – The identifier of the Git repository
+     * @returns {[string]} – The list of plugins’ name
+     */
+    getRepositoryPlugins = async (repositoryId) => {
+        try {
+            const repositoryPath = path.join(directoryPluginsRepositories, repositoryId);
+
+            if (!fs.existsSync(repositoryPath)) {
+                return {
+                    message: "Cannot found the identifier in the plugins directory",
+                    status: "failure",
+                }
+            }
+
+            const mainJSFile = "/public/js/main.js"
+            const directories = await fg([`${repositoryPath}/**${mainJSFile}`]);
+
+            return {
+                message: directories.map(
+                    (directory) => path.basename(directory.replace(mainJSFile, ""))
+                ),
+                status: "success",
+            }
+        } catch (error) {
+            console.error(error);
+            return {
+                message: "An error occurs on the server",
+                status: "failure",
+            }
+        }
+    };
+
+    /**
+     * Retrieve the tag from the specified Git repository
+     *
+     * @param {string} repositoryId – The identifier of the Git repository
+     * @param {number} limit – The number of tag to retrieve (default: 5)
+     * @returns {[string]} – The list of Git tags
+     */
+    getRepositoryTags = async (repositoryId, limit = 5) => {
+        try {
+            const repositoryPath = path.join(directoryPluginsRepositories, repositoryId);
+
+            if (!fs.existsSync(repositoryPath)) {
+                return {
+                    message: "Cannot found the identifier in the plugins directory",
+                    status: "failure",
+                }
+            }
+
+            const tags = await simpleGit(repositoryPath).tags(["--sort=-refname"]);
+            return {
+                message: tags.all.slice(0, limit),
+                status: "success",
+            }
+        } catch (error) {
+            console.error(error);
+            return {
+                message: "An error occurs on the server",
+                status: "failure",
+            }
+        }
+    };
+
+    /**
+     * Read the plugins configuration from pluginsConfig.json file
+     *
+     * @returns {{}} plugins configuration
+     */
+    readConfig = () => {
+        try {
+            const fileContent = fs.readFileSync(configPluginsConfig).toString();
+            return JSON.parse(fileContent);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    /**
+     * Read the plugins repositories from plugins.json file
+     *
+     * @returns {Tool[]} plugins
+     */
+    readRepositories = () => {
+        try {
+            const fileContent = fs.readFileSync(configPluginsRepository).toString();
+            return JSON.parse(fileContent);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    /**
      * Write the plugins structure in the pluginsConfig.json file
      *
-     * @param {Tool[]} plugins – The plugins structure from pluginsConfig.json
+     * @param {{}} plugins – The plugins structure from pluginsConfig.json
      */
     writeConfig = async (plugins) => {
         await lock.acquire("PluginsThread");
 
         try {
             const convertedPlugins = await this.convertPluginsConfig(plugins);
-            await fs.promises.writeFile(configPlugins, JSON.stringify(convertedPlugins, null, 2));
+            await fs.promises.writeFile(configPluginsConfig, JSON.stringify(convertedPlugins, null, 2));
+        } catch (error) {
+            console.error(error);
+        } finally {
+            lock.release("PluginsThread");
+        }
+    };
+
+    /**
+     * Write the repositories structure in the plugins.json file
+     *
+     * @param {{}} repositories – The repositories structure from plugins.json
+     */
+    writeRepositories = async (repositories) => {
+        await lock.acquire("PluginsThread");
+
+        try {
+            await fs.promises.writeFile(configPluginsRepository, JSON.stringify(repositories, null, 2));
+
+            const currentPlugins = this.plugins.map((plugin) => plugin.name);
+
+            const nextPlugins = Object.entries(repositories).map(([identifier, data]) => {
+                if (data.plugins === undefined) {
+                    return path.parse(data.url).name;
+                }
+                return data.plugins;
+            }).flat();
+
+            // Remove useless symlinks
+            currentPlugins.forEach((plugin) => {
+                if (!nextPlugins.includes(plugin)) {
+                    const pluginPath = path.join(directoryPlugins, plugin);
+
+                    const stats = fs.lstatSync(pluginPath, { throwIfNoEntry: false });
+                    if (stats !== undefined && stats.isSymbolicLink()) {
+                        fs.rmSync(pluginPath);
+                    }
+                }
+            });
+
+            // Add the new plugin symlinks if necessary
+            Object.entries(repositories).map(([identifier, data]) => {
+                try {
+                    if (data.plugins === undefined) {
+                        const pluginPath = path.join(directoryPlugins, path.parse(data.url).name);
+                        if (!fs.existsSync(pluginPath)) {
+                            fs.symlinkSync(path.resolve(path.join(directoryPluginsRepositories, identifier)), pluginPath);
+                        }
+                    } else {
+                        data.plugins.forEach((plugin) => {
+                            try {
+                                const pluginPath = path.join(directoryPlugins, plugin);
+                                if (!fs.existsSync(pluginPath)) {
+                                    fs.symlinkSync(path.resolve(path.join(directoryPluginsRepositories, identifier, plugin)), pluginPath);
+                                }
+                            } catch (error) {
+                                console.error(identifier, error);
+                            }
+                        });
+                    }
+                } catch (error) {
+                    console.error(identifier, error);
+                }
+            });
         } catch (error) {
             console.error(error);
         } finally {

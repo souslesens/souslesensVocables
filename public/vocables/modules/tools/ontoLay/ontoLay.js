3c83f1b7ff04437f692ee5713bf77dd022983cb1
OUNNOUGHI01~ <ounnoughikarim@gmail.com>
2024-11-18 16:21:24 +0100

    containers tree classes not displayed

--- public/vocables/modules/tools/ontoLay/ontoLay.js
+++ public/vocables/modules/tools/ontoLay/ontoLay.js
@@ -5,7 +5,7 @@ import MainController from "../../shared/mainController.js";
 var OntoLay = (function () {
     var self = {}
        self.maxClasses = 100;
-
+    self.currentTab='Class'
     self.onLoaded = function () {
         if (self.firstLoad) {
             self.firstLoad = false;
@@ -32,21 +32,23 @@ var OntoLay = (function () {
             $("#lateralPanelDiv").load("./modules/tools/OntoLay/html/lateralPanel.html", function () {
                 Lineage_whiteboard.initWhiteboardTab();
                 Lineage_whiteboard.initUI();
-
-                self.getTopClasses(Lineage_sources.activeSource, {},function (err, result) {
-                    if (err) {
-                        alert(err)
-                    }
-                    self.nodeIds = result.nodeIds
-                    self.currentDepth = result.currentDepth
-
-
-                self.drawTopClasses ( self.nodeIds, self.currentDepth)
-                })
+                self.loadTopClasses();
+                
             });
         })
     }
+    self.loadTopClasses=function(){
+        self.getTopClasses(Lineage_sources.activeSource, {},function (err, result) {
+            if (err) {
+                alert(err)
+            }
+            self.nodeIds = result.nodeIds
+            self.currentDepth = result.currentDepth
 
+
+        self.drawTopClasses ( self.nodeIds, self.currentDepth)
+        });
+    }
 
 
 self.getTopClasses = function (sourceLabel, options, callback) {
@@ -167,12 +169,19 @@ self.getTopClasses = function (sourceLabel, options, callback) {
 
         $("#classesTab").css("display","none")
         $("#propertiesTab").css("display","none")
-
+        if(!type){
+            type=self.currentTab;
+        }else{
+            self.currentTab=type;
+        }
+        
         if(type=="Class"){
+
             $("#classesTab").css("display","block")
             var options={
                 term:term,
-                searchedSources:true
+                searchedSources:[Lineage_sources.activeSource],
+                jstreeDiv: "LineageNodesJsTreeDiv"
             }
 
             SearchWidget.searchTermInSources(options)
@@ -180,13 +189,13 @@ self.getTopClasses = function (sourceLabel, options, callback) {
 
         else if(type=="Property"){
             $("#propertiesTab").css("display","block")
-            Lineage_properties.searchTermInSources(term,true)
+            Lineage_properties.searchTermInSources(term,true,false,"property")
         }
-        else if(type=="Whitboard"){
+        else if(type=="Whiteboard"){
 
             Lineage_whiteboard.graph.searchNode(null, term)
         }
-
+        
 
 
 
@@ -196,7 +205,10 @@ self.getTopClasses = function (sourceLabel, options, callback) {
 
     }
 
-
+    self.clearAll=function(){
+        Lineage_whiteboard.initUI();
+        self.loadTopClasses();
+    }
     return self;
 
 

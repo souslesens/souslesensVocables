bc626579dcb7bd7c4bd983305f3d7a154d12b8d8
alubert <alubert@logilab.fr>
2024-12-02 14:27:28 +0100

    chore(mainConfig): remove authenticationDatabase
    This section will be used to managed the database where all the data (sources,
    profiles, etc) will be stored.

--- model/config.js
+++ model/config.js
@@ -26,7 +26,7 @@ const MainConfigObject = z
                 defaultTheme: z.string(),
             })
             .strict(),
-        auth: z.enum(["local", "disabled", "keycloak", "auth0"]),
+        auth: z.enum(["local", "database", "disabled", "keycloak", "auth0"]),
         cookieSameSite: z.string(),
         cookieSecure: z.boolean(),
         cookieSecureTrustProxy: z.boolean(),
@@ -97,24 +97,11 @@ const MainConfigObject = z
             .strict(),
         database: z
             .object({
-                host: z.string(),
-                port: z.number().positive().max(65535),
-                database: z.string(),
-                user: z.string(),
-                password: z.string(),
-            })
-            .strict(),
-        authenticationDatabase: z
-            .object({
                 user: z.string(),
                 password: z.string(),
                 host: z.string(),
                 database: z.string(),
                 port: z.number().positive().max(65535),
-                table: z.string(),
-                loginColumn: z.string(),
-                passwordColumn: z.string(),
-                groupsColumn: z.string(),
             })
             .strict(),
         annotator: z

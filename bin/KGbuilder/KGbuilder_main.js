9915504f6db5b7d3a43eb9ac5234ada9ce7d8a91
Claude Fauconnet <claude.fauconnet@gmail.com>
2025-01-14 18:32:50 +0100

    corrections in Dexpi process for Guillaume

--- bin/KGbuilder/KGbuilder_main.js
+++ bin/KGbuilder/KGbuilder_main.js
@@ -218,66 +218,66 @@ var KGbuilder_main = {
 
             async.series([
 // interruption
-                function(callbackSeries) {
-                    if (KGbuilder_main.stopCreateTriples) {
-                        var message = "mapping " + mappings.table + " : import interrupted by user";
-                        KGbuilder_socket.message(options.clientSocketId, message);
-                        return callbackSeries(message);
-                    }
-                    return callbackSeries();
-                },
-                //set triples
-                function(callbackSeries) {
-                    options.customMetaData = { [KGbuilder_triplesMaker.mappingFilePredicate]: mappings.table };
-                    KGbuilder_triplesMaker.createTriples(mappings, dataSlice, options, function(err, result) {
-                        if (err) {
-                            return callbackSeries(err);
+                    function(callbackSeries) {
+                        if (KGbuilder_main.stopCreateTriples) {
+                            var message = "mapping " + mappings.table + " : import interrupted by user";
+                            KGbuilder_socket.message(options.clientSocketId, message);
+                            return callbackSeries(message);
                         }
-                        triples = result;
+                        return callbackSeries();
+                    },
+                    //set triples
+                    function(callbackSeries) {
+                        options.customMetaData = { [KGbuilder_triplesMaker.mappingFilePredicate]: mappings.table };
+                        KGbuilder_triplesMaker.createTriples(mappings, dataSlice, options, function(err, result) {
+                            if (err) {
+                                return callbackSeries(err);
+                            }
+                            triples = result;
 
+                            callbackSeries();
+
+
+                        });
+                    },
+
+
+                    //add metadata
+                    function(callbackSeries) {
+
+                        triples.forEach(function(triple) {
+                            if (!uniqueSubjects[triple.s]) {
+                                uniqueSubjects[triple.s] = 1;
+
+                                triples = triples.concat(KGbuilder_triplesMaker.getMetaDataTriples(triple.s, { mappingTable: mappings.table }));
+                            }
+                        });
                         callbackSeries();
+                    }
 
+                    ,
 
-                    });
-                },
-
-
-                //add metadata
-                function(callbackSeries) {
-
-                    triples.forEach(function(triple) {
-                        if (!uniqueSubjects[triple.s]) {
-                            uniqueSubjects[triple.s] = 1;
-
-                            triples = triples.concat(KGbuilder_triplesMaker.getMetaDataTriples(triple.s, { mappingTable: mappings.table }));
-                        }
-                    });
-                    callbackSeries();
-                }
-
-                ,
-
-                //writeTriples
-                function(callbackSeries) {
-                   // KGbuilder_socket.message(options.clientSocketId, "table " + mappings.table + " : writing triples:" + triples.length);
+                    //writeTriples
+                    function(callbackSeries) {
+                        // KGbuilder_socket.message(options.clientSocketId, "table " + mappings.table + " : writing triples:" + triples.length);
 
 //return callbackSeries();
-                    KGbuilder_triplesWriter.writeTriples(triples, mappings.graphUri, mappings.sparqlServerUrl, function(err, result) {
-                        if (err) {
-                            var error = " slice " + sliceIndex + "/" + slices.length + "\n";
-                            KGbuilder_socket.message(options.clientSocketId, error);
-                            return callbackEach(err);
-                        }
-                        sliceIndex += 1;
-                        totalTriples += result;
-                        KGbuilder_socket.message(options.clientSocketId, "table " + mappings.table + " : writen triples:" + totalTriples);
+                        KGbuilder_triplesWriter.writeTriples(triples, mappings.graphUri, mappings.sparqlServerUrl, function(err, result) {
+                            if (err) {
+                                var error = " slice " + sliceIndex + "/" + slices.length + "\n";
+                                KGbuilder_socket.message(options.clientSocketId, error);
+                                return callbackEach(err);
+                            }
+                            sliceIndex += 1;
+                            totalTriples += result;
+                            KGbuilder_socket.message(options.clientSocketId, "table " + mappings.table + " : writen triples:" + totalTriples);
 
-                        callbackSeries();
-                    });
-                }],
+                            callbackSeries();
+                        });
+                    }],
                 function(err) {
-                callbackEach(err)
-            })
+                    callbackEach(err)
+                })
         }, function(err) {
             return callback(err, totalTriples);
         });
@@ -288,10 +288,14 @@ var KGbuilder_main = {
     ,
 
     getSourceConfig: function(source, callback) {
-        var sourceMappingsDir = path.join(__dirname, "../../data/mappings/" + source + "/");
+        // var sourceMappingsDir = path.join(__dirname, "../../data/mappings/" + source + "/");
+
+
         try {
-            var mainJsonPath = sourceMappingsDir + "main.json";
-            var sourceMainJson = JSON.parse("" + fs.readFileSync(mainJsonPath));
+            // var mainJsonPath = sourceMappingsDir + "main.json";
+            var mainJsonPath= path.join(__dirname,"../../data/graphs/mappings_" + source + "_ALL" + ".json")
+            var visjsMappingsJson = JSON.parse("" + fs.readFileSync(mainJsonPath));
+            var sourceMainJson =visjsMappingsJson.options.config
             if (sourceMainJson.sparqlServerUrl == "_default") {
                 sourceMainJson.sparqlServerUrl = ConfigManager.config.sparql_server.url;
             }
@@ -327,12 +331,13 @@ var KGbuilder_main = {
                 // read datasourceMappings
                 function(callbackSeries) {
                     try {
-                        var dataSourceMappingsPath = sourceMappingsDir + datasource + ".json";
+
                         var mappings;
                         if (options.mappingsFilter) {
-                          //  mappings = JSON.parse(options.mappingsFilter);
+                            //  mappings = JSON.parse(options.mappingsFilter);
                             mappings = options.mappingsFilter;
                         } else {
+                            var dataSourceMappingsPath = sourceMappingsDir + datasource + ".json";
                             mappings = JSON.parse("" + fs.readFileSync(dataSourceMappingsPath));
                         }
                         dataSourceMappings.mappings = mappings;
@@ -380,7 +385,7 @@ var KGbuilder_main = {
                 }
             ],
             function(err) {
-                return callback(null, tableMappingsToProcess);
+                return callback(err, tableMappingsToProcess);
             }
         );
     }

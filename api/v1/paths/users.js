116e076b8d6dbdff87bc0ce405a6a64fcc6f03c3
Viet-Hung NHU <viet-hung.nhu@logilab.fr>
2025-01-27 11:22:34 +0100

    feat(api/user) Add user api with profile

---
+++ api/v1/paths/users.js
@@ -0,0 +1,44 @@
+const { userModel } = require("../../../model/users");
+const { sortObjectByKey, responseSchema, successfullyFetched } = require("./utils");
+const { profileModel } = require("../../../model/profiles");
+const userManager = require("../../../bin/user.");
+
+module.exports = function () {
+    let operations = {
+        GET,
+    };
+
+    /**
+     * Filter user by his profiles
+     *
+     * @param {users} users - the all users to filter
+     * @param {profiles} the group to filter
+     * @returns {users} - user objects with matched profiles
+     */
+    filterUserByGroup = (users, profiles) => {
+        const userProfiles = Object.fromEntries(Object.entries(users).filter(([key, value]) => value.groups.some((g) => profiles.includes(g))));
+        return Object.entries(userProfiles).map(([key, value]) => ({ [key]: { login: value.login, profiles: value.groups } }));
+    };
+    ///// GET api/v1/users
+    async function GET(req, res, next) {
+        try {
+            const userInfo = await userManager.getUser(req.user);
+            const profiles = await profileModel.getUserProfiles(userInfo.user);
+            const allUsers = await userModel.getUserAccounts();
+            const profileNames = await [...new Set(Object.values(profiles).map((item) => item.name))];
+            const userProfiles = await this.filterUserByGroup(allUsers, profileNames);
+            res.status(200).json(successfullyFetched(sortObjectByKey(userProfiles)));
+        } catch (error) {
+            next(error);
+        }
+    }
+    GET.apiDoc = {
+        summary: "Return a list of all User Accounts in the same profiles",
+        security: [{ restrictLoggedUser: [] }],
+        operationId: "getAllUsersWithProfiles",
+        parameters: [],
+        responses: responseSchema("Users", "GET"),
+    };
+
+    return operations;
+};

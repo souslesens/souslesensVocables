984bfb69d94ed7044ded803fce28c39e7e6b3af0
alubert <alubert@logilab.fr>
2025-01-30 11:49:31 +0100

    test(model): convert model.users.test to the database format

--- tests/model.users.test.js
+++ tests/model.users.test.js
@@ -1,167 +1,175 @@
-const fs = require("fs");
 const bcrypt = require("bcrypt");
+const fs = require("fs");
 const path = require("path");
-const { UserModelJson } = require("../model/users");
 const tmp = require("tmp");
 
+const { createTracker, MockClient } = require("knex-mock-client");
+
+const { cleanupConnection, getKnexConnection } = require("../model/utils");
+const { userModel } = require("../model/users");
+
+
+jest.mock("../model/utils", () => {
+    const knex = require("knex");
+    return {
+        cleanupConnection: jest.fn().mockReturnThis(),
+        getKnexConnection: knex({ client: MockClient, dialect: "pg" }),
+    };
+});
+
+
 describe("UserModelJson", () => {
-    /**
-     * @type {UserModelJson}
-     */
-    let userModelJson;
+    let tracker;
+    let dbUsers;
+
     beforeAll(() => {
-        userModelJson = new UserModelJson(path.join(__dirname, "data/config/users/users.json"));
+        tracker = createTracker(getKnexConnection);
+
+        dbUsers = JSON.parse(fs.readFileSync(
+            path.join(__dirname, "data", "config", "users", "users.json")
+        ));
+    });
+
+    afterEach(() => {
+        tracker.reset();
     });
 
     test("read the list of users with get()", async () => {
-        const users = await userModelJson.getUserAccounts();
+        tracker.on.select("public_users_list").response(dbUsers);
+
+        const users = await userModel.getUserAccounts();
         expect(users).toStrictEqual({
             admin: {
-                id: "admin",
+                id: "1",
                 login: "admin",
+                password: "$2b$10$LhdaXF3tHrvA8b8fKMvNmeOSyogCBgN/L9XLN/vqjz6tgN5rSxMyy",
+                token: "admin-token",
                 groups: ["admin"],
-                source: "json",
-                _type: "user",
                 allowSourceCreation: false,
                 maxNumberCreatedSource: 5,
+                source: "database",
             },
             owl_user: {
-                id: "owl_user",
+                id: "2",
                 login: "owl_user",
+                password: "$2b$10$ed6ZDHUbN7NfOGrufiDniudK9EMLCFg9qFgJ0N/T.nRTBPlA5Mm9C",
+                token: "owl-token",
                 groups: ["owl_only"],
-                source: "json",
-                _type: "user",
                 allowSourceCreation: false,
                 maxNumberCreatedSource: 5,
+                source: "database",
             },
             skos_user: {
-                id: "skos_user",
+                id: "3",
                 login: "skos_user",
+                password: "",
+                token: "skos-token",
                 groups: ["skos_only"],
-                source: "json",
-                _type: "user",
                 allowSourceCreation: false,
                 maxNumberCreatedSource: 5,
+                source: "database",
             },
         });
     });
+
     test("find a user with findUserAccount()", async () => {
-        const users = await userModelJson.findUserAccount("admin");
-        expect(users).toStrictEqual({
-            id: "admin",
-            login: "admin",
-            groups: ["admin"],
-            source: "json",
-            token: "admin-token",
-            _type: "user",
-            allowSourceCreation: false,
-            maxNumberCreatedSource: 5,
-        });
+        tracker.on.select("users").response(dbUsers[0]);
+
+        const user = await userModel.findUserAccount("admin");
+        expect(user).toStrictEqual([
+            "admin",
+            {
+                id: "1",
+                login: "admin",
+                password: "$2b$10$LhdaXF3tHrvA8b8fKMvNmeOSyogCBgN/L9XLN/vqjz6tgN5rSxMyy",
+                groups: ["admin"],
+                source: "database",
+                token: "admin-token",
+                allowSourceCreation: false,
+                maxNumberCreatedSource: 5,
+            }
+        ]);
     });
+
     test("fail to find a user with findUserAccount()", async () => {
-        const emptyArray = await userModelJson.findUserAccount("unknown");
+        tracker.on.select("users").response(undefined);
+
+        const emptyArray = await userModel.findUserAccount("unknown");
         expect(emptyArray).toStrictEqual(undefined);
     });
+
     test("check user and password match with checkUserPassword()", async () => {
-        let goodpass = await userModelJson.checkUserPassword("admin", "pass");
+        tracker.on.select("users").response(dbUsers[0]);
+
+        const goodpass = await userModel.checkUserPassword("admin", "pass");
         expect(goodpass).toStrictEqual(true);
-        let badpass = await userModelJson.checkUserPassword("admin", "badpassword");
+        const badpass = await userModel.checkUserPassword("admin", "badpassword");
         expect(badpass).toStrictEqual(false);
-        let nouser = await userModelJson.checkUserPassword("unknown", "pass");
-        expect(nouser).toStrictEqual(false);
-        let emptypassword = await userModelJson.checkUserPassword("skos_user", "pass");
-        expect(emptypassword).toStrictEqual(false);
-        emptypassword = await userModelJson.checkUserPassword("skos_user", "");
-        expect(emptypassword).toStrictEqual(false);
     });
-    test("append a new user with add()", async () => {
-        tmpDir = tmp.dirSync({ unsafeCleanup: true });
-        fs.mkdirSync(path.join(tmpDir.name, "users"));
-        const usersPath = path.join(tmpDir.name, "users", "users.json");
-        fs.writeFileSync(usersPath, "{}");
-        const tmpUserModelJson = new UserModelJson(usersPath);
-        const newUser = {
-            id: "ID",
-            login: "LOGIN",
+
+    test("check unknown user with checkUserPassword()", async () => {
+        tracker.on.select("users").response(undefined);
+
+        const nouser = await userModel.checkUserPassword("unknown", "pass");
+        expect(nouser).toStrictEqual(false);
+    });
+
+    test("check user with empty password with checkUserPassword()", async () => {
+        tracker.on.select("users").response(dbUsers[2]);
+
+        let emptypassword = await userModel.checkUserPassword("skos_user", "pass");
+        expect(emptypassword).toStrictEqual(false);
+        emptypassword = await userModel.checkUserPassword("skos_user", "");
+        expect(emptypassword).toStrictEqual(true);
+    });
+
+    test("add a new user with addUserAccount()", async () => {
+        const addedUser = {
+            id: "42",
+            login: "login",
             password: "pass",
             groups: [],
             source: "",
-            _type: "user",
             allowSourceCreation: false,
             maxNumberCreatedSource: 5,
         };
-        const expected = {
-            id: "ID",
-            login: "LOGIN",
-            groups: [],
-            source: "",
-            _type: "user",
-            allowSourceCreation: false,
-            maxNumberCreatedSource: 5,
-        };
-        await tmpUserModelJson.addUserAccount(newUser);
-        const users = await tmpUserModelJson.getUserAccounts();
-        expect(users).toStrictEqual({ LOGIN: expected });
-        tmpDir.removeCallback();
+
+        tracker.on.select("users").response(undefined); // Unknown user
+        tracker.on.insert("users").response([]);
+        await userModel.addUserAccount(addedUser);
+
+        const updatedUsers = Array.from(dbUsers);
+        updatedUsers.push(userModel._convertToDatabase(addedUser));
+
+        tracker.reset();
+        tracker.on.select("public_users_list").response(updatedUsers);
+        const users = await userModel.getUserAccounts();
+        expect(users.login).toStrictEqual({
+            "id": "undefined",  // This is managed by Postgres
+            "login": "login",
+            "password": "pass",
+            "token": "",
+            "groups": [],
+            "source": "database",
+            "allowSourceCreation": false,
+            "maxNumberCreatedSource": 5,
+        });
     });
+
     test("update an existing user with update()", async () => {
-        const USERS = {
-            LOGIN1: {
-                id: "ID1",
-                login: "LOGIN1",
-                password: "pass",
-                groups: [],
-                source: "",
-                _type: "user",
-                allowSourceCreation: false,
-                maxNumberCreatedSource: 5,
-            },
-            LOGIN2: {
-                id: "ID2",
-                login: "LOGIN2",
-                password: "pass",
-                groups: [],
-                source: "",
-                _type: "user",
-                allowSourceCreation: false,
-                maxNumberCreatedSource: 5,
-            },
-        };
-        tmpDir = tmp.dirSync({ unsafeCleanup: true });
-        fs.mkdirSync(path.join(tmpDir.name, "users"));
-        const usersPath = path.join(tmpDir.name, "users", "users.json");
-        fs.writeFileSync(usersPath, JSON.stringify(USERS));
-        const tmpUserModelJson = new UserModelJson(usersPath);
-        const modifiedUser = {
-            id: "ID1",
-            login: "LOGIN1",
-            groups: ["test"],
-            source: "",
-            _type: "user",
-            allowSourceCreation: false,
-            maxNumberCreatedSource: 5,
-        };
-        const expectedModifedUser1 = {
-            id: "ID1",
-            login: "LOGIN1",
-            groups: ["test"],
-            source: "",
-            _type: "user",
-            allowSourceCreation: false,
-            maxNumberCreatedSource: 5,
-        };
-        const user2WithoutPass = {
-            id: "ID2",
-            login: "LOGIN2",
+        const addedUser = {
+            id: "42",
+            login: "owl_user",
+            password: "pass",
             groups: [],
             source: "",
-            _type: "user",
             allowSourceCreation: false,
             maxNumberCreatedSource: 5,
         };
-        await tmpUserModelJson.updateUserAccount(modifiedUser);
-        const users = await tmpUserModelJson.getUserAccounts();
-        expect(users).toStrictEqual({ LOGIN1: expectedModifedUser1, LOGIN2: user2WithoutPass });
-        tmpDir.removeCallback();
+
+        tracker.on.select("users").response(dbUsers[1]); // Existing user
+        expect(async () => {
+            await userModel.addUserAccount(addedUser)
+        }).rejects.toThrow();
     });
 });

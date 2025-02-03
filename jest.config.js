6806980996c28115d06003576c21ba5a383c3197
Olivier Giorgis <ogiorgis@logilab.fr>
2025-02-03 15:40:39 +0100

    feat: add jest-coverage github action

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  setupFiles: ["<rootDir>/environment.js"]
+    setupFiles: ["<rootDir>/environment.js"],
+    coverageThreshold: {
+        global: {
+            lines: 70,
+            statements: 70,
+            branches: 70,
+            functions: 70,
+        },
+    },
 };

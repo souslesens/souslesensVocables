b34fd0d3cea687bd8ccf55ba76169d6e6bc5ea06
Olivier Giorgis <ogiorgis@logilab.fr>
2025-02-28 16:55:04 +0100

    fix: correct some eslint errors

--- model/userData.js
+++ model/userData.js
@@ -107,7 +107,7 @@ class UserDataModel {
     };
 
     update = async (userData) => {
-        if (userData.hasOwnProperty("created_at")) {
+        if (Object.hasOwn(userData, "created_at")) {
             delete userData.created_at;
         }
         const data = this._check(userData);

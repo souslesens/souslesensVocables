02c309de760830566a63caa8ba4993f4247a209c
OUNNOUGHI01~ <ounnoughikarim@gmail.com>
2025-03-28 15:31:05 +0100

    Draw graph even if there is no nodes on it

---
+++ public/vocables/modules/uiWidgets/importFileWidget.js
@@ -0,0 +1,37 @@
+var ImportFileWidget = (function () {
+    var self = {};
+
+    self.showImportDialog=function(callback){
+     
+        $('#smallDialogDiv').load('modules/uiWidgets/html/importDialog.html' , function () {
+                $("#smallDialogDiv").dialog("open");
+                $('#importFileButton').on('click', function () {
+
+                    const file = $("#importFileInput")[0].files[0];
+                    if(!file){
+                        return alert('no file selected');
+                    }
+                    if (file && file.type !== "application/json") {
+                        return alert("Select valid JSON file !");
+
+                    }
+                    const reader = new FileReader();
+                    reader.onload = function (e) {
+                        var fileContent = e.target.result;
+                        $('#smallDialogDiv').dialog('close');
+                        if(callback){
+                            callback(null,fileContent);
+                        }
+                    }
+                    reader.readAsText(file);
+                    
+                    
+                });
+
+        });
+    }
+    return self;
+})();
+
+export default ImportFileWidget;
+window.ImportFileWidget = ImportFileWidget;

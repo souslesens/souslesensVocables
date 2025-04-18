9fb74834f49c18c36aa79c558c4a3a6d44f80439
OUNNOUGHI01~ <ounnoughikarim@gmail.com>
2025-04-18 14:24:59 +0200

    Test for import graphURI for OntoCreator

--- public/vocables/modules/bots/createSLSVsource_bot.js
+++ public/vocables/modules/bots/createSLSVsource_bot.js
@@ -33,12 +33,32 @@ var CreateSLSVsource_bot = (function () {
             "Create source": { saveFn: self.workflowUpload },
         },
     };
+    self.workflow2withoutUpload={
+        _OR: {
+                "Add import": { listImportsFn: { afterImportFnWithoutUpload: {} } },
+                "Create source": {  getURIFromUpload:{fillParamsFromUpload:{Finish: self.loadingWorkflow}} },
+        }
+        
+        
+    }
 
     self.workflow = {
         promptSourceNameFn: {
-            promptGraphUriFn: { validateGraphUriFn: self.workflow2 },
+             _OR:{
+                    'Create source from upload':{
+                        saveUploadSource:{
+                            _OR: {
+                                "Upload graph from file": { uploadFromFileFn: self.workflow2withoutUpload },
+                                "Upload graph from URL": { uploadFromUrlFn: self.workflow2withoutUpload }
+                            },
+
+                        }
+                    },
+                    'Define new source':{ promptGraphUriFn:{validateGraphUriFn: self.workflow2}}
+            }
         },
     };
+  
 
     self.functionTitles = {
         promptSourceNameFn: "Enter source label",
@@ -89,6 +109,12 @@ var CreateSLSVsource_bot = (function () {
             _botEngine.currentObj = self.workflow2;
             _botEngine.nextStep(self.workflow2);
         },
+        afterImportFnWithoutUpload: function () {
+             //_botEngine.previousStep();
+             _botEngine.currentObj = self.workflow2withoutUpload;
+             _botEngine.nextStep(self.workflow2withoutUpload);
+        },
+
 
         uploadFromUrlFn: function () {
             _botEngine.promptValue("enter remote Url", "uploadUrl", "", null, function (value) {
@@ -133,7 +159,83 @@ var CreateSLSVsource_bot = (function () {
                 },
             );
         },
+        saveUploadSource: function(){
+            async.series(
+                [
+                    function (callbackSeries) {
+                        var randomID=common.getRandomHexaId(8);
+                        var url='http://temporary.graphUri.'+self.params.sourceLabel+randomID+'/'
+                        self.params.graphUri=url;
+                        Lineage_createSLSVsource.createSource(self.params.sourceLabel, url, null, function (err, result) {
+                            if (err) {
+                                callbackSeries(err);
+                            }
 
+                            callbackSeries();
+                        });
+                    },
+                ],
+                function (err) {
+                    if (err) {
+                        alert(err.responseText);
+                        return _botEngine.reset();
+                    }
+                    return _botEngine.nextStep();
+                },
+            );
+        },
+        
+        getURIFromUpload: function(){
+            var query=`PREFIX owl: <http://www.w3.org/2002/07/owl#>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            SELECT * FROM <${self.params.graphUri}>   WHERE{
+            ?sub ?pred owl:Ontology .
+            } `;
+            SparqlProxy.querySPARQL_GET_proxy('default',query,null, null, function(err,result){
+                if(result?.results?.bindings?.length>0){
+                    var graphUri=result.results.bindings[0].sub.value;
+                    self.params.graphUri=graphUri;
+                }
+                if(!graphUri){
+                    alert('graphUri not found in the source');
+                    // Enter it manually and continue worflow
+                    return _botEngine.promptValue('enter manually graphUri', 'graphUri', self.params.graphUri, null, function (value) {
+                        if (!value) {
+                            alert("enter a value ");
+                            return _botEngine.previousStep();
+                        }
+                        self.params.graphUri=value;
+                        _botEngine.nextStep();
+                        
+                    });
+                }else{
+                    
+                    _botEngine.nextStep();
+                }
+                  
+            });
+        },
+        fillParamsFromUpload: function(){
+          var sourceConfig=Config.sources[self.params.sourceLabel];
+          sourceConfig.graphUri=self.params.graphUri;
+          sourceConfig.imports=self.params.imports;
+            $.ajax({
+                type: "PUT",
+                url: `${Config.apiUrl}/sources/${self.params.sourceLabel}`,
+                data: JSON.stringify(sourceConfig),
+                contentType: "application/json",
+                dataType: "json",
+                success: function (data, _textStatus, _jqXHR) {
+                    return _botEngine.nextStep();
+                },
+                error: function (err) {
+                    return alert(err.responseText);
+                    
+                },
+            });
+
+        },
         loadLineageFn: function () {
             var url = window.location.href;
             url = url.replace("index_old.html", "");

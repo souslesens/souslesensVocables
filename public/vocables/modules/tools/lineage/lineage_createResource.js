3ad487c9604aefce49a8505eb354f2c8b91694ac
OUNNOUGHI01~ <ounnoughikarim@gmail.com>
2025-05-23 15:25:28 +0200

    bug fix : reset Add predicates values

--- public/vocables/modules/tools/lineage/lineage_createResource.js
+++ public/vocables/modules/tools/lineage/lineage_createResource.js
@@ -440,6 +440,9 @@ var Lineage_createResource = (function () {
      * @memberof Lineage_createResource
      */
     self.startCreateRessourceBot = function () {
+        // reset precendent values to not add triples to another resource
+        $('#editPredicate_objectValue').val('');
+        $('#editPredicate_propertyValue').val('');
         CreateResource_bot.start(null, { source: Lineage_sources.activeSource }, function (err, result) {});
     };
 

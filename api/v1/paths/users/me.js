f12e139626167ca8c8135d52938e1b764f790c11
xgaia <xgaia@gmx.com>
2025-07-18 13:54:23 +0200

    fix(api): fix /users/me route for disabled auth

--- api/v1/paths/users/me.js
+++ api/v1/paths/users/me.js
@@ -1,4 +1,5 @@
 const { userModel } = require("../../../../model/users");
+const { mainConfigModel } = require("../../../../model/mainConfig");
 
 module.exports = function () {
     let operations = {
@@ -7,11 +8,22 @@ module.exports = function () {
 
     async function GET(req, res, _next) {
         try {
+            const config = await mainConfigModel.getConfig();
+            if (config.auth === "disabled") {
+                res.status(200).json({
+                    id: "0",
+                    login: "admin",
+                    groups: ["admin"],
+                    allowSourceCreation: true,
+                    maxNumberCreatedSource: 1000,
+                });
+                return;
+            }
             const user = req.user;
             delete user.password;
             delete user.token;
             delete user.source;
-            res.status(404).json(user);
+            res.status(200).json(user);
         } catch (error) {
             console.error(error);
             res.status(500).json({ message: "An error occurs on the server" });

6fa819573558f1e0eddb0ee44ce5efba70aef509
Claude Fauconnet <claude.fauconnet@gmail.com>
2025-07-22 16:04:21 +0200

    fix bug in sparql_OWL.js

--- public/vocables/modules/sparqlProxies/sparql_OWL.js
+++ public/vocables/modules/sparqlProxies/sparql_OWL.js
@@ -776,7 +776,7 @@ var Sparql_OWL = (function () {
 
         //   query+="filter(!isBlank(?superClassSubClass))"
 
-     //   query += "}}} LIMIT 10000";
+    query += "}}} LIMIT 10000";
 
         var url = Config.sources[sourceLabel].sparql_server.url + "?format=json&query=";
         self.no_params = true;

91bc53085d2ced4b85d92bf91b45d089432fe6fa
xgaia <xgaia@gmx.com>
2025-09-22 11:11:50 +0200

    feat(utils): add a addFromsToSparqlQuery function

--- tests/model.utils.test.js
+++ tests/model.utils.test.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { jest } from "@jest/globals";
 
 import { convertType, chunk, cleanupConnection, getKnexConnection, redoIfFailure } from "../model/utils.js";
@@ -16,6 +17,10 @@ describe("redoIfFailure", () => {
         expect(ntry).toBe(4);
     });
 });
+=======
+const { convertType, chunk, cleanupConnection, getKnexConnection, addFromsToSparqlQuery } = require("../model/utils");
+const fs = require("node:fs");
+>>>>>>> f07e45c87 (feat(utils): add a addFromsToSparqlQuery function)
 
 describe("convertTypeUtils", () => {
     test("Convert string to boolean", async () => {
@@ -76,5 +81,38 @@ describe("knex functions", () => {
         const connection = { destroy: jest.fn() };
         cleanupConnection(connection);
         expect(connection.destroy).toHaveBeenCalled();
+    });
+});
+
+describe("addFromToSparqlQuery", () => {
+    test("add FROM to a simple SPARQL query", async () => {
+        const sparql = "SELECT * WHERE { ?s ?p ?o .}";
+        const expected = "SELECT * FROM <http://toto.com/example>\nWHERE { ?s ?p ?o. }";
+        expect(addFromsToSparqlQuery(sparql, ["http://toto.com/example"])).toStrictEqual(expected);
+    });
+    test("add FROM to a complexe SPARQL query", async () => {
+        const sparql = fs.readFileSync("./tests/data/sparql/bsbm4.sparql", "utf8");
+        const expected = fs.readFileSync("./tests/data/sparql/bsbm4_FROM.sparql", "utf8").slice(0, -1); // slice to remove last \n
+        expect(addFromsToSparqlQuery(sparql, ["http://toto.com/example"])).toStrictEqual(expected);
+    });
+    test("replace FROM", async () => {
+        const sparql = "SELECT * FROM <urn:a:b:c> FROM <urn:d:e:f> WHERE { ?s ?p ?o .}";
+        const expected = "SELECT * FROM <http://toto.com/example>\nWHERE { ?s ?p ?o. }";
+        expect(addFromsToSparqlQuery(sparql, ["http://toto.com/example"], true)).toStrictEqual(expected);
+    });
+    test("append FROM", async () => {
+        const sparql = "SELECT * FROM <urn:a:b:c> FROM <urn:d:e:f> WHERE { ?s ?p ?o .}";
+        const expected = "SELECT * FROM <urn:a:b:c>\nFROM <urn:d:e:f>\nFROM <http://toto.com/example>\nWHERE { ?s ?p ?o. }";
+        expect(addFromsToSparqlQuery(sparql, ["http://toto.com/example"], false)).toStrictEqual(expected);
+    });
+    test("replace FROM with 2 FROM", async () => {
+        const sparql = "SELECT * FROM <urn:a:b:c> FROM <urn:d:e:f> WHERE { ?s ?p ?o .}";
+        const expected = "SELECT * FROM <http://toto.com/example>\nFROM <http://titi.com/toto>\nWHERE { ?s ?p ?o. }";
+        expect(addFromsToSparqlQuery(sparql, ["http://toto.com/example", "http://titi.com/toto"], true)).toStrictEqual(expected);
+    });
+    test("append FROM with 2 FROM", async () => {
+        const sparql = "SELECT * FROM <urn:a:b:c> FROM <urn:d:e:f> WHERE { ?s ?p ?o .}";
+        const expected = "SELECT * FROM <urn:a:b:c>\nFROM <urn:d:e:f>\nFROM <http://toto.com/example>\nFROM <http://titi.com/toto>\nWHERE { ?s ?p ?o. }";
+        expect(addFromsToSparqlQuery(sparql, ["http://toto.com/example", "http://titi.com/toto"], false)).toStrictEqual(expected);
     });
 });

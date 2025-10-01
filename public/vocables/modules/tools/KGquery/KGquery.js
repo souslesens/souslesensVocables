278d44d466044ae205728e39266c050795331812
OUNNOUGHI Karim <kounnoughi@jems-group.com>
2025-10-01 15:10:08 +0200

    delete KGquerySample

--- public/vocables/modules/tools/KGquery/KGquery.js
+++ public/vocables/modules/tools/KGquery/KGquery.js
@@ -553,10 +553,7 @@ var KGquery = (function () {
                             callbackSeries(err);
                         }
                         sampleSize = $("#KGquery_sampleInput").val();
-                        if (sampleSize && !Number.isInteger(sampleSize) ) {
-                            sampleSize = null;
-                            alert("sample size must be an integer");
-                        }
+                       
                             
                         KGquery.labelFromURIToDisplay = result.labelFromURIToDisplay;
                         optionalPredicatesSparql = result.optionalPredicatesSparql;

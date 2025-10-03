1a60f6cd66900ccf29541d6a1dcd80d4daee97eb
OUNNOUGHI Karim <kounnoughi@jems-group.com>
2025-10-03 16:37:40 +0200

    bug regenerate graph

--- public/vocables/modules/tools/KGquery/KGquery_graph.js
+++ public/vocables/modules/tools/KGquery/KGquery_graph.js
@@ -718,9 +718,16 @@ var KGquery_graph = (function () {
         var data_type = "KGmodelGraph";*/
         var fileName = KGquery.currentSource + "_KGmodelGraph.json";
         self.visjsData = null;
-        if (callback) {
-            options.callback = callback;
-        }
+        
+        options.callback = function(){
+            self.visjsData = {};
+            self.visjsData.nodes = self.KGqueryGraph.data.nodes.get();
+            self.visjsData.edges = self.KGqueryGraph.data.edges.get();
+            if(callback){
+                callback();
+            }
+        };
+        
         self.KGqueryGraph.saveGraph(fileName, true, options);
 
         return;
@@ -1085,6 +1092,7 @@ var KGquery_graph = (function () {
      */
     self.exportVisjsGraph = function () {
         self.saveVisjsModelGraph(function () {
+
             self.downloadVisjsGraph(KGquery.currentSource, function (err, result) {
                 var fileName = KGquery.currentSource + "_KGmodelGraph" + ".json";
                 Export.downloadJSON(result, fileName);

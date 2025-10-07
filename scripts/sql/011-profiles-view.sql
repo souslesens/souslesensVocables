de62cd6741dcfa0105c17e4a58071bb9710656d6
xgaia <xgaia@gmx.com>
2025-10-07 15:13:32 +0200

    feat(migrations): add allowed_databases column to profiles table

--- scripts/sql/011-profiles-view.sql
+++ scripts/sql/011-profiles-view.sql
@@ -1,4 +1,4 @@
 -- Add a view to retrieve the profiles and set the schema_types as texts array
 create view profiles_list as
-       select label, theme, allowed_tools, access_control, is_shared, schema_types::text[]
+       select label, theme, allowed_tools, allowed_databases, access_control, is_shared, schema_types::text[]
        from profiles;

bc2db658a889ca16b0bdbc633d2a6d521ee7b94f
juste64 <jbonzene@gmail.com>
2025-10-17 09:24:45 +0200

    connexions

--- bin/KGbuilder/triplesMaker.js
+++ bin/KGbuilder/triplesMaker.js
@@ -160,15 +160,9 @@ var TriplesMaker = {
                     async.eachSeries(
                         slices,
                         function (data, callbackEach) {
-<<<<<<< HEAD
                            
                             options.currentBatchRowIndex = currentBatchRowIndex;
                             
-=======
-                            if (options) {
-                                options.currentBatchRowIndex = currentBatchRowIndex;
-                            }
->>>>>>> origin/master
                             TriplesMaker.buildTriples(data, tableProcessingParams, options, function (err, batchTriples) {
                                 //  totalTriplesCount += batchTriples.length;
                                 var currentTime = new Date();

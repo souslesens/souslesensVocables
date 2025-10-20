afd2cb74c1c6cb697a82eec6c49d3db17b8404d7
xgaia <xgaia@gmx.com>
2025-10-20 17:46:04 +0200

    fix(migrations): sort migrations script with -V (sort by version number)

--- scripts/run-migrations.sh
+++ scripts/run-migrations.sh
@@ -1,6 +1,6 @@
 #!/bin/env sh
 
-for FILE in $(find scripts/migrations/ -type f -name "migration_*.js" | sort); do
+for FILE in $(find scripts/migrations/ -type f -name "migration_*.js" | sort -V); do
     echo "Run the migration $(basename $FILE)"
     node $FILE -c config -w && echo "🎉 DONE"
 done

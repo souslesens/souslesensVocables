afe027e9f042a5174016c2cffa857052df489f7d
juste64 <jbonzene@gmail.com>
2025-10-21 17:22:08 +0200

    prettier

--- public/vocables/modules/shared/UI.js
+++ public/vocables/modules/shared/UI.js
@@ -247,10 +247,14 @@ var UI = (function () {
         const offL = existingWindowConvert.offset() || { left: gap, top: gap };
         const top = Math.min(Math.max(gap, offL.top), windowHeight - gap - targetH);
         $(newWindow).dialog("option", "position", {
-            my: "right top", at: `right+${gap} top+${top}`, of: window
+            my: "right top",
+            at: `right+${gap} top+${top}`,
+            of: window,
         });
         $(existingWindow).dialog("option", "position", {
-            my: "left top", at: `left-${gap} top+${top}`, of: window
+            my: "left top",
+            at: `left-${gap} top+${top}`,
+            of: window,
         });
 
         //  Prevent from going outside during drag/resize

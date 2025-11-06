403f65a583d411931f62ff9dba1a9019175ad787
juste64 <jbonzene@gmail.com>
2025-11-06 11:52:40 +0100

    prettier

--- public/vocables/modules/tools/lineage/lineage_whiteboard.js
+++ public/vocables/modules/tools/lineage/lineage_whiteboard.js
@@ -2550,8 +2550,8 @@ var Lineage_whiteboard = (function () {
                             if (type.indexOf("Class") > -1) {
                                 rdfType = "Class";
                             }
-                            if(item.subjectType.value.indexOf("NamedIndividual")){
-                                rdfType="NamedIndividual";
+                            if (item.subjectType.value.indexOf("NamedIndividual")) {
+                                rdfType = "NamedIndividual";
                             }
 
                             if (item.object.type == "bnode") {

77d9724b316e1396b10a7f7f33bb5991bb6c10d1
ounnoughik <ounnoughikarim@gmail.com>
2025-12-29 15:06:26 +0100

    Kgquery_graph  fix: datatype property unicity checking

--- public/vocables/modules/tools/KGquery/KGquery_filter.js
+++ public/vocables/modules/tools/KGquery/KGquery_filter.js
@@ -203,7 +203,12 @@ var KGquery_filter = (function () {
             return callback("no properties selected");
         }
         var labelFromURIToDisplay = [];
+        var uniqueProps={};
         propertyNodes.forEach(function (node) {
+            if (uniqueProps[node.id]) {
+                return;
+            }
+            uniqueProps[node.id] = true;
             if (node?.data?.property && node?.data?.property?.id == "labelFromURI") {
                 labelFromURIToDisplay.push(node?.data?.varName);
                 return;
@@ -212,6 +217,7 @@ var KGquery_filter = (function () {
                 selectedPropertyNodes.push(node);
                 selectClauseSparql += " ?" + node.id;
             }
+            
         });
 
         if (selectedPropertyNodes.length > KGquery.maxOptionalPredicatesInQuery) {

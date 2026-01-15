2e32634ed5a90d8c48d28576a0ba5585e9bf6b8a
Alexandre Richardson <alexandre.richardson@logilab.fr>
2026-01-15 10:36:11 +0100

    feat: add a modal to show a formular for adding user data

--- mainapp/src/users-data-management.tsx
+++ mainapp/src/users-data-management.tsx
@@ -9,6 +9,13 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { ButtonWithConfirmation } from "./Component/ButtonWithConfirmation";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+import { style } from "./Utils";
 
 interface UsersData {
     id: number;
@@ -37,6 +44,7 @@ declare global {
 
 export default function UsersDataManagement() {
     const [usersData, setUsersData] = useState<UsersData[]>([]);
+    const [displayModal, setDisplayModal] = useState<boolean>(false);
 
     const fetchUsersData = async () => {
         const response = await fetch("/api/v1/users/data");
@@ -49,51 +57,81 @@ export default function UsersDataManagement() {
         await fetchUsersData();
     };
 
+    const handleOpenCreateModal = () => {
+	setDisplayModal(true);
+    };
+
     useEffect(() => {
         void fetchUsersData();
     }, []);
     return (
-        <TableContainer sx={{ height: "400px" }} component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell style={{ fontWeight: "bold" }}>Id</TableCell>
-                        <TableCell style={{ fontWeight: "bold" }} align="right">
-                            Label
-                        </TableCell>
-                        <TableCell style={{ fontWeight: "bold" }} align="right">
-                            Type
-                        </TableCell>
-                        <TableCell style={{ fontWeight: "bold" }} align="right">
-                            Tool
-                        </TableCell>
+        <>
+            <Stack direction="column" spacing={{ xs: 2 }} sx={{ m: 4 }} useFlexGap>
+                <TableContainer sx={{ height: "400px" }} component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell style={{ fontWeight: "bold" }}>Id</TableCell>
+                                <TableCell style={{ fontWeight: "bold" }} align="right">
+                                    Label
+                                </TableCell>
+                                <TableCell style={{ fontWeight: "bold" }} align="right">
+                                    Type
+                                </TableCell>
+                                <TableCell style={{ fontWeight: "bold" }} align="right">
+                                    Tool
+                                </TableCell>
 
-                        <TableCell style={{ fontWeight: "bold" }} align="right">
-                            Source
-                        </TableCell>
-                        <TableCell style={{ fontWeight: "bold" }} align="right">
-                            Actions
-                        </TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {usersData.map((row) => (
-                        <TableRow key={row.id} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                            <TableCell align="left" component="th" scope="row">
-                                {row.id}
-                            </TableCell>
-                            <TableCell align="right">{row.data_label}</TableCell>
-                            <TableCell align="right">{row.data_type}</TableCell>
-                            <TableCell align="right">{row.data_tool}</TableCell>
-                            <TableCell align="right">{row.data_source}</TableCell>
-                            <TableCell align="right">
-                                <ButtonWithConfirmation func={deleteUserData} label="Delete" args={[row.id]} />
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                                <TableCell style={{ fontWeight: "bold" }} align="right">
+                                    Source
+                                </TableCell>
+                                <TableCell style={{ fontWeight: "bold" }} align="right">
+                                    Actions
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {usersData.map((row) => (
+                                <TableRow key={row.id} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                                    <TableCell align="left" component="th" scope="row">
+                                        {row.id}
+                                    </TableCell>
+                                    <TableCell align="right">{row.data_label}</TableCell>
+                                    <TableCell align="right">{row.data_type}</TableCell>
+                                    <TableCell align="right">{row.data_tool}</TableCell>
+                                    <TableCell align="right">{row.data_source}</TableCell>
+                                    <TableCell align="right">
+                                        <ButtonWithConfirmation func={deleteUserData} label="Delete" args={[row.id]} />
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <Stack direction="row" justifyContent="center" spacing={{ xs: 1 }} useFlexGap>
+                    <Button variant="contained" onClick={handleOpenCreateModal}>
+                        create
+                    </Button>
+                </Stack>
+            </Stack>
+            <Modal
+                open={displayModal}
+                onClose={() => {
+                    setDisplayModal(false);
+                }}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style} style={{ maxHeight: "100%", overflow: "auto" }}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Text in a modal
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                    </Typography>
+                </Box>
+            </Modal>
+        </>
     );
 }
 

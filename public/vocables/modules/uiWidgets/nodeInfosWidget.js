f01a58050f4cedac328e8ad18f639fa1ac9db5dc
juste64 <jbonzene@gmail.com>
2026-02-04 10:50:33 +0100

    nodeInfosAxiom

--- public/vocables/modules/uiWidgets/nodeInfosWidget.js
+++ public/vocables/modules/uiWidgets/nodeInfosWidget.js
@@ -127,8 +127,10 @@ var NodeInfosWidget = (function () {
                     $("[aria-selected='true']").addClass("nodesInfos-selectedTab");
                     if (ui.newPanel.selector == "#nodeInfosWidget_AxiomsTabDiv") {
                         var source = self.currentSource;
-                        // source = Lineage_sources.mainSource;
                         UI.setDialogTitle("#smallDialogDiv", "Axioms of resource " + self.currentNode.data.label);
+                        $("#smallDialogDiv").dialog("option", "close", function () {
+                            $("#nodeInfosWidget_tabsDiv").tabs("option", "active", 0);
+                        });
                         NodeInfosAxioms.init(source, self.currentNode, "smallDialogDiv");
                     }
                     if (ui.newPanel.selector == "#nodeInfosWidget_relationsDiv") {

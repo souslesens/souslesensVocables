f71fd5d880df12d61752665af12b369500690760
ounnoughik <ounnoughikarim@gmail.com>
2026-02-06 15:09:24 +0100

    Reapply "fix(ecmascript): remigrate new files"
    This reverts commit dad9ebbd8940dd305e91011baeaffc3381556448.

--- api/v1/paths/elasticsearch/indexdocuments.js
+++ api/v1/paths/elasticsearch/indexdocuments.js
@@ -1,131 +1,17 @@
-const path = require("path");
-const elasticRestProxy = require(path.resolve("bin/elasticRestProxy..js"));
-const ConfigManager = require("../../../../bin/configManager.");
-const UserRequestFiltering = require("../../../../bin/userRequestFiltering.");
-const { processResponse } = require("../utils");
-const { sourceModel } = require("../../../../model/sources");
-const async = require("async");
+import elasticRestProxy from "../../../../bin/elasticRestProxy.js";
 
-module.exports = function () {
+export default function () {
     let operations = {
         POST,
     };
 
     function POST(req, res, _next) {
-
-
-       elasticRestProxy.indexDocuments( req.body.rootDir, req.body.indexName, function (err, result) {
+        elasticRestProxy.indexDocuments(req.body.rootDir, req.body.indexName, function (err, result) {
             if (err) {
                 return res.status(400).json({ error: err });
             }
             return res.status(200).json(result);
         });
-       return;
-
-
-
-
-
-        async function isIndexPrivate(userInfo, indexName, callback) {
-            const sources = await sourceModel.getAllSources();
-
-            // ajout provisoire CF
-            if (indexName.startsWith("whiteboard_")) {
-                return callback(null, true);
-            }
-            var sourceObj = null;
-            for (var key in sources) {
-                if (indexName == key.toLowerCase()) {
-                    sourceObj = sources[key];
-                }
-            }
-            if (!sourceObj) {
-                return callback("source not exists ");
-            }
-            callback(null, userInfo.user.login == sourceObj.owner);
-        }
-
-        var parsingError = null;
-        var result = null;
-        var userInfo = null;
-        async.series(
-            [
-                //get UserInfo
-                function (callbackSeries) {
-                    ConfigManager.getUser(req, res, function (err, _userInfo) {
-                        if (err) {
-                            return callbackSeries(err);
-                        }
-                        userInfo = _userInfo;
-                        callbackSeries();
-                    });
-                },
-                // is source private
-                function (callbackSeries) {
-                    isIndexPrivate(userInfo, req.body.indexName, function (err, isPrivate) {
-                        if (err) {
-                            return callbackSeries(err);
-                        }
-                        if (isPrivate) {
-                            elasticRestProxy.indexSource(req.body.indexName, req.body.data, req.body.options, function (err, _result) {
-                                callbackSeries(err);
-                            });
-                        } else {
-                            callbackSeries();
-                        }
-                    });
-                },
-                //normal source and ConfigManager
-                function (callbackSeries) {
-                    if (!ConfigManager.config) {
-                        return callbackSeries();
-                    } else {
-                        ConfigManager.getUserSources(req, res, function (err, userSources) {
-                            UserRequestFiltering.validateElasticSearchIndices(null, [req.body.indexName], userSources, "w", function (_parsingError, _filteredQuery) {
-                                if (_parsingError) {
-                                    parsingError = _parsingError;
-                                    return callbackSeries();
-                                    // return processResponse(res, parsingError, null);
-                                }
-
-                                elasticRestProxy.indexSource(req.body.indexName, req.body.data, req.body.options, function (err, _result) {
-                                    if (err) {
-                                        return callbackSeries(err);
-                                    }
-                                    result = _result;
-                                });
-                            });
-                        });
-                    }
-                },
-
-                function (callbackSeries) {
-                    if (!ConfigManager.config) {
-                        return callbackSeries();
-                    } else {
-                        elasticRestProxy.indexSource(req.body.indexName, req.body.data, req.body.options, function (err, result) {
-                            if (err) {
-                                return res.status(400).json({ error: err });
-                            }
-                            return res.status(200).json(result);
-                        });
-                    }
-                },
-            ],
-
-            function (err) {
-                if (err) {
-                    return res.status(400).json({ error: err });
-                }
-                if (parsingError) {
-                    return processResponse(res, parsingError, null);
-                }
-                if (result) {
-                    return res.status(200).json(result);
-                }
-                return res.status(200).json(result);
-            },
-        );
     }
 
     POST.apiDoc = {
@@ -146,7 +32,7 @@ module.exports = function () {
                         },
                         rootDir: {
                             type: "string",
-                        }
+                        },
                     },
                 },
             },
@@ -164,4 +50,4 @@ module.exports = function () {
     };
 
     return operations;
-};
+}

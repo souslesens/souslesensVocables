6942abc0eb8b192a61dbfa1b6e9c0456f5cee7a8
juste64 <jbonzene@gmail.com>
2026-04-28 16:56:13 +0200

    dcidenitifier

--- public/vocables/modules/bots/predicates_bot.js
+++ public/vocables/modules/bots/predicates_bot.js
@@ -108,11 +108,30 @@ var Predicates_bot = (function () {
         return false;
     }
 
+    var knownLiteralProperties = [
+        "http://purl.org/dc/elements/1.1/identifier",
+        "http://purl.org/dc/elements/1.1/title",
+        "http://purl.org/dc/elements/1.1/description",
+        "http://purl.org/dc/elements/1.1/subject",
+        "http://purl.org/dc/elements/1.1/creator",
+        "http://purl.org/dc/elements/1.1/contributor",
+        "http://purl.org/dc/elements/1.1/publisher",
+        "http://purl.org/dc/elements/1.1/date",
+        "http://purl.org/dc/elements/1.1/type",
+        "http://purl.org/dc/elements/1.1/format",
+        "http://purl.org/dc/elements/1.1/source",
+        "http://purl.org/dc/elements/1.1/language",
+        "http://purl.org/dc/elements/1.1/rights",
+    ];
+
     function isLiteralProperty(property) {
         if (!property) {
             return false;
         }
         if (property.indexOf("xsd:") === 0) {
+            return true;
+        }
+        if (knownLiteralProperties.indexOf(property) > -1) {
             return true;
         }
         for (var vocab in Config.ontologiesVocabularyModels) {

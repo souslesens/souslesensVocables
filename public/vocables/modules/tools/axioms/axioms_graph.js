b69b23c9a0e905034c87ed7c119096b81beb2e34
ounnoughikarim <ounnoughikarim@users.noreply.github.com>
2026-07-17 11:17:09 +0200

    fix(Axioms): improve delay of classes loading

--- public/vocables/modules/tools/axioms/axioms_graph.js
+++ public/vocables/modules/tools/axioms/axioms_graph.js
@@ -104,11 +104,21 @@ var Axioms_graph = (function () {
                             return "bnode";
                         }
 
+                        // resources from imports have no rdf:type triple in the axiom, resolve their type
+                        // from the resources map so they keep the same rendering as during axiom creation
+                        var resourceTypeUris = {
+                            Class: "http://www.w3.org/2002/07/owl#Class",
+                            ObjectProperty: "http://www.w3.org/2002/07/owl#ObjectProperty",
+                        };
+
                         if (!nodesMap[s]) {
                             nodesMap[s] = { id: s, axiomId: axiomIndex };
                             if (s.indexOf("http") == 0) {
                                 var obj = Axiom_manager.allResourcesMap[s];
                                 nodesMap[s].label = obj ? obj.label.replace(/_/g, " ") : null;
+                                if (obj && obj.resourceType && resourceTypeUris[obj.resourceType]) {
+                                    nodesMap[s].type = resourceTypeUris[obj.resourceType];
+                                }
                             }
                         }
 
@@ -123,6 +133,9 @@ var Axioms_graph = (function () {
                             if (o.indexOf("http") == 0) {
                                 var objectResource = Axiom_manager.allResourcesMap[o];
                                 nodesMap[o].label = objectResource ? objectResource.label.replace(/_/g, " ") : null;
+                                if (objectResource && objectResource.resourceType && resourceTypeUris[objectResource.resourceType]) {
+                                    nodesMap[o].type = resourceTypeUris[objectResource.resourceType];
+                                }
                             }
                         }
                         if (p == "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" && !nodesMap[s].type) {

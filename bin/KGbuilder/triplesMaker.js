souslesens/souslesensVocables

commit 9ef76fc0d2bc7f0cb643b8c9dc65d95166a4fb0f

fixed some bugs iin mappingModeler and KGbuilder and implement lineage_nodeCentricGraph.js: exportVisjsGraphTreeText

bin/KGbuilder/triplesMaker.js
@@ -441,10 +441,10 @@ var TriplesMaker = {
                 var property = TriplesMaker.getPropertyUri(edge.data.id);
 
                 if (edge.isRestriction) {
-                    var triples = TriplesMaker.getRestrictionTriples(subjectUri, property, objectUri, edge.restrictionType, { cardinality: edge.cardinality });
+                    var triples = TriplesMaker.getRestrictionTriples(subjectUri, property, objectUri, edge.restrictionType, tableProcessingParams);
                     triples.forEach(function (triple) {
                         addTriple(triple.s, triple.p, triple.o);
-                    });
+                           });
                 } else {
                     addTriple(subjectUri, property, objectUri);
                 }
@@ -750,7 +750,7 @@ var TriplesMaker = {
         return objectStr;
     },
 
-    getRestrictionTriples: function (subjectUri, predicateUri, objectUri, restrictionType, options) {
+    getRestrictionTriples: function (subjectUri, predicateUri, objectUri, restrictionType,tableProcessingParams, options) {
         if (!options) {
             options = {};
         }
@@ -800,7 +800,10 @@ var TriplesMaker = {
             p: "rdfs:subClassOf",
             o: blankNode,
         });
-
+        if (!tableProcessingParams.isSampleData) {
+            var metaDataTriples = TriplesMaker.getMetaDataTriples(blankNode, tableProcessingParams.tableInfos.table);
+            triples = triples.concat(metaDataTriples)
+        }
         TriplesMaker.existingRestrictions[subjectUri + predicateUri + objectUri] = 1;
 
         return triples;

public/vocables/modules/shared/export.js
@@ -1,8 +1,10 @@
 import common from "./common.js";
 import SearchUtil from "../search/searchUtil.js";
 import Sparql_common from "../sparqlProxies/sparql_common.js";
+import Lineage_whiteboard from "../tools/lineage/lineage_whiteboard.js";
 
 import MainController from "./mainController.js";
+import Lineage_nodeCentricGraph from "../tools/lineage/lineage_nodeCentricGraph.js";
 
 var Export = (function () {
     var self = {};
@@ -507,7 +509,7 @@ fixedColumns: true*/
         }
         if (visjsGraph == "KGquery_graph.KGqueryGraph") {
             html += `<span class="popupMenuItem" onclick="KGquery_graph.exportVisjsGraph();">JSON</span>`;
-        }
+             }
         if (visjsGraph == "Lineage_whiteboard.graph") {
             html += `<span class="popupMenuItem" onclick="Lineage_whiteboard.graph.exportWhiteboard();">JSON</span>`;
         }
@@ -550,6 +552,18 @@ fixedColumns: true*/
         link.click();
         document.body.removeChild(link);
         URL.revokeObjectURL(link.href);
+    };
+
+    self.downloadText = function (text, fileName) {
+
+        const blob = new Blob([text], { type: "text/plain; charset=utf-8" });
+        const link = document.createElement("a");
+
+        link.href = URL.createObjectURL(blob);
+        link.download = fileName;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
     };
 
     /**

public/vocables/modules/sparqlProxies/sparql_generic.js
@@ -1496,6 +1496,9 @@ var Sparql_generic = (function () {
                     // processedParents guards against cycles (e.g. OWL reflexive subClassOf triples materialized by reasoner)
                     var processedParents = {};
                     function buildParentChain(nodeId) {
+                      if(self.processedParents[nodeId])
+                          return []
+                        self.processedParents[nodeId]=1
                         var obj = allClassesMap[nodeId];
                         if (!obj || obj._chainBuilt) return obj ? obj.parents : [];
                         if (processedParents[nodeId]) return [];
@@ -1505,7 +1508,7 @@ var Sparql_generic = (function () {
                         obj._chainBuilt = true;
                         return obj.parents;
                     }
-
+                    self.processedParents={}
                     for (var key in allClassesMap) {
                         buildParentChain(key);
                         delete allClassesMap[key]._directParents;

public/vocables/modules/tools/KGquery/KGquery_graph.js
@@ -1556,6 +1556,9 @@ var KGquery_graph = (function () {
         });
     };
 
+
+
+
     /**
      * Imports a KG model graph from a JSON file.
      * @function

public/vocables/modules/tools/lineage/lineage_nodeCentricGraph.js
@@ -24,7 +24,10 @@ var Lineage_nodeCentricGraph = (function () {
      * @returns {object} nodes: newNodes, edges: newEdges, hierarchical sub‑graph
      * (nodes with level, and the traversed edges)
      */
-    self.getHierarchicalViewVisjsdata = function (rootNodeId) {
+    self.getHierarchicalViewVisjsdata = function (rootNodeId,options) {
+        if(!options){
+            options={}
+        }
         var edgesFromMap = {};
         var edgesToMap = {};
         var nodesMap = {};
@@ -54,7 +57,8 @@ var Lineage_nodeCentricGraph = (function () {
             if (!existingNodes[nodeId]) {
                 existingNodes[nodeId] = 1;
                 nodesMap[nodeId].level = level;
-                newNodes.push(nodesMap[nodeId]);
+                var node = nodesMap[nodeId]
+                newNodes.push(node);
                 var edges = edgesFromMap[nodeId];
                 if (edges) {
                     edges.forEach(function (edge) {
@@ -68,12 +72,26 @@ var Lineage_nodeCentricGraph = (function () {
 
                 //relations inverses
                 edges = edgesToMap[nodeId];
-                if (false && edges) {
+                if (options.inverse && edges) {
                     edges.forEach(function (edge) {
                         if (!existingNodes[edge.id]) {
                             existingNodes[edge.id] = 1;
                             newEdges.push(edge);
                             recurse(edge.from, level + 1);
+                        }
+                    });
+                }
+
+                // subClassOf or rdftype
+                if (edges) {
+                    edges.forEach(function (edge) {
+                        var nodeTo = nodesMap[edge.to]
+                        if (edge.data && edge.data.type == "parent") {
+                            if (!existingNodes[edge.id]) {
+                                existingNodes[edge.id] = 1;
+                                newEdges.push(edge);
+                                recurse(edge.from, level + 1);
+                            }
                         }
                     });
                 }
@@ -91,7 +109,7 @@ var Lineage_nodeCentricGraph = (function () {
             }
         }
 
-        return { nodes: newNodes, edges: newEdges };
+        return {nodes: newNodes, edges: newEdges};
     };
 
     /**
@@ -163,7 +181,7 @@ var Lineage_nodeCentricGraph = (function () {
     self.listAllNodeRelations = function (rootNodeId) {
         var nodes = Lineage_whiteboard.lineageVisjsGraph.data.nodes.get();
         var edges = Lineage_whiteboard.lineageVisjsGraph.data.edges.get();
-        var visjsData = { nodes: nodes, edges: edges };
+        var visjsData = {nodes: nodes, edges: edges};
 
         GraphPaths_bot.start(visjsData, rootNodeId, null);
 
@@ -172,6 +190,96 @@ var Lineage_nodeCentricGraph = (function () {
         common.copyTextToClipboard(str);
 
         return;
+    };
+
+
+    /**
+     * provides a text of the tree calculated from a root node
+     */
+    self.exportVisjsGraphTreeText = function () {
+        var rootNode = Lineage_whiteboard.currentGraphNode;
+        if (!rootNode) {
+            return alert("select a node as root for the tree and do it again")
+        }
+
+        var hierarchicalVisjsData = self.getHierarchicalViewVisjsdata(rootNode.id,{inverse:1});
+
+        var edgesFromMap = {}
+        var nodesMap = {}
+
+        var visitedNodes={}
+
+        hierarchicalVisjsData.nodes.forEach(function (node) {
+            nodesMap[node.id] = node
+
+        })
+        hierarchicalVisjsData.edges.forEach(function (edge) {
+            if (!edgesFromMap[edge.from]) {
+                edgesFromMap[edge.from] = []
+            }
+            edgesFromMap[edge.from].push(edge)
+
+
+
+
+           // if(edge.data && edge.data.type=="parent"){
+                if(edge.data ){
+                if (!edgesFromMap[edge.to]) {
+                    edgesFromMap[edge.to] = []
+                }
+                edgesFromMap[edge.to].push({
+                    from:edge.to,
+                    to:edge.from,
+                    data:edge.data,
+                    id:common.getRandomHexaId(6),
+                    label:edge.label
+                })
+                    if(edge.data.type!=="parent")
+                    nodesMap[edge.to].data.relation=edge.label
+            }else{
+                nodesMap[edge.to].data.relation=edge.label
+            }
+        })
+
+        if (!nodesMap[rootNode.id]) {
+            return alert("select a node as root for the tree and do it again")
+        }
+
+        var nl=String.fromCharCode(10)
+        var str=rootNode.label+nl
+        function recurse(x, nodeId,parentNode) {
+            str += "│"
+            var node = nodesMap[nodeId]
+            for (var i = 0; i < node.level; i++) {
+                str += " "
+            }
+            if(parentNode && parentNode.data){
+                node.data.label= node.data.label.replace(parentNode.data.label,"")
+            }
+            var label=node.data.label
+            if(node.data.relation)
+                label="["+node.data.relation+"]──>"+label
+            str +="├──" +label + nl
+
+            if (edgesFromMap[nodeId]){
+                edgesFromMap[nodeId].forEach(function (edge) {
+                    if (!visitedNodes[edge.to]) {
+
+                        if(nodesMap[edge.to]) {
+                            visitedNodes[edge.to] = 1
+                            recurse(str, edge.to, node)
+                        }
+                }
+                })
+        }
+
+console.log(str)
+        }
+        var text = recurse("", rootNode.id)
+        var fileName = rootNode.data.source + "_" + rootNode.data.label + "_tree.txt"
+        Export.downloadJSON(str, fileName);
+
+
     };
 
     return self;

public/vocables/modules/tools/lineage/lineage_whiteboard.js
@@ -29,6 +29,7 @@ import LegendOverlayWidget from "../../uiWidgets/legendOverlayWidget.js";
 import Lineage_legendOverlay from "./lineage_legendOverlay.js";
 import Axioms_graph from "../axioms/axioms_graph.js";
 import Axioms_manager from "../axioms/axioms_manager.js";
+import KGquery from "../KGquery/KGquery.js";
 
 /** The MIT License
  Copyright 2020 Claude Fauconnet / SousLesens Claude.fauconnet@gmail.com
@@ -1930,7 +1931,7 @@ var Lineage_whiteboard = (function () {
         options.skipRestrictions = 1;
         options.selectGraph = 1;
         options.includeSources = [MainController.currentSource];
-        options.filter = ' FILTER (regex(str(?child1),"http"))';
+       // options.filter = ' FILTER (regex(str(?child1),"http"))';
 
         Sparql_generic.getNodeChildren(source, null, parentIds, depth, options, function (err, result) {
             if (err) {
@@ -2994,7 +2995,9 @@ restrictionSource = Config.predicatesSource;
                     if (!item.subject.value.startsWith("http") || !item.value.value.startsWith("http")) {
                         return;
                     }
-
+if(item.subject.value=='_:b02d0194c19'){
+    var x=3
+}
                     if (!existingNodes[item.subject.value]) {
                         existingNodes[item.subject.value] = 1;
 
@@ -3384,6 +3387,8 @@ restrictionSource = Config.predicatesSource;
         if (node && !node.from && !(node.data && node.data.rdfType === "literal")) {
             html += '    <span  class="popupMenuItem" onclick="Lineage_whiteboard.graphActions.showHierarchicalView();">Horizontal view </span>';
             html += '    <span  class="popupMenuItem" onclick="Lineage_whiteboard.graphActions.showVerticalView();">Vertical view </span>';
+            html += `<span class="popupMenuItem" style="font-family: Courier" onclick="Lineage_nodeCentricGraph.exportVisjsGraphTreeText();">tree text</span>`;
+
             html += '    <span  class="popupMenuItem" onclick="Lineage_whiteboard.graphActions.listAllNodeRelations();">List All relations </span>';
             html += '    <span  class="popupMenuItem" onclick="Lineage_whiteboard.graphActions.drawAxioms();">Draw xioms</span>';
         }
@@ -5365,6 +5370,7 @@ attrs.color=self.getSourceColor(superClassValue)
             self.initUI();
         }
     };
+
 
     return self;
 })();

public/vocables/modules/tools/mappingModeler/mappingModeler.js
@@ -44,7 +44,7 @@ var MappingModeler = (function () {
      * @memberof module:MappingModeler
      */
     self.jstreeDivId = "mappingModeler_dataSourcesJstreeDiv";
-    self.columnsMappingsObjects = ["Column", "RowIndex", "VirtualColumn", "URI"];
+    self.columnsMappingsObjects = ["Column", "RowIndex", "VirtualColumn", "URI","Class"];
     /**
      * ID of the legend container.
      * @type {string}

public/vocables/modules/tools/mappingModeler/tripleFactory.js
@@ -883,7 +883,8 @@ var TripleFactory = (function () {
             var toMappingNode = self.columnsMap[edge.to];
             var toClassNode = classNodesMap[edge.to];
             if (fromMappingNode && toMappingNode) {
-                var label = fromMappingNode.label + "-" + edge.label + "->" + toMappingNode.label;
+                var jstreeEdgeLabel = edge.label === "a" ? "rdf:type" : edge.label;
+                var label = fromMappingNode.label + "-" + jstreeEdgeLabel + "->" + toMappingNode.label;
                 treeData.push({
                     id: edge.id,
                     text: label,

commit 53ddd507965dd8cfb2c45e0641b854a11c6706ad

fix(mappingModeler): restore dropped cardinality in KG restriction triples
Regression from 9ef76fc0d: adding tableProcessingParams to
getRestrictionTriples clobbered the { cardinality } argument, so
qualifiedCardinality restrictions built by KGbuilder silently degraded
to someValuesFrom. Pass cardinality again in the options slot.

Expand owl: prefixed restriction predicates to the full owl namespace
URI so qualifiedCardinality predicates are valid (KGbuilder declares no
owl: prefix), matching the triples lineage produces.

Also strip trailing spaces from owl:allValuesFrom/owl:hasValue bot ids
and fix a broken concat of options.additionalTriples in lineage.

bin/KGbuilder/triplesMaker.js
@@ -476,7 +476,7 @@ var TriplesMaker = {
                 var property = TriplesMaker.getPropertyUri(edge.data.id);
 
                 if (edge.isRestriction) {
-                    var triples = TriplesMaker.getRestrictionTriples(subjectUri, property, objectUri, edge.restrictionType, tableProcessingParams);
+                    var triples = TriplesMaker.getRestrictionTriples(subjectUri, property, objectUri, edge.restrictionType, tableProcessingParams, { cardinality: edge.cardinality });
                     triples.forEach(function (triple) {
                         addTriple(triple.s, triple.p, triple.o);
                         if (!tableProcessingParams.isSampleData) {
@@ -804,6 +804,12 @@ var TriplesMaker = {
 
         var blankNode = "<_:b" + util.getRandomHexaId(10) + ">";
 
+        // lineage stores the restriction predicate as an owl: prefixed name resolved via PREFIX header;
+        // triplesMaker has no owl: prefix declared, so expand owl:* to the full owl namespace URI (identical triple)
+        var owlNamespaceUri = "http://www.w3.org/2002/07/owl#";
+        var restrictionPredicateUri = restrictionType.indexOf("owl:") === 0 ? owlNamespaceUri + restrictionType.substring("owl:".length) : restrictionType;
+        var restrictionPredicateTerm = "<" + restrictionPredicateUri + ">";
+
         triples.push({
             s: blankNode,
             p: "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>",
@@ -818,7 +824,7 @@ var TriplesMaker = {
         if (options.cardinality && options.cardinality.value) {
             triples.push({
                 s: blankNode,
-                p: "<" + restrictionType + ">",
+                p: restrictionPredicateTerm,
                 o: '"' + options.cardinality.value + '"^^<http://www.w3.org/2001/XMLSchema#nonNegativeInteger>',
             });
             triples.push({
@@ -829,7 +835,7 @@ var TriplesMaker = {
         } else {
             triples.push({
                 s: blankNode,
-                p: "<" + restrictionType + ">",
+                p: restrictionPredicateTerm,
                 o: objectUri,
             });
         }

public/vocables/modules/bots/createRestriction_bot.js
@@ -109,8 +109,8 @@ var CreateRestriction_bot = (function () {
         chooseConstraintTypeFn: function () {
             var choices = [
                 { id: "owl:someValuesFrom", label: "owl:someValuesFrom" },
-                { id: "owl:allValuesFrom ", label: "owl:allValuesFrom " },
-                { id: "owl:hasValue ", label: "owl:hasValue " },
+                { id: "owl:allValuesFrom", label: "owl:allValuesFrom" },
+                { id: "owl:hasValue", label: "owl:hasValue" },
                 { id: "owl:maxQualifiedCardinality", label: "owl:maxQualifiedCardinality" },
                 { id: "owl:minQualifiedCardinality", label: "owl:minQualifiedCardinality" },
                 { id: "owl:qualifiedCardinality", label: "owl:qualifiedCardinality" },

public/vocables/modules/tools/lineage/lineage_createRelation.js
@@ -935,7 +935,7 @@ var Lineage_createRelation = (function () {
 
                     allTriples = allTriples.concat(restrictionTriples);
                     if (options.additionalTriples) {
-                        allTriplesallTriples.concat(options.additionalTriples);
+                        allTriples = allTriples.concat(options.additionalTriples);
                     }
 
                     Sparql_generic.insertTriples(inSource, allTriples, null, function (err, _result) {